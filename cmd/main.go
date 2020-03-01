5b52cfd5a911ca25594a99fc83402041b29613fe
Michael Filanov <mfilanov@redhat.com>
2020-03-01 12:00:49 +0200

    image api implementaion

--- cmd/main.go
+++ cmd/main.go
@@ -6,14 +6,29 @@ import (
 	"net/http"
 
 	"github.com/filanov/bm-inventory/internal/bminventory"
+	"github.com/filanov/bm-inventory/models"
 	"github.com/filanov/bm-inventory/restapi"
 	"github.com/go-openapi/swag"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	port := flag.String("port", "8090", "define port that the service will listen to")
 	flag.Parse()
+
+	logrus.Println("Starting bm service")
+
+	db, err := gorm.Open("postgres", "host=172.17.0.7 port=5432 user=postgresadmin dbname=postgresdb password=admin123 sslmode=disable")
+	if err != nil {
+		logrus.Error("Fail to connect to DB, ", err)
+	}
+	defer db.Close()
+
+	if err := db.AutoMigrate(&models.Image{}, &models.Node{}, &models.Cluster{}).Error; err != nil {
+		logrus.Fatal("failed to auto migrate, ", err)
+	}
 
 	bm := bminventory.NewBareMetalInventory()
 	h, err := restapi.Handler(restapi.Config{
--- internal/bminventory/inventory.go
+++ internal/bminventory/inventory.go
@@ -4,62 +4,119 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/sirupsen/logrus"
+
+	"github.com/google/uuid"
+
 	"github.com/filanov/bm-inventory/models"
-
-	"github.com/go-openapi/runtime/middleware"
-
 	"github.com/filanov/bm-inventory/restapi/operations/inventory"
+	"github.com/go-openapi/runtime/middleware"
+	"github.com/go-openapi/strfmt"
+	"github.com/go-openapi/swag"
+	"github.com/jinzhu/gorm"
 )
 
-const baseHref = "/api/bm.inventory/v1/node/"
+const baseHref = "/api/bm.inventory/v1"
+const image = "image"
 const bareMetalType = "BareMetalNode"
 
+const (
+	ImageStatusCreating = "creating"
+	ImageStatusReady    = "ready"
+	ImageStatusError    = "error"
+)
+
+const (
+	ResourceKindImage   = "image"
+	ResourceKindNode    = "node"
+	ResourceKindCluster = "cluster"
+)
+
 type bareMetalInventory struct {
+	db *gorm.DB
 }
 
 func NewBareMetalInventory() *bareMetalInventory {
 	return &bareMetalInventory{}
 }
 
-func (b *bareMetalInventory) ListNodes(ctx context.Context, params inventory.ListNodesParams) middleware.Responder {
-	return inventory.NewListNodesOK().WithPayload(models.Nodes{
-		&models.RegisteredNode{
-
-			Node: models.Node{
-				Name: "example",
-				URL:  "example.com",
-			},
-			Base: models.Base{
-				Href: fmt.Sprintf("%s:%s", baseHref, "123"),
-				ID:   "123",
-				Kind: bareMetalType,
-			},
-		},
-		&models.RegisteredNode{
-
-			Node: models.Node{
-				Name: "example",
-				URL:  "example.com",
-			},
-			Base: models.Base{
-				Href: fmt.Sprintf("%s:%s", baseHref, "456"),
-				ID:   "456",
-				Kind: bareMetalType,
-			},
-		},
-	})
+func strToURI(str string) *strfmt.URI {
+	uri := strfmt.URI(str)
+	return &uri
 }
 
-func (b *bareMetalInventory) RegisterNode(ctx context.Context, params inventory.RegisterNodeParams) middleware.Responder {
-	return inventory.NewRegisterNodeCreated().WithPayload(&models.RegisteredNode{
-		Node: models.Node{
-			Name: "example",
-			URL:  "example.com",
-		},
+func buildHrefURI(base, id string) *strfmt.URI {
+	return strToURI(fmt.Sprintf("%s/%s/%s", baseHref, base, id))
+}
+
+func (b bareMetalInventory) CreateImage(ctx context.Context, params inventory.CreateImageParams) middleware.Responder {
+	id := strfmt.UUID(uuid.New().String())
+	image := &models.Image{
 		Base: models.Base{
-			Href: fmt.Sprintf("%s:%s", baseHref, "123"),
-			ID:   "123",
-			Kind: bareMetalType,
+			Href: buildHrefURI(image, id.String()),
+			ID:   &id,
+			Kind: swag.String(ResourceKindCluster),
 		},
-	})
+		Status: swag.String(ImageStatusCreating),
+	}
+
+	if params.NewImageParams != nil {
+		image.ImageCreateParams = *params.NewImageParams
+	}
+
+	if err := b.db.Create(image).Error; err != nil {
+		logrus.WithError(err).Error("failed to create image")
+		return inventory.NewCreateImageInternalServerError()
+	}
+
+	return inventory.NewCreateImageCreated().WithPayload(image)
+}
+
+func (b bareMetalInventory) GetImage(ctx context.Context, params inventory.GetImageParams) middleware.Responder {
+	var image *models.Image
+	if err := b.db.First(image, "id = ? ", params.ImageID).Error; err != nil {
+		logrus.WithError(err).Errorf("failed to find image %s", params.ImageID)
+		return inventory.NewGetImageNotFound()
+	}
+	return inventory.NewGetImageOK().WithPayload(image)
+}
+
+func (b bareMetalInventory) ListImages(ctx context.Context, params inventory.ListImagesParams) middleware.Responder {
+	var images []*models.Image
+	if err := b.db.Find(images).Error; err != nil {
+		return inventory.NewListImagesInternalServerError()
+	}
+	return inventory.NewListImagesOK().WithPayload(images)
+}
+
+func (b bareMetalInventory) DeregisterCluster(ctx context.Context, params inventory.DeregisterClusterParams) middleware.Responder {
+	panic("implement me")
+}
+
+func (b bareMetalInventory) DeregisterNode(ctx context.Context, params inventory.DeregisterNodeParams) middleware.Responder {
+	panic("implement me")
+}
+
+func (b bareMetalInventory) GetCluster(ctx context.Context, params inventory.GetClusterParams) middleware.Responder {
+	panic("implement me")
+}
+
+func (b bareMetalInventory) GetNode(ctx context.Context, params inventory.GetNodeParams) middleware.Responder {
+	panic("implement me")
+}
+
+func (b bareMetalInventory) ListClusters(ctx context.Context, params inventory.ListClustersParams) middleware.Responder {
+	panic("implement me")
+}
+
+func (b bareMetalInventory) ListNodes(ctx context.Context, params inventory.ListNodesParams) middleware.Responder {
+	panic("implement me")
+}
+
+func (b bareMetalInventory) RegisterCluster(ctx context.Context, params inventory.RegisterClusterParams) middleware.Responder {
+	panic("implement me")
+}
+
+func (b bareMetalInventory) RegisterNode(ctx context.Context, params inventory.RegisterNodeParams) middleware.Responder {
+	panic("implement me")
 }
