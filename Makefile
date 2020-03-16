f4fda0ef94601246436c5fb56eede3669fbbbc87
Michael Filanov <mfilanov@redhat.com>
2020-03-16 10:27:09 +0200

    use environment variables to run subsystem, it will automaticali connect to local minikube and get the ip addresses

--- Makefile
+++ Makefile
@@ -21,3 +21,32 @@ generate-swagger:
 update: build
 	docker build -f Dockerfile.bm-inventory . -t $(SERVICE)
 	docker push $(SERVICE)
+
+deploy-all: deploy-postgres deploy-service
+
+deploy-service: deploy-role
+	kubectl apply -f deploy/bm-inventory.yaml
+
+deploy-role:
+	kubectl apply -f deploy/roles/role_binding.yaml
+
+deploy-postgres:
+	kubectl apply -f deploy/postgres/postgres-configmap.yaml
+	kubectl apply -f deploy/postgres/postgres-storage.yaml
+	kubectl apply -f deploy/postgres/postgres-deployment.yaml
+
+subsystem-run:
+	INVENTORY=$(shell minikube service bm-inventory --url| sed 's/http:\/\///g') \
+		DB_HOST=$(shell minikube service postgres --url| sed 's/http:\/\///g' | cut -d ":" -f 1) \
+		DB_PORT=$(shell minikube service postgres --url| sed 's/http:\/\///g' | cut -d ":" -f 2) \
+		go test ./subsystem/... -count=1
+
+.PHONY: subsystem
+subsystem: deploy-all subsystem-run subsystem-clean
+
+subsystem-clean:
+	kubectl get pod -o name | grep create-image | xargs kubectl delete
+
+agent:
+	export DBHOST=$(shell minikube service postgres --url| sed 's/http:\/\///g')
+	echo $(DBHOST)
--- subsystem/subsystem_suite_test.go
+++ subsystem/subsystem_suite_test.go
@@ -1,12 +1,15 @@
 package subsystem
 
 import (
+	"fmt"
+	"log"
 	"net/url"
 	"testing"
 
 	"github.com/filanov/bm-inventory/client"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/kelseyhightower/envconfig"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/sirupsen/logrus"
@@ -15,17 +18,29 @@ import (
 var db *gorm.DB
 var bmclient *client.BMInventory
 
+var Options struct {
+	DBHost        string `envconfig:"DB_HOST"`
+	DBPort        string `envconfig:"DB_PORT"`
+	InventoryHost string `envconfig:"INVENTORY"`
+}
+
 func init() {
 	var err error
+	err = envconfig.Process("subsystem", &Options)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
 	bmclient = client.New(client.Config{
 		URL: &url.URL{
 			Scheme: client.DefaultSchemes[0],
-			Host:   "192.168.39.74:32562",
+			Host:   Options.InventoryHost,
 			Path:   client.DefaultBasePath,
 		},
 	})
 
-	db, err = gorm.Open("postgres", "host=192.168.39.74 port=30935 user=postgresadmin dbname=postgresdb password=admin123 sslmode=disable")
+	db, err = gorm.Open("postgres", fmt.Sprintf("host=%s port=%s user=postgresadmin dbname=postgresdb password=admin123 sslmode=disable",
+		Options.DBHost, Options.DBPort))
 	if err != nil {
 		logrus.Fatal("Fail to connect to DB, ", err)
 	}
