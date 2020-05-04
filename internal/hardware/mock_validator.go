ed2bfc6186b63e129b1a1c94108d57af7b248379
Michael Filanov <mfilanov@redhat.com>
2020-05-04 21:29:31 +0300

    Revert "autogenerated mocks: Choose boot device for the installation"
    This reverts commit dd569e993fb803a7a0233f07c6fe8b0790e7e4ad.

--- internal/hardware/mock_validator.go
+++ internal/hardware/mock_validator.go
@@ -42,13 +42,7 @@ func (m *MockValidator) IsSufficient(host *models.Host) (*IsSufficientReply, err
 	return ret0, ret1
 }
 
-// IsSufficient indicates an expected call of IsSufficient.
-func (mr *MockValidatorMockRecorder) IsSufficient(host interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "IsSufficient", reflect.TypeOf((*MockValidator)(nil).IsSufficient), host)
-}
-
-// GetHostValidDisks mocks base method.
+// IsSufficient mocks base method.
 func (m *MockValidator) GetHostValidDisks(host *models.Host) ([]*models.BlockDevice, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "GetHostValidDisks", host)
@@ -57,8 +51,15 @@ func (m *MockValidator) GetHostValidDisks(host *models.Host) ([]*models.BlockDev
 	return ret0, ret1
 }
 
+// IsSufficient indicates an expected call of IsSufficient.
+func (mr *MockValidatorMockRecorder) IsSufficient(host interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "IsSufficient", reflect.TypeOf((*MockValidator)(nil).IsSufficient), host)
+}
+
 // GetHostValidDisks indicates an expected call of GetHostValidDisks.
 func (mr *MockValidatorMockRecorder) GetHostValidDisks(host interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetHostValidDisks", reflect.TypeOf((*MockValidator)(nil).GetHostValidDisks), host)
 }
+
