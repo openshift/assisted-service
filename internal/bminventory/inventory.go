filanov/bm-inventory

MGMT-703 - move install logic to getNextStep

internal/bminventory/inventory.go
@@ -49,10 +49,6 @@ const (
 	ResourceKindCluster = "cluster"
 )
 
-const (
-	bootstrap = "bootstrap"
-)
-
 type Config struct {
 	ImageBuilder        string `envconfig:"IMAGE_BUILDER" default:"quay.io/oscohen/installer-image-build"`
 	ImageBuilderCmd     string `envconfig:"IMAGE_BUILDER_CMD" default:"echo hello"`
@@ -64,7 +60,6 @@ type Config struct {
 	S3Bucket            string `envconfig:"S3_BUCKET" default:"test"`
 	AwsAccessKeyID      string `envconfig:"AWS_ACCESS_KEY_ID" default:"accessKey1"`
 	AwsSecretAccessKey  string `envconfig:"AWS_SECRET_ACCESS_KEY" default:"verySecretKey1"`
-	InstallerImage      string `envconfig:"INSTALLER_IMAGE" default:"quay.io/ocpmetal/assisted-installer:stable"`
 }
 
 const ignitionConfigFormat = `{
@@ -375,22 +370,29 @@ func (b *bareMetalInventory) InstallCluster(ctx context.Context, params inventor
 		return inventory.NewInstallClusterConflict()
 	}
 
-	// Temporary hack - use debug API for setting the executing install command:
+	// set one of the master nodes as bootstrap
 	masterNodesIds, err := b.clusterApi.GetMasterNodesIds(ctx, &cluster, tx)
 	if err != nil {
 		tx.Rollback()
 		log.WithError(err).Errorf("failed to get cluster %s master node id's", cluster.ID)
 		return inventory.NewInstallClusterInternalServerError()
 	}
-	if err = b.addInstallCommand(ctx, masterNodesIds, log, params, cluster); err != nil {
-		tx.Rollback()
-		log.WithError(err).Errorf("failed to add install command to cluster <%s>", params.ClusterID)
-		return inventory.NewInstallClusterInternalServerError()
+	bootstrapId := masterNodesIds[len(masterNodesIds)-1]
+	log.Debugf("Bootstrap ID is %s", bootstrapId)
+	for i := range cluster.Hosts {
+		if cluster.Hosts[i].ID.String() == bootstrapId.String() {
+			err = b.hostApi.UpdateBootstrap(ctx, cluster.Hosts[i], true)
+			if err != nil {
+				tx.Rollback()
+				log.WithError(err).Errorf("failed to update bootstrap host for cluster %s", params.ClusterID)
+				return inventory.NewInstallClusterInternalServerError()
+			}
+		}
 	}
 
 	// move hosts states to installing
 	for i := range cluster.Hosts {
-		if _, err = b.hostApi.Install(ctx, cluster.Hosts[i], tx); err != nil {
+		if _, err := b.hostApi.Install(ctx, cluster.Hosts[i], tx); err != nil {
 			log.WithError(err).Errorf("failed to install hosts <%s> in cluster: %s",
 				cluster.Hosts[i].ID.String(), cluster.ID.String())
 			tx.Rollback()
@@ -429,54 +431,6 @@ func (b *bareMetalInventory) generateClusterInstallConfig(ctx context.Context, c
 	if err := b.job.Monitor(ctx, jobName, defaultJobNamespace); err != nil {
 		log.WithError(err).Errorf("Generating kubeconfig files %s failed for cluster %s", jobName, cluster.ID)
 		return inventory.NewInstallClusterInternalServerError()
-	}
-	return nil
-}
-
-func (b *bareMetalInventory) addInstallCommand(ctx context.Context, masterNodesIds []*strfmt.UUID,
-	log logrus.FieldLogger, params inventory.InstallClusterParams, cluster models.Cluster) error {
-	// set one of the master nodes as bootstrap
-	bootstrapId := masterNodesIds[len(masterNodesIds)-1]
-	log.Debugf("Bootstrap ID is %s", bootstrapId)
-
-	const cmdTmpl = `sudo podman run -v /dev:/dev:rw -v /opt:/opt:rw --privileged --pid=host  {{.INSTALLER}} --role {{.ROLE}}  --cluster-id {{.CLUSTER_ID}}  --host {{.HOST}} --port {{.PORT}} --boot-device {{.BOOT_DEVICE}} --host-id {{.HOST_ID}}`
-
-	t, err := template.New("cmd").Parse(cmdTmpl)
-	if err != nil {
-		return err
-	}
-
-	data := map[string]string{
-		"HOST":        b.InventoryURL,
-		"PORT":        b.InventoryPort,
-		"CLUSTER_ID":  string(params.ClusterID),
-		"ROLE":        "",
-		"INSTALLER":   b.Config.InstallerImage,
-		"BOOT_DEVICE": "",
-	}
-	for i := range cluster.Hosts {
-		role := cluster.Hosts[i].Role
-		if cluster.Hosts[i].ID == bootstrapId {
-			role = bootstrap
-		}
-		data["ROLE"] = role
-		disks, err := b.hostApi.GetHostValidDisks(cluster.Hosts[i])
-		if err != nil {
-			log.Errorf("Failed to get valid disks on host with id %s", cluster.Hosts[i].ID)
-			return err
-		}
-		data["BOOT_DEVICE"] = fmt.Sprintf("/dev/%s", disks[0].Name)
-		data["HOST_ID"] = string(*cluster.Hosts[i].ID)
-		buf := &bytes.Buffer{}
-		if err := t.Execute(buf, data); err != nil {
-			return err
-		}
-		command := buf.String()
-		b.SetDebugStep(ctx, inventory.SetDebugStepParams{
-			ClusterID: params.ClusterID,
-			HostID:    *cluster.Hosts[i].ID,
-			Step:      &models.DebugStep{Command: &command},
-		})
 	}
 	return nil
 }
@@ -660,7 +614,7 @@ func (b *bareMetalInventory) GetNextSteps(ctx context.Context, params inventory.
 	var err error
 	steps, err = b.hostApi.GetNextSteps(ctx, &host)
 	if err != nil {
-		log.WithError(err).Errorf("failed to get steps for host %s cluster %s", params.ClusterID, params.HostID)
+		log.WithError(err).Errorf("failed to get steps for host %s cluster %s", params.HostID, params.ClusterID)
 	}
 
 	b.debugCmdMux.Lock()

internal/host/host.go
@@ -35,14 +35,21 @@ type SpecificHardwareParams interface {
 }
 
 const (
-	HostStatusDiscovering  = "discovering"
-	HostStatusKnown        = "known"
-	HostStatusDisconnected = "disconnected"
-	HostStatusInsufficient = "insufficient"
-	HostStatusDisabled     = "disabled"
-	HostStatusInstalling   = "installing"
-	HostStatusInstalled    = "installed"
-	HostStatusError        = "error"
+	HostStatusDiscovering          = "discovering"
+	HostStatusKnown                = "known"
+	HostStatusDisconnected         = "disconnected"
+	HostStatusInsufficient         = "insufficient"
+	HostStatusDisabled             = "disabled"
+	HostStatusInstalling           = "installing"
+	HostStatusInstallingInProgress = "installing-in-progress"
+	HostStatusInstalled            = "installed"
+	HostStatusError                = "error"
+)
+
+const (
+	RoleMaster    = "master"
+	RoleBootstrap = "bootstrap"
+	RoleWorker    = "worker"
 )
 
 type API interface {
@@ -50,6 +57,7 @@ type API interface {
 	InstructionApi
 	SpecificHardwareParams
 	UpdateInstallProgress(ctx context.Context, h *models.Host, progress string) error
+	UpdateBootstrap(ctx context.Context, h *models.Host, isbootstrap bool) error
 }
 
 type Manager struct {
@@ -67,7 +75,7 @@ type Manager struct {
 	hwValidator    hardware.Validator
 }
 
-func NewManager(log logrus.FieldLogger, db *gorm.DB, hwValidator hardware.Validator) *Manager {
+func NewManager(log logrus.FieldLogger, db *gorm.DB, hwValidator hardware.Validator, instructionApi InstructionApi) *Manager {
 	return &Manager{
 		log:            log,
 		db:             db,
@@ -79,7 +87,7 @@ func NewManager(log logrus.FieldLogger, db *gorm.DB, hwValidator hardware.Valida
 		installing:     NewInstallingState(log, db),
 		installed:      NewInstalledState(log, db),
 		error:          NewErrorState(log, db),
-		instructionApi: NewInstructionManager(log, db),
+		instructionApi: instructionApi,
 		hwValidator:    hwValidator,
 	}
 }
@@ -172,7 +180,7 @@ func (m *Manager) GetHostValidDisks(host *models.Host) ([]*models.BlockDevice, e
 }
 
 func (m *Manager) UpdateInstallProgress(ctx context.Context, h *models.Host, progress string) error {
-	if swag.StringValue(h.Status) != HostStatusInstalling {
+	if swag.StringValue(h.Status) != HostStatusInstalling && swag.StringValue(h.Status) != HostStatusInstallingInProgress {
 		return fmt.Errorf("can't set progress to host in status <%s>", swag.StringValue(h.Status))
 	}
 
@@ -184,6 +192,16 @@ func (m *Manager) UpdateInstallProgress(ctx context.Context, h *models.Host, pro
 	}
 
 	_, err := updateStateWithParams(logutil.FromContext(ctx, m.log),
-		HostStatusInstalling, progress, h, m.db)
+		HostStatusInstallingInProgress, progress, h, m.db)
 	return err
+}
+
+func (m *Manager) UpdateBootstrap(ctx context.Context, h *models.Host, isbootstrap bool) error {
+	if h.Bootstrap != isbootstrap {
+		err := m.db.Model(h).Update("bootstrap", isbootstrap).Error
+		if err != nil {
+			return fmt.Errorf("can't set bootstrap to host <%s>", h.ID)
+		}
+	}
+	return nil
 }

internal/host/mock_host_api.go
@@ -349,3 +349,17 @@ func (mr *MockAPIMockRecorder) UpdateInstallProgress(ctx, h, progress interface{
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "UpdateInstallProgress", reflect.TypeOf((*MockAPI)(nil).UpdateInstallProgress), ctx, h, progress)
 }
+
+// UpdateBootstrap mocks base method.
+func (m *MockAPI) UpdateBootstrap(ctx context.Context, h *models.Host, isbootstrap bool) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "UpdateBootstrap", ctx, h, isbootstrap)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// UpdateBootstrap indicates an expected call of UpdateBootstrap.
+func (mr *MockAPIMockRecorder) UpdateBootstrap(ctx, h, isbootstrap interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "UpdateBootstrap", reflect.TypeOf((*MockAPI)(nil).UpdateBootstrap), ctx, h, isbootstrap)
+}

openshift/assisted-service

internal/bminventory/inventory.go
@@ -49,10 +49,6 @@ const (
 	ResourceKindCluster = "cluster"
 )
 
-const (
-	bootstrap = "bootstrap"
-)
-
 type Config struct {
 	ImageBuilder        string `envconfig:"IMAGE_BUILDER" default:"quay.io/oscohen/installer-image-build"`
 	ImageBuilderCmd     string `envconfig:"IMAGE_BUILDER_CMD" default:"echo hello"`
@@ -64,7 +60,6 @@ type Config struct {
 	S3Bucket            string `envconfig:"S3_BUCKET" default:"test"`
 	AwsAccessKeyID      string `envconfig:"AWS_ACCESS_KEY_ID" default:"accessKey1"`
 	AwsSecretAccessKey  string `envconfig:"AWS_SECRET_ACCESS_KEY" default:"verySecretKey1"`
-	InstallerImage      string `envconfig:"INSTALLER_IMAGE" default:"quay.io/ocpmetal/assisted-installer:stable"`
 }
 
 const ignitionConfigFormat = `{
@@ -375,22 +370,15 @@ func (b *bareMetalInventory) InstallCluster(ctx context.Context, params inventor
 		return inventory.NewInstallClusterConflict()
 	}
 
-	// Temporary hack - use debug API for setting the executing install command:
-	masterNodesIds, err := b.clusterApi.GetMasterNodesIds(ctx, &cluster, tx)
-	if err != nil {
+	// set one of the master nodes as bootstrap
+	if err := b.setBootstrapHost(ctx, cluster, tx); err != nil {
 		tx.Rollback()
-		log.WithError(err).Errorf("failed to get cluster %s master node id's", cluster.ID)
-		return inventory.NewInstallClusterInternalServerError()
-	}
-	if err = b.addInstallCommand(ctx, masterNodesIds, log, params, cluster); err != nil {
-		tx.Rollback()
-		log.WithError(err).Errorf("failed to add install command to cluster <%s>", params.ClusterID)
-		return inventory.NewInstallClusterInternalServerError()
+		return err
 	}
 
 	// move hosts states to installing
 	for i := range cluster.Hosts {
-		if _, err = b.hostApi.Install(ctx, cluster.Hosts[i], tx); err != nil {
+		if _, err := b.hostApi.Install(ctx, cluster.Hosts[i], tx); err != nil {
 			log.WithError(err).Errorf("failed to install hosts <%s> in cluster: %s",
 				cluster.Hosts[i].ID.String(), cluster.ID.String())
 			tx.Rollback()
@@ -412,6 +400,28 @@ func (b *bareMetalInventory) InstallCluster(ctx context.Context, params inventor
 	return inventory.NewInstallClusterOK().WithPayload(&cluster)
 }
 
+func (b *bareMetalInventory) setBootstrapHost(ctx context.Context, cluster models.Cluster, db *gorm.DB) middleware.Responder {
+	log := logutil.FromContext(ctx, b.log)
+
+	masterNodesIds, err := b.clusterApi.GetMasterNodesIds(ctx, &cluster, db)
+	if err != nil {
+		log.WithError(err).Errorf("failed to get cluster %s master node id's", cluster.ID)
+		return inventory.NewInstallClusterInternalServerError()
+	}
+	bootstrapId := masterNodesIds[len(masterNodesIds)-1]
+	log.Infof("Bootstrap ID is %s", bootstrapId)
+	for i := range cluster.Hosts {
+		if cluster.Hosts[i].ID.String() == bootstrapId.String() {
+			err = b.hostApi.SetBootstrap(ctx, cluster.Hosts[i], true)
+			if err != nil {
+				log.WithError(err).Errorf("failed to update bootstrap host for cluster %s", cluster.ID)
+				return inventory.NewInstallClusterInternalServerError()
+			}
+		}
+	}
+	return nil
+}
+
 func (b *bareMetalInventory) generateClusterInstallConfig(ctx context.Context, cluster models.Cluster) middleware.Responder {
 	log := logutil.FromContext(ctx, b.log)
 
@@ -429,59 +439,6 @@ func (b *bareMetalInventory) generateClusterInstallConfig(ctx context.Context, c
 	if err := b.job.Monitor(ctx, jobName, defaultJobNamespace); err != nil {
 		log.WithError(err).Errorf("Generating kubeconfig files %s failed for cluster %s", jobName, cluster.ID)
 		return inventory.NewInstallClusterInternalServerError()
-	}
-	return nil
-}
-
-func (b *bareMetalInventory) addInstallCommand(ctx context.Context, masterNodesIds []*strfmt.UUID,
-	log logrus.FieldLogger, params inventory.InstallClusterParams, cluster models.Cluster) error {
-
-	const cmdTmpl = `sudo podman run -v /dev:/dev:rw -v /opt:/opt:rw --privileged --pid=host  {{.INSTALLER}} --role {{.ROLE}}  --cluster-id {{.CLUSTER_ID}}  --host {{.HOST}} --port {{.PORT}} --boot-device {{.BOOT_DEVICE}} --host-id {{.HOST_ID}} --openshift-version {{.OPENSHIFT_VERSION}}`
-
-	// set one of the master nodes as bootstrap
-	bootstrapId := masterNodesIds[len(masterNodesIds)-1]
-	log.Infof("Bootstrap ID is %s", bootstrapId)
-
-	t, err := template.New("cmd").Parse(cmdTmpl)
-	if err != nil {
-		return err
-	}
-
-	data := map[string]string{
-		"HOST":              b.InventoryURL,
-		"PORT":              b.InventoryPort,
-		"CLUSTER_ID":        string(params.ClusterID),
-		"ROLE":              "",
-		"INSTALLER":         b.Config.InstallerImage,
-		"BOOT_DEVICE":       "",
-		"OPENSHIFT_VERSION": cluster.OpenshiftVersion,
-	}
-	for _, h := range cluster.Hosts {
-		role := h.Role
-		if h.ID.String() == bootstrapId.String() {
-			role = bootstrap
-		}
-		data["ROLE"] = role
-		disks, err := b.hostApi.GetHostValidDisks(h)
-		if err != nil {
-			log.Errorf("Failed to get valid disks on host with id %s", h.ID)
-			return err
-		}
-		data["BOOT_DEVICE"] = fmt.Sprintf("/dev/%s", disks[0].Name)
-		data["HOST_ID"] = string(*h.ID)
-		buf := &bytes.Buffer{}
-
-		log.Infof("host installation data : %s", data)
-
-		if err := t.Execute(buf, data); err != nil {
-			return err
-		}
-		command := buf.String()
-		b.SetDebugStep(ctx, inventory.SetDebugStepParams{
-			ClusterID: params.ClusterID,
-			HostID:    *h.ID,
-			Step:      &models.DebugStep{Command: &command},
-		})
 	}
 	return nil
 }
@@ -665,7 +622,7 @@ func (b *bareMetalInventory) GetNextSteps(ctx context.Context, params inventory.
 	var err error
 	steps, err = b.hostApi.GetNextSteps(ctx, &host)
 	if err != nil {
-		log.WithError(err).Errorf("failed to get steps for host %s cluster %s", params.ClusterID, params.HostID)
+		log.WithError(err).Errorf("failed to get steps for host %s cluster %s", params.HostID, params.ClusterID)
 	}
 
 	b.debugCmdMux.Lock()

internal/host/host.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/pkg/errors"
+
 	"github.com/filanov/bm-inventory/internal/hardware"
 	"github.com/filanov/bm-inventory/models"
 	logutil "github.com/filanov/bm-inventory/pkg/log"
@@ -36,14 +38,21 @@ type SpecificHardwareParams interface {
 }
 
 const (
-	HostStatusDiscovering  = "discovering"
-	HostStatusKnown        = "known"
-	HostStatusDisconnected = "disconnected"
-	HostStatusInsufficient = "insufficient"
-	HostStatusDisabled     = "disabled"
-	HostStatusInstalling   = "installing"
-	HostStatusInstalled    = "installed"
-	HostStatusError        = "error"
+	HostStatusDiscovering          = "discovering"
+	HostStatusKnown                = "known"
+	HostStatusDisconnected         = "disconnected"
+	HostStatusInsufficient         = "insufficient"
+	HostStatusDisabled             = "disabled"
+	HostStatusInstalling           = "installing"
+	HostStatusInstallingInProgress = "installing-in-progress"
+	HostStatusInstalled            = "installed"
+	HostStatusError                = "error"
+)
+
+const (
+	RoleMaster    = "master"
+	RoleBootstrap = "bootstrap"
+	RoleWorker    = "worker"
 )
 
 const (
@@ -56,6 +65,7 @@ type API interface {
 	InstructionApi
 	SpecificHardwareParams
 	UpdateInstallProgress(ctx context.Context, h *models.Host, progress string) error
+	SetBootstrap(ctx context.Context, h *models.Host, isbootstrap bool) error
 }
 
 type Manager struct {
@@ -73,7 +83,7 @@ type Manager struct {
 	hwValidator    hardware.Validator
 }
 
-func NewManager(log logrus.FieldLogger, db *gorm.DB, hwValidator hardware.Validator) *Manager {
+func NewManager(log logrus.FieldLogger, db *gorm.DB, hwValidator hardware.Validator, instructionApi InstructionApi) *Manager {
 	return &Manager{
 		log:            log,
 		db:             db,
@@ -85,7 +95,7 @@ func NewManager(log logrus.FieldLogger, db *gorm.DB, hwValidator hardware.Valida
 		installing:     NewInstallingState(log, db),
 		installed:      NewInstalledState(log, db),
 		error:          NewErrorState(log, db),
-		instructionApi: NewInstructionManager(log, db),
+		instructionApi: instructionApi,
 		hwValidator:    hwValidator,
 	}
 }
@@ -178,7 +188,7 @@ func (m *Manager) GetHostValidDisks(host *models.Host) ([]*models.BlockDevice, e
 }
 
 func (m *Manager) UpdateInstallProgress(ctx context.Context, h *models.Host, progress string) error {
-	if swag.StringValue(h.Status) != HostStatusInstalling {
+	if swag.StringValue(h.Status) != HostStatusInstalling && swag.StringValue(h.Status) != HostStatusInstallingInProgress {
 		return fmt.Errorf("can't set progress to host in status <%s>", swag.StringValue(h.Status))
 	}
 
@@ -197,6 +207,16 @@ func (m *Manager) UpdateInstallProgress(ctx context.Context, h *models.Host, pro
 	}
 
 	_, err := updateStateWithParams(logutil.FromContext(ctx, m.log),
-		HostStatusInstalling, progress, h, m.db)
+		HostStatusInstallingInProgress, progress, h, m.db)
 	return err
+}
+
+func (m *Manager) SetBootstrap(ctx context.Context, h *models.Host, isbootstrap bool) error {
+	if h.Bootstrap != isbootstrap {
+		err := m.db.Model(h).Update("bootstrap", isbootstrap).Error
+		if err != nil {
+			return errors.Wrapf(err, "failed to set bootstrap to host %s", h.ID.String())
+		}
+	}
+	return nil
 }

internal/host/mock_host_api.go
@@ -349,3 +349,17 @@ func (mr *MockAPIMockRecorder) UpdateInstallProgress(ctx, h, progress interface{
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "UpdateInstallProgress", reflect.TypeOf((*MockAPI)(nil).UpdateInstallProgress), ctx, h, progress)
 }
+
+// SetBootstrap mocks base method.
+func (m *MockAPI) SetBootstrap(ctx context.Context, h *models.Host, isbootstrap bool) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "SetBootstrap", ctx, h, isbootstrap)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// SetBootstrap indicates an expected call of SetBootstrap.
+func (mr *MockAPIMockRecorder) SetBootstrap(ctx, h, isbootstrap interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SetBootstrap", reflect.TypeOf((*MockAPI)(nil).SetBootstrap), ctx, h, isbootstrap)
+}