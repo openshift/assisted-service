rawagner/bm-inventory

MGMT-747 Cluster status should be updated periodically to to reflect changed in host states

cmd/main.go
@@ -4,16 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"time"
 
-	"github.com/filanov/bm-inventory/internal/cluster"
-
-	"github.com/filanov/bm-inventory/internal/bminventory"
-	"github.com/filanov/bm-inventory/internal/hardware"
-	"github.com/filanov/bm-inventory/internal/host"
-	"github.com/filanov/bm-inventory/models"
-	"github.com/filanov/bm-inventory/pkg/job"
-	"github.com/filanov/bm-inventory/pkg/requestid"
-	"github.com/filanov/bm-inventory/restapi"
 	"github.com/go-openapi/strfmt"
 	"github.com/go-openapi/swag"
 	"github.com/jinzhu/gorm"
@@ -24,6 +16,16 @@ import (
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
+
+	"github.com/filanov/bm-inventory/internal/bminventory"
+	"github.com/filanov/bm-inventory/internal/cluster"
+	"github.com/filanov/bm-inventory/internal/hardware"
+	"github.com/filanov/bm-inventory/internal/host"
+	"github.com/filanov/bm-inventory/models"
+	"github.com/filanov/bm-inventory/pkg/job"
+	"github.com/filanov/bm-inventory/pkg/requestid"
+	"github.com/filanov/bm-inventory/pkg/thread"
+	"github.com/filanov/bm-inventory/restapi"
 )
 
 func init() {
@@ -31,12 +33,13 @@ func init() {
 }
 
 var Options struct {
-	BMConfig          bminventory.Config
-	DBHost            string `envconfig:"DB_HOST" default:"mariadb"`
-	DBPort            string `envconfig:"DB_PORT" default:"3306"`
-	HWValidatorConfig hardware.ValidatorCfg
-	JobConfig         job.Config
-	InstructionConfig host.InstructionConfig
+	BMConfig               bminventory.Config
+	DBHost                 string `envconfig:"DB_HOST" default:"mariadb"`
+	DBPort                 string `envconfig:"DB_PORT" default:"3306"`
+	HWValidatorConfig      hardware.ValidatorCfg
+	JobConfig              job.Config
+	InstructionConfig      host.InstructionConfig
+	ClusterMonitorTimeLoop string `envconfig:"CLUSTER_MONITOR_TIME_LOOP" default:"500"` // Microsecond
 }
 
 func main() {
@@ -46,7 +49,7 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	port := flag.String("port", "8090", "define port that the service will listen to")
+	port := flag.String("port", "8090", "define port that the service will listen to") //nolint:staticcheck
 	flag.Parse()
 
 	log.Println("Starting bm service")
@@ -65,7 +68,7 @@ func main() {
 		log.Fatal("Failed to add K8S scheme", err)
 	}
 
-	kclient, err := client.New(config.GetConfigOrDie(), client.Options{Scheme: scheme})
+	kclient, err := client.New(config.GetConfigOrDie(), client.Options{Scheme: scheme}) //nolint:staticcheck
 	if err != nil {
 		log.Fatal("failed to create client:", err)
 	}
@@ -74,10 +77,15 @@ func main() {
 		log.Fatal("failed to auto migrate, ", err)
 	}
 
-	clusterApi := cluster.NewManager(log.WithField("pkg", "cluster-state"), db)
 	hwValidator := hardware.NewValidator(Options.HWValidatorConfig)
 	instructionApi := host.NewInstructionManager(log, db, hwValidator, Options.InstructionConfig)
 	hostApi := host.NewManager(log.WithField("pkg", "host-state"), db, hwValidator, instructionApi)
+	clusterApi := cluster.NewManager(log.WithField("pkg", "cluster-monitor"), db)
+
+	clusterMonitor := thread.New(log, "State Monitor", time.Duration(500)*time.Microsecond, clusterApi.ClusterMonitoring)
+	clusterMonitor.Start()
+	defer clusterMonitor.Stop()
+
 	jobApi := job.New(log.WithField("pkg", "k8s-job-wrapper"), kclient, Options.JobConfig)
 	bm := bminventory.NewBareMetalInventory(db, log.WithField("pkg", "Inventory"), hostApi, clusterApi, Options.BMConfig, jobApi)
 	h, err := restapi.Handler(restapi.Config{

internal/cluster/common.go
@@ -61,17 +61,18 @@ func getKnownMastersNodesIds(c *models.Cluster, db *gorm.DB) ([]*strfmt.UUID, er
 	return masterNodesIds, nil
 }
 
-func isClusterReady(c *models.Cluster, db *gorm.DB, log logrus.FieldLogger) (bool, error) {
-	masterNodesIds, err := getKnownMastersNodesIds(c, db)
-	if err != nil {
-		return false, errors.Errorf("unable to determine cluster %s hosts state ", c.ID)
+func mapMasterHostsByRole(c *models.Cluster) map[string][]*models.Host {
+	hostMap := make(map[string][]*models.Host)
+
+	for _, host := range c.Hosts {
+		if host.Role != "master" {
+			continue
+		}
+		if _, ok := hostMap[swag.StringValue(host.Status)]; ok {
+			hostMap[swag.StringValue(host.Status)] = append(hostMap[swag.StringValue(host.Status)], host)
+		} else {
+			hostMap[swag.StringValue(host.Status)] = []*models.Host{host}
+		}
 	}
-	minimumKnownMasterNodes := 3
-	if len(masterNodesIds) < minimumKnownMasterNodes {
-		log.Infof("cluster %s has %d known master hosts which is less then the %d minimum needed for cluster installation",
-			c.ID, len(masterNodesIds), minimumKnownMasterNodes)
-		return false, nil
-	} else {
-		return true, nil
-	}
+	return hostMap
 }

internal/cluster/installing.go
@@ -3,10 +3,13 @@ package cluster
 import (
 	context "context"
 
+	host2 "github.com/filanov/bm-inventory/internal/host"
+
+	logutil "github.com/filanov/bm-inventory/pkg/log"
+
 	"github.com/sirupsen/logrus"
 
 	"github.com/filanov/bm-inventory/models"
-	"github.com/go-openapi/swag"
 	"github.com/jinzhu/gorm"
 	"github.com/pkg/errors"
 )
@@ -23,13 +26,53 @@ type installingState baseState
 var _ StateAPI = (*Manager)(nil)
 
 func (i *installingState) RefreshStatus(ctx context.Context, c *models.Cluster, db *gorm.DB) (*UpdateReply, error) {
+	log := logutil.FromContext(ctx, i.log)
+	installationState, err := i.getClusterInstallationState(ctx, c)
+	if err != nil {
+		log.WithError(err).Errorf("cluster %s couldn't determine cluster installation state", c.ID)
+	}
+	switch installationState {
+	case clusterStatusInstalled:
+		return updateState(clusterStatusInstalled, c, i.db, log)
+	case clusterStatusError:
+		return updateState(clusterStatusError, c, i.db, log)
+	case clusterStatusInstalling:
+		return &UpdateReply{
+			State:     clusterStatusInstalling,
+			IsChanged: false,
+		}, nil
+	}
 	return &UpdateReply{
 		State:     clusterStatusInstalling,
 		IsChanged: false,
-	}, nil
+	}, errors.Errorf("cluster % state transaction is not clear, installation state: %s ", c.ID, installationState)
 }
 
-func (i *installingState) Install(ctx context.Context, c *models.Cluster) (*UpdateReply, error) {
-	return nil, errors.Errorf("unable to install cluster <%s> in <%s> status",
-		c.ID, swag.StringValue(c.Status))
+func (i *installingState) getClusterInstallationState(ctx context.Context, c *models.Cluster) (string, error) {
+	log := logutil.FromContext(ctx, i.log)
+
+	if err := i.db.Preload("Hosts").First(&c, "id = ?", c.ID).Error; err != nil {
+		return "", errors.Errorf("cluster %s not found", c.ID)
+	}
+
+	mappedMastersByRole := mapMasterHostsByRole(c)
+
+	// Cluster is in installed
+	mastersInInstalled, ok := mappedMastersByRole[host2.HostStatusInstalled]
+	if ok && len(mastersInInstalled) >= minHostsNeededForInstallation {
+		log.Infof("Cluster %s has at least %d installed hosts, cluster is installed.", c.ID, len(mastersInInstalled))
+		return host2.HostStatusInstalled, nil
+	}
+
+	// Cluster is installing
+	mastersInInstalling, ok := mappedMastersByRole[host2.HostStatusInstalling]
+	if ok && len(mastersInInstalling) > 0 &&
+		(len(mastersInInstalling)+len(mastersInInstalled)) >= minHostsNeededForInstallation {
+		return host2.HostStatusInstalling, nil
+	}
+
+	// Cluster is in error
+	mastersInError := mappedMastersByRole[host2.HostStatusError]
+	log.Warningf("Cluster %s has %d hosts in error.", c.ID, len(mastersInError))
+	return host2.HostStatusError, nil
 }

internal/cluster/insufficient.go
@@ -5,8 +5,10 @@ import (
 
 	"github.com/sirupsen/logrus"
 
+	host2 "github.com/filanov/bm-inventory/internal/host"
 	"github.com/filanov/bm-inventory/models"
-	"github.com/go-openapi/swag"
+	logutil "github.com/filanov/bm-inventory/pkg/log"
+
 	"github.com/jinzhu/gorm"
 	"github.com/pkg/errors"
 )
@@ -22,23 +24,24 @@ type insufficientState baseState
 
 func (i *insufficientState) RefreshStatus(ctx context.Context, c *models.Cluster, db *gorm.DB) (*UpdateReply, error) {
 
-	clusterIsReady, err := isClusterReady(c, db, i.log)
-	if err != nil {
-		return nil, errors.Errorf("unable to determine cluster %s hosts state ", c.ID)
-	}
+	log := logutil.FromContext(ctx, i.log)
 
-	if clusterIsReady {
-		return updateState(clusterStatusReady, c, db, i.log)
-	} else {
-		i.log.Infof("Cluster %s does not have sufficient resources to be installed.", c.ID)
+	if err := db.Preload("Hosts").First(&c, "id = ?", c.ID).Error; err != nil {
 		return &UpdateReply{
 			State:     clusterStatusInsufficient,
-			IsChanged: false,
-		}, nil
+			IsChanged: false}, errors.Errorf("cluster %s not found", c.ID)
 	}
-}
+	mappedMastersByRole := mapMasterHostsByRole(c)
 
-func (i *insufficientState) Install(ctx context.Context, c *models.Cluster) (*UpdateReply, error) {
-	return nil, errors.Errorf("unable to install cluster <%s> in <%s> status",
-		c.ID, swag.StringValue(c.Status))
+	// Cluster is ready
+	mastersInKnown, ok := mappedMastersByRole[host2.HostStatusKnown]
+	if ok && len(mastersInKnown) >= minHostsNeededForInstallation {
+		log.Infof("Cluster %s has at least %d known master hosts, cluster is ready.", c.ID, minHostsNeededForInstallation)
+		return updateState(clusterStatusReady, c, db, log)
+
+		//cluster is still insufficient
+	} else {
+		return &UpdateReply{State: clusterStatusInsufficient,
+			IsChanged: false}, nil
+	}
 }

internal/cluster/ready.go
@@ -5,7 +5,10 @@ import (
 
 	"github.com/sirupsen/logrus"
 
+	host2 "github.com/filanov/bm-inventory/internal/host"
 	"github.com/filanov/bm-inventory/models"
+	logutil "github.com/filanov/bm-inventory/pkg/log"
+
 	"github.com/jinzhu/gorm"
 	"github.com/pkg/errors"
 )
@@ -22,23 +25,24 @@ type readyState baseState
 var _ StateAPI = (*Manager)(nil)
 
 func (r *readyState) RefreshStatus(ctx context.Context, c *models.Cluster, db *gorm.DB) (*UpdateReply, error) {
-	clusterIsReady, err := isClusterReady(c, db, r.log)
-	if err != nil {
-		return nil, errors.Errorf("unable to determine cluster %s hosts state ", c.ID)
-	}
+	log := logutil.FromContext(ctx, r.log)
 
-	if clusterIsReady {
-
+	if err := db.Preload("Hosts").First(&c, "id = ?", c.ID).Error; err != nil {
 		return &UpdateReply{
-			State:     clusterStatusReady,
-			IsChanged: false,
-		}, nil
-	} else {
-		return updateState(clusterStatusInsufficient, c, db, r.log)
-
+			State:     clusterStatusInsufficient,
+			IsChanged: false}, errors.Errorf("cluster %s not found", c.ID)
 	}
-}
+	mappedMastersByRole := mapMasterHostsByRole(c)
 
-func (r *readyState) Install(ctx context.Context, c *models.Cluster) (*UpdateReply, error) {
-	return updateState(clusterStatusInstalling, c, r.db, r.log)
+	// Cluster is insufficient
+	mastersInKnown := mappedMastersByRole[host2.HostStatusKnown]
+	if len(mastersInKnown) < minHostsNeededForInstallation {
+		log.Infof("Cluster %s dos not have at least %d known master hosts, cluster is insufficient.", c.ID, minHostsNeededForInstallation)
+		return updateState(clusterStatusInsufficient, c, db, log)
+
+		//cluster is still ready
+	} else {
+		return &UpdateReply{State: clusterStatusReady,
+			IsChanged: false}, nil
+	}
 }

openshift/assisted-service

cmd/main.go
@@ -4,16 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"time"
 
-	"github.com/filanov/bm-inventory/internal/cluster"
-
-	"github.com/filanov/bm-inventory/internal/bminventory"
-	"github.com/filanov/bm-inventory/internal/hardware"
-	"github.com/filanov/bm-inventory/internal/host"
-	"github.com/filanov/bm-inventory/models"
-	"github.com/filanov/bm-inventory/pkg/job"
-	"github.com/filanov/bm-inventory/pkg/requestid"
-	"github.com/filanov/bm-inventory/restapi"
 	"github.com/go-openapi/strfmt"
 	"github.com/go-openapi/swag"
 	"github.com/jinzhu/gorm"
@@ -24,6 +16,16 @@ import (
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
+
+	"github.com/filanov/bm-inventory/internal/bminventory"
+	"github.com/filanov/bm-inventory/internal/cluster"
+	"github.com/filanov/bm-inventory/internal/hardware"
+	"github.com/filanov/bm-inventory/internal/host"
+	"github.com/filanov/bm-inventory/models"
+	"github.com/filanov/bm-inventory/pkg/job"
+	"github.com/filanov/bm-inventory/pkg/requestid"
+	"github.com/filanov/bm-inventory/pkg/thread"
+	"github.com/filanov/bm-inventory/restapi"
 )
 
 func init() {
@@ -31,12 +33,13 @@ func init() {
 }
 
 var Options struct {
-	BMConfig          bminventory.Config
-	DBHost            string `envconfig:"DB_HOST" default:"mariadb"`
-	DBPort            string `envconfig:"DB_PORT" default:"3306"`
-	HWValidatorConfig hardware.ValidatorCfg
-	JobConfig         job.Config
-	InstructionConfig host.InstructionConfig
+	BMConfig                    bminventory.Config
+	DBHost                      string `envconfig:"DB_HOST" default:"mariadb"`
+	DBPort                      string `envconfig:"DB_PORT" default:"3306"`
+	HWValidatorConfig           hardware.ValidatorCfg
+	JobConfig                   job.Config
+	InstructionConfig           host.InstructionConfig
+	clusterStateMonitorInterval time.Duration `envconfig:"CLUSTER_MONITOR_INTERVAL" default:"10s"`
 }
 
 func main() {
@@ -74,10 +77,16 @@ func main() {
 		log.Fatal("failed to auto migrate, ", err)
 	}
 
-	clusterApi := cluster.NewManager(log.WithField("pkg", "cluster-state"), db)
 	hwValidator := hardware.NewValidator(Options.HWValidatorConfig)
 	instructionApi := host.NewInstructionManager(log, db, hwValidator, Options.InstructionConfig)
 	hostApi := host.NewManager(log.WithField("pkg", "host-state"), db, hwValidator, instructionApi)
+	clusterApi := cluster.NewManager(log.WithField("pkg", "cluster-state"), db)
+
+	clusterStateMonitor := thread.New(
+		log.WithField("pkg", "cluster-monitor"), "State Monitor", Options.clusterStateMonitorInterval, clusterApi.ClusterMonitoring)
+	clusterStateMonitor.Start()
+	defer clusterStateMonitor.Stop()
+
 	jobApi := job.New(log.WithField("pkg", "k8s-job-wrapper"), kclient, Options.JobConfig)
 	bm := bminventory.NewBareMetalInventory(db, log.WithField("pkg", "Inventory"), hostApi, clusterApi, Options.BMConfig, jobApi)
 	h, err := restapi.Handler(restapi.Config{

internal/cluster/common.go
@@ -61,17 +61,18 @@ func getKnownMastersNodesIds(c *models.Cluster, db *gorm.DB) ([]*strfmt.UUID, er
 	return masterNodesIds, nil
 }
 
-func isClusterReady(c *models.Cluster, db *gorm.DB, log logrus.FieldLogger) (bool, error) {
-	masterNodesIds, err := getKnownMastersNodesIds(c, db)
-	if err != nil {
-		return false, errors.Errorf("unable to determine cluster %s hosts state ", c.ID)
+func mapMasterHostsByStatus(c *models.Cluster) map[string][]*models.Host {
+	hostMap := make(map[string][]*models.Host)
+
+	for _, host := range c.Hosts {
+		if host.Role != "master" {
+			continue
+		}
+		if _, ok := hostMap[swag.StringValue(host.Status)]; ok {
+			hostMap[swag.StringValue(host.Status)] = append(hostMap[swag.StringValue(host.Status)], host)
+		} else {
+			hostMap[swag.StringValue(host.Status)] = []*models.Host{host}
+		}
 	}
-	minimumKnownMasterNodes := 3
-	if len(masterNodesIds) < minimumKnownMasterNodes {
-		log.Infof("cluster %s has %d known master hosts which is less then the %d minimum needed for cluster installation",
-			c.ID, len(masterNodesIds), minimumKnownMasterNodes)
-		return false, nil
-	} else {
-		return true, nil
-	}
+	return hostMap
 }

internal/cluster/installing.go
@@ -3,10 +3,13 @@ package cluster
 import (
 	context "context"
 
+	host2 "github.com/filanov/bm-inventory/internal/host"
+
+	logutil "github.com/filanov/bm-inventory/pkg/log"
+
 	"github.com/sirupsen/logrus"
 
 	"github.com/filanov/bm-inventory/models"
-	"github.com/go-openapi/swag"
 	"github.com/jinzhu/gorm"
 	"github.com/pkg/errors"
 )
@@ -23,13 +26,51 @@ type installingState baseState
 var _ StateAPI = (*Manager)(nil)
 
 func (i *installingState) RefreshStatus(ctx context.Context, c *models.Cluster, db *gorm.DB) (*UpdateReply, error) {
-	return &UpdateReply{
-		State:     clusterStatusInstalling,
-		IsChanged: false,
-	}, nil
+	log := logutil.FromContext(ctx, i.log)
+	installationState, err := i.getClusterInstallationState(ctx, c)
+	if err != nil {
+		return nil, errors.Errorf("couldn't determine cluster %s installation state", c.ID)
+	}
+
+	switch installationState {
+	case clusterStatusInstalled:
+		return updateState(clusterStatusInstalled, c, i.db, log)
+	case clusterStatusError:
+		return updateState(clusterStatusError, c, i.db, log)
+	case clusterStatusInstalling:
+		return &UpdateReply{
+			State:     clusterStatusInstalling,
+			IsChanged: false,
+		}, nil
+	}
+	return nil, errors.Errorf("cluster % state transaction is not clear, installation state: %s ", c.ID, installationState)
 }
 
-func (i *installingState) Install(ctx context.Context, c *models.Cluster) (*UpdateReply, error) {
-	return nil, errors.Errorf("unable to install cluster <%s> in <%s> status",
-		c.ID, swag.StringValue(c.Status))
+func (i *installingState) getClusterInstallationState(ctx context.Context, c *models.Cluster) (string, error) {
+	log := logutil.FromContext(ctx, i.log)
+
+	if err := i.db.Preload("Hosts").First(&c, "id = ?", c.ID).Error; err != nil {
+		return "", errors.Errorf("cluster %s not found", c.ID)
+	}
+
+	mappedMastersByRole := mapMasterHostsByStatus(c)
+
+	// Cluster is in installed
+	mastersInInstalled, ok := mappedMastersByRole[host2.HostStatusInstalled]
+	if ok && len(mastersInInstalled) >= minHostsNeededForInstallation {
+		log.Infof("Cluster %s has at least %d installed hosts, cluster is installed.", c.ID, len(mastersInInstalled))
+		return host2.HostStatusInstalled, nil
+	}
+
+	// Cluster is installing
+	mastersInInstalling, ok := mappedMastersByRole[host2.HostStatusInstalling]
+	if ok && len(mastersInInstalling) > 0 &&
+		(len(mastersInInstalling)+len(mastersInInstalled)) >= minHostsNeededForInstallation {
+		return host2.HostStatusInstalling, nil
+	}
+
+	// Cluster is in error
+	mastersInError := mappedMastersByRole[host2.HostStatusError]
+	log.Warningf("Cluster %s has %d hosts in error.", c.ID, len(mastersInError))
+	return host2.HostStatusError, nil
 }

internal/cluster/insufficient.go
@@ -5,8 +5,10 @@ import (
 
 	"github.com/sirupsen/logrus"
 
+	host2 "github.com/filanov/bm-inventory/internal/host"
 	"github.com/filanov/bm-inventory/models"
-	"github.com/go-openapi/swag"
+	logutil "github.com/filanov/bm-inventory/pkg/log"
+
 	"github.com/jinzhu/gorm"
 	"github.com/pkg/errors"
 )
@@ -22,23 +24,24 @@ type insufficientState baseState
 
 func (i *insufficientState) RefreshStatus(ctx context.Context, c *models.Cluster, db *gorm.DB) (*UpdateReply, error) {
 
-	clusterIsReady, err := isClusterReady(c, db, i.log)
-	if err != nil {
-		return nil, errors.Errorf("unable to determine cluster %s hosts state ", c.ID)
-	}
+	log := logutil.FromContext(ctx, i.log)
 
-	if clusterIsReady {
-		return updateState(clusterStatusReady, c, db, i.log)
-	} else {
-		i.log.Infof("Cluster %s does not have sufficient resources to be installed.", c.ID)
+	if err := db.Preload("Hosts").First(&c, "id = ?", c.ID).Error; err != nil {
 		return &UpdateReply{
 			State:     clusterStatusInsufficient,
-			IsChanged: false,
-		}, nil
+			IsChanged: false}, errors.Errorf("cluster %s not found", c.ID)
 	}
-}
+	mappedMastersByRole := mapMasterHostsByStatus(c)
 
-func (i *insufficientState) Install(ctx context.Context, c *models.Cluster) (*UpdateReply, error) {
-	return nil, errors.Errorf("unable to install cluster <%s> in <%s> status",
-		c.ID, swag.StringValue(c.Status))
+	// Cluster is ready
+	mastersInKnown, ok := mappedMastersByRole[host2.HostStatusKnown]
+	if ok && len(mastersInKnown) >= minHostsNeededForInstallation {
+		log.Infof("Cluster %s has at least %d known master hosts, cluster is ready.", c.ID, minHostsNeededForInstallation)
+		return updateState(clusterStatusReady, c, db, log)
+
+		//cluster is still insufficient
+	} else {
+		return &UpdateReply{State: clusterStatusInsufficient,
+			IsChanged: false}, nil
+	}
 }

internal/cluster/ready.go
@@ -5,7 +5,10 @@ import (
 
 	"github.com/sirupsen/logrus"
 
+	host2 "github.com/filanov/bm-inventory/internal/host"
 	"github.com/filanov/bm-inventory/models"
+	logutil "github.com/filanov/bm-inventory/pkg/log"
+
 	"github.com/jinzhu/gorm"
 	"github.com/pkg/errors"
 )
@@ -22,23 +25,24 @@ type readyState baseState
 var _ StateAPI = (*Manager)(nil)
 
 func (r *readyState) RefreshStatus(ctx context.Context, c *models.Cluster, db *gorm.DB) (*UpdateReply, error) {
-	clusterIsReady, err := isClusterReady(c, db, r.log)
-	if err != nil {
-		return nil, errors.Errorf("unable to determine cluster %s hosts state ", c.ID)
-	}
+	log := logutil.FromContext(ctx, r.log)
 
-	if clusterIsReady {
-
+	if err := db.Preload("Hosts").First(&c, "id = ?", c.ID).Error; err != nil {
 		return &UpdateReply{
-			State:     clusterStatusReady,
-			IsChanged: false,
-		}, nil
-	} else {
-		return updateState(clusterStatusInsufficient, c, db, r.log)
-
+			State:     clusterStatusInsufficient,
+			IsChanged: false}, errors.Errorf("cluster %s not found", c.ID)
 	}
-}
+	mappedMastersByRole := mapMasterHostsByStatus(c)
 
-func (r *readyState) Install(ctx context.Context, c *models.Cluster) (*UpdateReply, error) {
-	return updateState(clusterStatusInstalling, c, r.db, r.log)
+	// Cluster is insufficient
+	mastersInKnown := mappedMastersByRole[host2.HostStatusKnown]
+	if len(mastersInKnown) < minHostsNeededForInstallation {
+		log.Infof("Cluster %s dos not have at least %d known master hosts, cluster is insufficient.", c.ID, minHostsNeededForInstallation)
+		return updateState(clusterStatusInsufficient, c, db, log)
+
+		//cluster is still ready
+	} else {
+		return &UpdateReply{State: clusterStatusReady,
+			IsChanged: false}, nil
+	}
 }