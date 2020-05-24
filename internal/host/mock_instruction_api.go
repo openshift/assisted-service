042ade08f05ff9fb37d3bab6cf8efa89ff64a75e
Michael Filanov <mfilanov@redhat.com>
2020-05-24 09:15:02 +0300

    go generate

--- internal/host/mock_instruction_api.go
+++ internal/host/mock_instruction_api.go
@@ -6,10 +6,9 @@ package host
 
 import (
 	context "context"
-	reflect "reflect"
-
 	models "github.com/filanov/bm-inventory/models"
 	gomock "github.com/golang/mock/gomock"
+	reflect "reflect"
 )
 
 // MockInstructionApi is a mock of InstructionApi interface.
