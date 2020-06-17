danielerez/bm-inventory

MGMT-1145 Move agent commands to execute only - code changes

internal/host/connectivitycheckcmd_test.go
@@ -22,7 +22,7 @@ var _ = Describe("connectivitycheckcmd", func() {
 
 	BeforeEach(func() {
 		db = prepareDB()
-		connectivityCheckCmd = NewConnectivityCheckCmd(getTestLog(), db, nil)
+		connectivityCheckCmd = NewConnectivityCheckCmd(getTestLog(), db, nil, "quay.io/ocpmetal/connectivity_check:stable")
 
 		id = strfmt.UUID(uuid.New().String())
 		clusterId = strfmt.UUID(uuid.New().String())

internal/host/instructionmanager.go
@@ -28,16 +28,19 @@ type InstructionManager struct {
 	stateToSteps stateToStepsMap
 }
 type InstructionConfig struct {
-	InventoryURL   string `envconfig:"INVENTORY_URL" default:"10.35.59.36"`
-	InventoryPort  string `envconfig:"INVENTORY_PORT" default:"30485"`
-	InstallerImage string `envconfig:"INSTALLER_IMAGE" default:"quay.io/ocpmetal/assisted-installer:stable"`
+	InventoryURL           string `envconfig:"INVENTORY_URL" default:"10.35.59.36"`
+	InventoryPort          string `envconfig:"INVENTORY_PORT" default:"30485"`
+	InstallerImage         string `envconfig:"INSTALLER_IMAGE" default:"quay.io/ocpmetal/assisted-installer:stable"`
+	ConnectivityCheckImage string `envconfig:"CONNECTIVITY_CHECK_IMAGE" default:"quay.io/ocpmetal/connectivity_check:stable"`
+	InventoryImage         string `envconfig:"INVENTORY_IMAGE" default:"quay.io/ocpmetal/inventory:stable"`
+	HardwareInfoImage      string `envconfig:"HARDWARE_INFO_IMAGE" default:"quay.io/ocpmetal/hardware_info:stable"`
 }
 
 func NewInstructionManager(log logrus.FieldLogger, db *gorm.DB, hwValidator hardware.Validator, instructionConfig InstructionConfig) *InstructionManager {
-	connectivityCmd := NewConnectivityCheckCmd(log, db, hwValidator)
+	connectivityCmd := NewConnectivityCheckCmd(log, db, hwValidator, instructionConfig.ConnectivityCheckImage)
 	installCmd := NewInstallCmd(log, db, hwValidator, instructionConfig)
-	hwCmd := NewHwInfoCmd(log)
-	inventoryCmd := NewInventoryCmd(log)
+	hwCmd := NewHwInfoCmd(log, instructionConfig.HardwareInfoImage)
+	inventoryCmd := NewInventoryCmd(log, instructionConfig.InventoryImage)
 
 	return &InstructionManager{
 		log: log,

internal/host/inventorycmd_test.go
@@ -22,7 +22,7 @@ var _ = Describe("inventory", func() {
 
 	BeforeEach(func() {
 		db = prepareDB()
-		invCmd = NewInventoryCmd(getTestLog())
+		invCmd = NewInventoryCmd(getTestLog(), "quay.io/ocpmetal/inventory:stable")
 
 		id = strfmt.UUID(uuid.New().String())
 		clusterId = strfmt.UUID(uuid.New().String())

openshift/assisted-service

internal/host/connectivitycheckcmd_test.go
@@ -22,7 +22,7 @@ var _ = Describe("connectivitycheckcmd", func() {
 
 	BeforeEach(func() {
 		db = prepareDB()
-		connectivityCheckCmd = NewConnectivityCheckCmd(getTestLog(), db, nil)
+		connectivityCheckCmd = NewConnectivityCheckCmd(getTestLog(), db, nil, "quay.io/ocpmetal/connectivity_check:latest")
 
 		id = strfmt.UUID(uuid.New().String())
 		clusterId = strfmt.UUID(uuid.New().String())

internal/host/instructionmanager.go
@@ -28,17 +28,19 @@ type InstructionManager struct {
 	stateToSteps stateToStepsMap
 }
 type InstructionConfig struct {
-	InventoryURL      string `envconfig:"INVENTORY_URL" default:"10.35.59.36"`
-	InventoryPort     string `envconfig:"INVENTORY_PORT" default:"30485"`
-	InstallerImage    string `envconfig:"INSTALLER_IMAGE" default:"quay.io/ocpmetal/assisted-installer:latest"`
-	HardwareInfoImage string `envconfig:"HARDWARE_INFO_IMAGE" default:"quay.io/ocpmetal/hardware_info:latest"`
+	InventoryURL           string `envconfig:"INVENTORY_URL" default:"10.35.59.36"`
+	InventoryPort          string `envconfig:"INVENTORY_PORT" default:"30485"`
+	InstallerImage         string `envconfig:"INSTALLER_IMAGE" default:"quay.io/ocpmetal/assisted-installer:latest"`
+	ConnectivityCheckImage string `envconfig:"CONNECTIVITY_CHECK_IMAGE" default:"quay.io/ocpmetal/connectivity_check:latest"`
+	InventoryImage         string `envconfig:"INVENTORY_IMAGE" default:"quay.io/ocpmetal/inventory:latest"`
+	HardwareInfoImage      string `envconfig:"HARDWARE_INFO_IMAGE" default:"quay.io/ocpmetal/hardware_info:latest"`
 }
 
 func NewInstructionManager(log logrus.FieldLogger, db *gorm.DB, hwValidator hardware.Validator, instructionConfig InstructionConfig) *InstructionManager {
-	connectivityCmd := NewConnectivityCheckCmd(log, db, hwValidator)
+	connectivityCmd := NewConnectivityCheckCmd(log, db, hwValidator, instructionConfig.ConnectivityCheckImage)
 	installCmd := NewInstallCmd(log, db, hwValidator, instructionConfig)
 	hwCmd := NewHwInfoCmd(log, instructionConfig.HardwareInfoImage)
-	inventoryCmd := NewInventoryCmd(log)
+	inventoryCmd := NewInventoryCmd(log, instructionConfig.InventoryImage)
 
 	return &InstructionManager{
 		log: log,
@@ -51,7 +53,6 @@ func NewInstructionManager(log logrus.FieldLogger, db *gorm.DB, hwValidator hard
 			HostStatusInstalling:   {installCmd},
 		},
 	}
-
 }
 
 func (i *InstructionManager) GetNextSteps(ctx context.Context, host *models.Host) (models.Steps, error) {

internal/host/inventorycmd_test.go
@@ -22,7 +22,7 @@ var _ = Describe("inventory", func() {
 
 	BeforeEach(func() {
 		db = prepareDB()
-		invCmd = NewInventoryCmd(getTestLog())
+		invCmd = NewInventoryCmd(getTestLog(), "quay.io/ocpmetal/inventory:latest")
 
 		id = strfmt.UUID(uuid.New().String())
 		clusterId = strfmt.UUID(uuid.New().String())