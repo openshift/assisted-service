696cf7859c8ff83807d1b9af9e4b63540a4e0a01
Igal Tsoiref <itsoiref@redhat.com>
2020-06-28 13:42:03 +0300

    Fixing depeloy-all issue with wating for inventory pod

--- tools/wait_for_pod.py
+++ tools/wait_for_pod.py
@@ -3,6 +3,7 @@ import time
 import subprocess
 import json
 import sys
+import waiting
 
 TIMEOUT = 60 * 3
 
@@ -14,39 +15,39 @@ args = parser.parse_args()
 
 def main():
     print("waiting for pod app {} to reach {} status".format(args.app, args.state))
-
-    start_time = time.time()
-    while not is_pod_in_state():
-        if time.time() - start_time > TIMEOUT:
-            print("ERROR: pods app name {} did not enter {} state within {} seconds".format(args.app, args.state, TIMEOUT))
-            sys.exit("pods app name {} did not enter {} state within {} seconds".format(args.app, args.state, TIMEOUT))
-        time.sleep(1)
+    waiting.wait(lambda: is_pod_in_state(),
+                 timeout_seconds=TIMEOUT,
+                 expected_exceptions=Exception,
+                 sleep_seconds=1, waiting_for="pod app {} is in {} status".format(args.app, args.state))
 
     print("pod app {} is in {} status".format(args.app, args.state))
     return
 
+
 def is_pod_in_state():
+    state_keys = get_pod_state()
 
-        state_keys = get_pod_state()
+    if args.state not in state_keys:
+        return False
 
-        if not args.state in state_keys:
-            return False
+    # Re-check pod state after 5 sec, makes sure the pod is running and didn't fail while starting
+    time.sleep(5)
+    state_keys = get_pod_state()
+    if args.state in state_keys:
+        return True
 
-        # Re-check pod state after 5 sec, makes sure the pod is running and didn't fail while starting
-        time.sleep  (5)
-        state_keys = get_pod_state()
-        if args.state in state_keys:
-            return True
 
 def get_pod_state():
-        ret = subprocess.check_output("kubectl get pods -l app={app} -A -o json --namespace=assisted-installer".format(app=args.app), shell=True)
-        pod_json = json.loads(ret)
-        if not pod_json["items"]:
-            print("ERROR: pods app name {} not found".format(args.app))
-            sys.exit("pods app name {} not found".format(args.app))
+    ret = subprocess.check_output(
+        "kubectl get pods -l app={app} -o json --namespace=assisted-installer".format(app=args.app), shell=True)
+    pod_json = json.loads(ret)
+    if not pod_json["items"]:
+        print("ERROR: pods app name {} not found".format(args.app))
+        sys.exit("pods app name {} not found".format(args.app))
 
-        state_keys = pod_json["items"][0]["status"]["containerStatuses"][0]["state"].keys()
-        return state_keys
+    state_keys = pod_json["items"][0]["status"]["containerStatuses"][0]["state"].keys()
+    return state_keys
+
 
 if __name__ == "__main__":
     main()
