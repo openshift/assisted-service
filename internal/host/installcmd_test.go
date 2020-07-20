b786fabf77baadf1c4b713bac16c813359c80fde
Yevgeny Shnaidman <yshnaidm@redhat.com>
2020-07-20 09:00:24 +0300

    MGMT-1419 - always set hstname in ignition, so that the hostname reported by LiveCD will be the hostname after reboot

--- internal/host/installcmd_test.go
+++ internal/host/installcmd_test.go
@@ -48,7 +48,7 @@ var _ = Describe("installcmd", func() {
 		installCmd = NewInstallCmd(getTestLog(), db, mockValidator, instructionConfig)
 		cluster = createClusterInDb(db)
 		clusterId = *cluster.ID
-		host = createHostInDb(db, clusterId, models.HostRoleMaster, false)
+		host = createHostInDb(db, clusterId, models.HostRoleMaster, false, "")
 		validDiskSize := int64(128849018880)
 		disks = []*models.Disk{
 			{DriveType: "HDD", Name: "sdb", SizeBytes: validDiskSize},
@@ -75,25 +75,25 @@ var _ = Describe("installcmd", func() {
 		mockValidator.EXPECT().GetHostValidDisks(gomock.Any()).Return(disks, nil).Times(1)
 		stepReply, stepErr = installCmd.GetStep(ctx, &host)
 		postvalidation(false, false, stepReply, stepErr, models.HostRoleMaster)
-		validateInstallCommand(stepReply, models.HostRoleMaster, string(clusterId), string(*host.ID))
+		validateInstallCommand(stepReply, models.HostRoleMaster, string(clusterId), string(*host.ID), "")
 		Expect(getHost(*host.ID, clusterId, db).InstallerVersion).
 			To(Equal(defaultInstructionConfig.InstallerImage))
 	})
 
 	It("get_step_three_master_success", func() {
 
-		host2 := createHostInDb(db, clusterId, models.HostRoleMaster, false)
-		host3 := createHostInDb(db, clusterId, models.HostRoleMaster, true)
+		host2 := createHostInDb(db, clusterId, models.HostRoleMaster, false, "")
+		host3 := createHostInDb(db, clusterId, models.HostRoleMaster, true, "some_hostname")
 		mockValidator.EXPECT().GetHostValidDisks(gomock.Any()).Return(disks, nil).Times(3)
 		stepReply, stepErr = installCmd.GetStep(ctx, &host)
 		postvalidation(false, false, stepReply, stepErr, models.HostRoleMaster)
-		validateInstallCommand(stepReply, models.HostRoleMaster, string(clusterId), string(*host.ID))
+		validateInstallCommand(stepReply, models.HostRoleMaster, string(clusterId), string(*host.ID), "")
 		stepReply, stepErr = installCmd.GetStep(ctx, &host2)
 		postvalidation(false, false, stepReply, stepErr, models.HostRoleMaster)
-		validateInstallCommand(stepReply, models.HostRoleMaster, string(clusterId), string(*host2.ID))
+		validateInstallCommand(stepReply, models.HostRoleMaster, string(clusterId), string(*host2.ID), "")
 		stepReply, stepErr = installCmd.GetStep(ctx, &host3)
 		postvalidation(false, false, stepReply, stepErr, models.HostRoleBootstrap)
-		validateInstallCommand(stepReply, models.HostRoleBootstrap, string(clusterId), string(*host3.ID))
+		validateInstallCommand(stepReply, models.HostRoleBootstrap, string(clusterId), string(*host3.ID), "some_hostname")
 	})
 
 	AfterEach(func() {
@@ -116,15 +116,16 @@ func createClusterInDb(db *gorm.DB) common.Cluster {
 	return cluster
 }
 
-func createHostInDb(db *gorm.DB, clusterId strfmt.UUID, role models.HostRole, bootstrap bool) models.Host {
+func createHostInDb(db *gorm.DB, clusterId strfmt.UUID, role models.HostRole, bootstrap bool, hostname string) models.Host {
 	id := strfmt.UUID(uuid.New().String())
 	host := models.Host{
-		ID:           &id,
-		ClusterID:    clusterId,
-		Status:       swag.String(HostStatusDiscovering),
-		Role:         role,
-		Bootstrap:    bootstrap,
-		HardwareInfo: defaultHwInfo,
+		ID:                &id,
+		ClusterID:         clusterId,
+		Status:            swag.String(HostStatusDiscovering),
+		Role:              role,
+		Bootstrap:         bootstrap,
+		HardwareInfo:      defaultHwInfo,
+		RequestedHostname: hostname,
 	}
 	Expect(db.Create(&host).Error).ShouldNot(HaveOccurred())
 	return host
@@ -144,15 +145,27 @@ func postvalidation(isstepreplynil bool, issteperrnil bool, expectedstepreply *m
 	}
 }
 
-func validateInstallCommand(reply *models.Step, role models.HostRole, clusterId string, hostId string) {
-	installCommand := "sudo podman run -v /dev:/dev:rw -v /opt:/opt:rw --privileged --pid=host " +
-		"--net=host -v /var/log:/var/log:rw " +
-		"--name assisted-installer quay.io/ocpmetal/assisted-installer:latest --role %s " +
-		"--cluster-id %s --host %s --port %s " +
-		"--boot-device /dev/sdb --host-id %s --openshift-version 4.5 " +
-		"--controller-image %s"
-	ExpectWithOffset(1, reply.Args[1]).Should(Equal(fmt.Sprintf(installCommand, role, clusterId,
-		defaultInstructionConfig.InventoryURL, defaultInstructionConfig.InventoryPort, hostId,
-		defaultInstructionConfig.ControllerImage)))
+func validateInstallCommand(reply *models.Step, role models.HostRole, clusterId string, hostId string, hostname string) {
+	if hostname != "" {
+		installCommand := "sudo podman run -v /dev:/dev:rw -v /opt:/opt:rw --privileged --pid=host " +
+			"--net=host -v /var/log:/var/log:rw " +
+			"--name assisted-installer quay.io/ocpmetal/assisted-installer:latest --role %s " +
+			"--cluster-id %s --host %s --port %s " +
+			"--boot-device /dev/sdb --host-id %s --openshift-version 4.5 " +
+			"--controller-image %s --host-name %s"
+		ExpectWithOffset(1, reply.Args[1]).Should(Equal(fmt.Sprintf(installCommand, role, clusterId,
+			defaultInstructionConfig.InventoryURL, defaultInstructionConfig.InventoryPort, hostId,
+			defaultInstructionConfig.ControllerImage, hostname)))
+	} else {
+		installCommand := "sudo podman run -v /dev:/dev:rw -v /opt:/opt:rw --privileged --pid=host " +
+			"--net=host -v /var/log:/var/log:rw " +
+			"--name assisted-installer quay.io/ocpmetal/assisted-installer:latest --role %s " +
+			"--cluster-id %s --host %s --port %s " +
+			"--boot-device /dev/sdb --host-id %s --openshift-version 4.5 " +
+			"--controller-image %s"
+		ExpectWithOffset(1, reply.Args[1]).Should(Equal(fmt.Sprintf(installCommand, role, clusterId,
+			defaultInstructionConfig.InventoryURL, defaultInstructionConfig.InventoryPort, hostId,
+			defaultInstructionConfig.ControllerImage)))
+	}
 	ExpectWithOffset(1, reply.StepType).To(Equal(models.StepTypeInstall))
 }
