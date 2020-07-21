9d2a6c2ba7a24e55ceddf0daece1e93f77805201
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2020-07-21 12:46:08 +0200

    Fixing PSE deployment and added some adjustments on Prom svc

--- tools/deploy_grafana.py
+++ tools/deploy_grafana.py
@@ -1,10 +1,14 @@
+'''
+This script deploy Grafana instance of it on K8s and OCP
+n the OCP case, it will be integrated automatically with OCP oauth.
+'''
+
 import os
-import utils
+import sys
 from time import sleep
 import argparse
 import secrets
-import string
-import base64
+import utils
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--target")
@@ -17,11 +21,13 @@ else:
     CMD_BIN = 'oc'
 
 def deploy_oauth_reqs():
+    '''oauth Integration in OCP'''
     # Token generation for session_secret
     session_secret = secrets.token_hex(43)
     secret_name = 'grafana-proxy'
     if not utils.check_if_exists('secret', secret_name):
-        cmd = "{} -n assisted-installer create secret generic {} --from-literal=session_secret={}".format(CMD_BIN, secret_name, session_secret)
+        cmd = "{} -n assisted-installer create secret generic {} --from-literal=session_secret={}"\
+        .format(CMD_BIN, secret_name, session_secret)
         utils.check_output(cmd)
 
     ## Create and Annotate Serviceaccount
@@ -29,23 +35,25 @@ def deploy_oauth_reqs():
     if not utils.check_if_exists('sa', sa_name):
         cmd = "{} -n assisted-installer create serviceaccount {} ".format(CMD_BIN, sa_name)
         utils.check_output(cmd)
-    json_manifest = '{"kind":"OAuthRedirectReference","apiVersion":"v1","reference":{"kind":"Route","name":"grafana"}}' 
+    json_manifest = '{"kind":"OAuthRedirectReference","apiVersion":"v1","reference":{"kind":"Route","name":"grafana"}}'
     annotation_name = 'serviceaccounts.openshift.io/oauth-redirectreference.grafana'
     cmd = "{} -n assisted-installer annotate serviceaccount {} --overwrite {}='{}'".format(
-            CMD_BIN, sa_name, annotation_name, json_manifest)
+        CMD_BIN, sa_name, annotation_name, json_manifest)
     utils.check_output(cmd)
 
     # Get OCP Certificate
     if not utils.check_if_exists('secret', 'openshift-custom-ca'):
         secret_name = 'router-certs-default'
-        ns = 'openshift-ingress'
+        namespace = 'openshift-ingress'
         template = '{{index .data "tls.crt"}}'
-        cmd = "{} get secret {} --namespace={} --template '{}'".format(CMD_BIN, secret_name, ns, template)
+        cmd = "{} get secret {} --namespace={} --template '{}'".format(CMD_BIN, secret_name, namespace, template)
         ca_cert = utils.check_output(cmd)
 
         # Renderized secret with CA Certificate of the OCP Cluster
-        src_file = os.path.join(os.getcwd(), "deploy/monitoring/prometheus/assisted-installer-ocp-prometheus-custom-ca.yaml")
-        dst_file = os.path.join(os.getcwd(), "build/assisted-installer-ocp-prometheus-custom-ca.yaml")
+        src_file = os.path.join(os.getcwd(),\
+                "deploy/monitoring/prometheus/assisted-installer-ocp-prometheus-custom-ca.yaml")
+        dst_file = os.path.join(os.getcwd(),\
+                "build/assisted-installer-ocp-prometheus-custom-ca.yaml")
         topic = 'OCP Custom CA'
         with open(src_file, "r") as src:
             with open(dst_file, "w+") as dst:
@@ -57,17 +65,34 @@ def deploy_oauth_reqs():
 
 
 def deployer(src_file, topic):
+    '''Wrapper for oc/kubectl apply -f'''
     src_file = os.path.join(os.getcwd(), src_file)
     print("Deploying {}: {}".format(topic ,src_file))
     utils.apply(src_file)
 
 
 def deploy_grafana_route():
-    # Deploy Grafana Route
+    '''Deploy Grafana Route'''
     topic = 'Grafana Route'
-    src_file = os.path.join(os.getcwd(), "deploy/monitoring/grafana/assisted-installer-ocp-grafana-route.yaml")
-    dst_file = os.path.join(os.getcwd(), "build/assisted-installer-ocp-grafana-route.yaml")
-    ingress_domain = utils.get_domain()
+    src_file = os.path.join(os.getcwd(),\
+            "deploy/monitoring/grafana/assisted-installer-ocp-grafana-route.yaml")
+    dst_file = os.path.join(os.getcwd(),\
+            "build/assisted-installer-ocp-grafana-route.yaml")
+    try:
+        # I have permissions
+        ingress_domain = utils.get_domain()
+    except:
+        # I have not permissions, yes it's ugly...
+        # This ingress should be there because of UI deployment
+        json_path_ingress = '{.spec.rules[0].host}'
+        cmd = "{} get ingress assisted-installer -o jsonpath='{}'".format(
+            CMD_BIN, json_path_ingress)
+        assisted_installer_ingress_domain = utils.check_output(cmd)
+        if assisted_installer_ingress_domain.split(".")[0] != 'assisted-installer':
+            print("Error recovering the ingress route")
+            sys.exit(1)
+        ingress_domain = assisted_installer_ingress_domain.split(".", maxsplit=1)[1]
+
     with open(src_file, "r") as src:
         with open(dst_file, "w+") as dst:
             data = src.read()
@@ -78,32 +103,38 @@ def deploy_grafana_route():
 
 
 def deploy_grafana_ds():
-    ns = 'assisted-installer'
+    '''Deploy grafana daemonSet'''
+    namespace = 'assisted-installer'
     secret_name = 'grafana-datasources'
     src_file = os.path.join(os.getcwd(), "deploy/monitoring/grafana/prometheus.json")
     if not utils.check_if_exists('secret', secret_name):
         print("Creating Grafana Datasource")
-        cmd = "{} create secret generic {} --namespace={} --from-file=prometheus.yaml={}".format(CMD_BIN, secret_name, ns, src_file)
+        cmd = "{} create secret generic {} --namespace={} --from-file=prometheus.yaml={}".format(
+            CMD_BIN, secret_name, namespace, src_file)
         utils.check_output(cmd)
 
 
 def deploy_grafana_config(conf_file):
-    ns = 'assisted-installer'
+    '''Deploy Grafana ConfigMap'''
+    namespace = 'assisted-installer'
     secret_name = 'grafana-config'
     src_file = os.path.join(os.getcwd(), "deploy/monitoring/grafana/" + conf_file)
     if not utils.check_if_exists('secret', secret_name):
         print("Creating Grafana Configuration")
-        cmd = "{} create secret generic {} --namespace={} --from-file=grafana.ini={}".format(CMD_BIN, secret_name, ns, src_file)
+        cmd = "{} create secret generic {} --namespace={} --from-file=grafana.ini={}".format(
+            CMD_BIN, secret_name, namespace, src_file)
         utils.check_output(cmd)
     else:
         print("Updating Grafana Configuration")
-        cmd = "{} delete secret {} --namespace={}".format(CMD_BIN, secret_name, ns)
+        cmd = "{} delete secret {} --namespace={}".format(CMD_BIN, secret_name, namespace)
         utils.check_output(cmd)
-        cmd = "{} create secret generic {} --namespace={} --from-file=grafana.ini={}".format(CMD_BIN, secret_name, ns, src_file)
+        cmd = "{} create secret generic {} --namespace={} --from-file=grafana.ini={}".format(
+            CMD_BIN, secret_name, namespace, src_file)
         utils.check_output(cmd)
 
 
 def main():
+    '''Deploy Grafana for Assisted Installer'''
     if args.target != "oc-ingress":
         # Deploy grafana configuration
         grafana_conf_file = 'grafana-k8s.ini'
@@ -111,11 +142,14 @@ def main():
         # Deploy grafana DS
         deploy_grafana_ds()
         # Deploy Dashboards
-        deployer('deploy/monitoring/grafana/grafana-dashboards.yaml', 'Grafana Dashboards')
+        deployer('deploy/monitoring/grafana/grafana-dashboards.yaml',
+                 'Grafana Dashboards')
         # Deploy Assisted Installer Dashboard
-        deployer('deploy/monitoring/grafana/assisted-installer-grafana-dashboard.yaml', 'Grafana Assisted Installer Dashboard')
-        # Deploy Grafana 
-        deployer('deploy/monitoring/grafana/assisted-installer-k8s-grafana.yaml', 'Grafana Instance on K8s')
+        deployer('deploy/monitoring/grafana/assisted-installer-grafana-dashboard.yaml',
+                 'Grafana Assisted Installer Dashboard')
+        # Deploy Grafana
+        deployer('deploy/monitoring/grafana/assisted-installer-k8s-grafana.yaml',
+                 'Grafana Instance on K8s')
         sleep(10)
         utils.check_k8s_rollout('deployment', 'grafana')
     else:
@@ -127,11 +161,14 @@ def main():
         # Deploy grafana DS
         deploy_grafana_ds()
         # Deploy Dashboards
-        deployer('deploy/monitoring/grafana/grafana-dashboards.yaml', 'Grafana Dashboards')
+        deployer('deploy/monitoring/grafana/grafana-dashboards.yaml',
+                 'Grafana Dashboards')
         # Deploy Assisted Installer Dashboard
-        deployer('deploy/monitoring/grafana/assisted-installer-grafana-dashboard.yaml', 'Grafana Assisted Installer Dashboard')
-        # Deploy Grafana 
-        deployer('deploy/monitoring/grafana/assisted-installer-ocp-grafana.yaml', 'Grafana Instance on OCP')
+        deployer('deploy/monitoring/grafana/assisted-installer-grafana-dashboard.yaml',
+                 'Grafana Assisted Installer Dashboard')
+        # Deploy Grafana
+        deployer('deploy/monitoring/grafana/assisted-installer-ocp-grafana.yaml',
+                 'Grafana Instance on OCP')
         sleep(10)
         utils.check_k8s_rollout('deployment', 'grafana')
         # Deploy grafana Route
--- tools/deploy_prometheus.py
+++ tools/deploy_prometheus.py
@@ -1,11 +1,14 @@
+'''
+This script deploy Prometheus Operator and the instance of it on K8s and OCP
+n the OCP case, it will be integrated automatically with OCP oauth.
+'''
+
 import os
-import utils
+import sys
 from time import sleep
 import argparse
 import secrets
-import string
-import base64
-from waiting import wait, TimeoutExpired
+import utils
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--target")
@@ -14,58 +17,83 @@ args = parser.parse_args()
 
 if args.target != "oc-ingress":
     CMD_BIN = 'kubectl'
-    olm_ns = 'olm'
-    cat_src = 'operatorhubio-catalog'
+    OLM_NS = 'olm'
+    CAT_SRC = 'operatorhubio-catalog'
 else:
     CMD_BIN = 'oc'
-    olm_ns = 'openshift-marketplace'
-    cat_src = 'community-operators'
+    OLM_NS = 'openshift-marketplace'
+    CAT_SRC = 'community-operators'
 
 def deploy_oauth_reqs():
-    # Token generation for session_secret
+    '''oauth Integration in OCP'''
+    ## Token generation for session_secret
     session_secret = secrets.token_hex(43)
     secret_name = 'prometheus-k8s-proxy'
     if not utils.check_if_exists('secret', secret_name):
-        cmd = "{} -n assisted-installer create secret generic {} --from-literal=session_secret={}".format(CMD_BIN, secret_name, session_secret)
+        cmd = "{} -n assisted-installer create secret generic {} --from-literal=session_secret={}" \
+                .format(CMD_BIN, secret_name, session_secret)
         utils.check_output(cmd)
 
     ## Annotate Serviceaccount
     json_manifest = '{"kind":"OAuthRedirectReference","apiVersion":"v1","reference":{"kind":"Route","name":"prometheus-assisted"}}'
     sa_name = 'prometheus-k8s'
     annotation_name = 'serviceaccounts.openshift.io/oauth-redirectreference.assisted-installer-prometheus'
-    cmd = "{} -n assisted-installer annotate serviceaccount {} --overwrite {}='{}'".format(
-            CMD_BIN, sa_name, annotation_name, json_manifest)
+    cmd = "{} -n assisted-installer annotate serviceaccount {} --overwrite {}='{}'"\
+            .format(CMD_BIN, sa_name, annotation_name, json_manifest)
     utils.check_output(cmd)
 
-    # Get OCP Certificate
-    secret_name = 'router-certs-default'
-    ns = 'openshift-ingress'
-    template = '{{index .data "tls.crt"}}'
-    cmd = "{} get secret {} --namespace={} --template '{}'".format(CMD_BIN, secret_name, ns, template)
-    ca_cert = utils.check_output(cmd)
+    # Download OCP Certificate as a secret
+    cert_secret_name = 'openshift-custom-ca'
+    cmd = "{} get secret {} -n assisted-installer --no-headers".format(CMD_BIN, cert_secret_name)
+    cert_secret = utils.check_output(cmd)
+    if not cert_secret:
+        # Get OCP Certificate
+        secret_name = 'router-certs-default'
+        namespace = 'openshift-ingress'
+        template = '{{index .data "tls.crt"}}'
+        cmd = "{} get secret {} --namespace={} --template '{}'"\
+                .format(CMD_BIN, secret_name, namespace, template)
+        ca_cert = utils.check_output(cmd)
 
-    # Check the Cert
-    # print(base64.b64decode(ca_cert).decode('utf-8'))
-
-    # Renderized secret with CA Certificate of the OCP Cluster
-    src_file = os.path.join(os.getcwd(), "deploy/monitoring/prometheus/assisted-installer-ocp-prometheus-custom-ca.yaml")
-    dst_file = os.path.join(os.getcwd(), "build/assisted-installer-ocp-prometheus-custom-ca.yaml")
-    topic = 'OCP Custom CA'
-    with open(src_file, "r") as src:
-        with open(dst_file, "w+") as dst:
-            data = src.read()
-            data = data.replace("BASE64_CERT", ca_cert)
-            print("Deploying {}: {}".format(topic, dst_file))
-            dst.write(data)
-    utils.apply(dst_file)
+        # Renderized secret with CA Certificate of the OCP Cluster
+        src_file = os.path.join(os.getcwd(), \
+                "deploy/monitoring/prometheus/assisted-installer-ocp-prometheus-custom-ca.yaml")
+        dst_file = os.path.join(os.getcwd(), \
+                "build/assisted-installer-ocp-prometheus-custom-ca.yaml")
+        topic = 'OCP Custom CA'
+        with open(src_file, "r") as src:
+            with open(dst_file, "w+") as dst:
+                data = src.read()
+                data = data.replace("BASE64_CERT", ca_cert)
+                print("Deploying {}: {}".format(topic, dst_file))
+                dst.write(data)
+        utils.apply(dst_file)
+    else:
+        print("Secret {} already exists", cert_secret_name)
 
 
 def deploy_prometheus_route():
-    # Deploy Prometheus Route
+    '''Deploy Prometheus Route'''
     topic = 'Prometheus Operator Route'
-    src_file = os.path.join(os.getcwd(), "deploy/monitoring/prometheus/assisted-installer-ocp-prometheus-route.yaml")
-    dst_file = os.path.join(os.getcwd(), "build/assisted-installer-ocp-prometheus-route.yaml")
-    ingress_domain = utils.get_domain()
+    src_file = os.path.join(os.getcwd(),\
+            "deploy/monitoring/prometheus/assisted-installer-ocp-prometheus-route.yaml")
+    dst_file = os.path.join(os.getcwd(),\
+            "build/assisted-installer-ocp-prometheus-route.yaml")
+    try:
+        # I have permissions
+        ingress_domain = utils.get_domain()
+    except:
+        # I have not permissions, yes it's ugly...
+        # This ingress should be there because of UI deployment
+        json_path_ingress = '{.spec.rules[0].host}'
+        cmd = "{} get ingress assisted-installer -o jsonpath='{}'".format(
+            CMD_BIN, json_path_ingress)
+        assisted_installer_ingress_domain = utils.check_output(cmd)
+        if assisted_installer_ingress_domain.split(".")[0] != 'assisted-installer':
+            print("Error recovering the ingress route")
+            sys.exit(1)
+
+        ingress_domain = assisted_installer_ingress_domain.split(".", maxsplit=1)[1]
     with open(src_file, "r") as src:
         with open(dst_file, "w+") as dst:
             data = src.read()
@@ -76,10 +104,12 @@ def deploy_prometheus_route():
 
 
 def deploy_prometheus_sub(olm_ns, cat_src):
-    # Deploy Operator Subscription
+    '''Deploy Operator Subscription'''
     topic = 'Prometheus Operator Subscription'
-    src_file = os.path.join(os.getcwd(), "deploy/monitoring/prometheus/assisted-installer-operator-subscription.yaml")
-    dst_file = os.path.join(os.getcwd(), "build/assisted-installer-operator-subscription.yaml")
+    src_file = os.path.join(os.getcwd(),\
+            "deploy/monitoring/prometheus/assisted-installer-operator-subscription.yaml")
+    dst_file = os.path.join(os.getcwd(),\
+            "build/assisted-installer-operator-subscription.yaml")
     with open(src_file, "r") as src:
         with open(dst_file, "w+") as dst:
             data = src.read()
@@ -91,40 +121,59 @@ def deploy_prometheus_sub(olm_ns, cat_src):
 
 
 def deployer(src_file, topic):
+    '''Wrapper for oc/kubectl apply -f'''
     src_file = os.path.join(os.getcwd(), src_file)
-    print("Deploying {}: {}".format(topic ,src_file))
+    print("Deploying {}: {}".format(topic, src_file))
     utils.apply(src_file)
 
 
 def main():
+    '''Deploy Prometheus operator and Instance '''
     if args.target != "oc-ingress":
         # Deploy Operator Group
-        deployer('deploy/monitoring/prometheus/assisted-installer-operator-group.yaml', 'OperatorGroup')
+        deployer('deploy/monitoring/prometheus/assisted-installer-operator-group.yaml',
+                 'OperatorGroup')
         # Deploy Subscription
-        deploy_prometheus_sub(olm_ns, cat_src)
+        deploy_prometheus_sub(OLM_NS, CAT_SRC)
         # Deploy Prom svc
-        deployer('deploy/monitoring/prometheus/assisted-installer-k8s-prometheus-svc.yaml', 'Prometheus Service')
+        deployer('deploy/monitoring/prometheus/assisted-installer-k8s-prometheus-svc.yaml',
+                 'Prometheus Service')
         # Deploy Prometheus Instance
-        deployer('deploy/monitoring/prometheus/assisted-installer-k8s-prometheus-subscription-instance.yaml', 'Prometheus Instance on K8s')
+        deployer('deploy/monitoring/prometheus/assisted-installer-k8s-prometheus-subscription-instance.yaml',
+                 'Prometheus Instance on K8s')
         sleep(10)
         utils.check_k8s_rollout('statefulset', 'prometheus-assisted-installer-prometheus')
         # Deploy Prom svc Monitor
-        deployer('deploy/monitoring/prometheus/assisted-installer-prometheus-svc-monitor.yaml', 'Prometheus Service Monitor')
+        deployer('deploy/monitoring/prometheus/assisted-installer-prometheus-svc-monitor.yaml',
+                 'Prometheus Service Monitor')
     else:
         # Deploy Operator Group
-        deployer('deploy/monitoring/prometheus/assisted-installer-operator-group.yaml', 'OperatorGroup')
+        try:
+            deployer('deploy/monitoring/prometheus/assisted-installer-operator-group.yaml',
+                     'OperatorGroup')
+        except:
+            cmd = "{} get OperatorGroup --no-headers".format(CMD_BIN)
+            if not utils.check_output(cmd):
+                print("The creation of an OperatorGroup is Forbidden for you user please request a creation of one before execute this again, exiting...")
+                sys.exit(1)
+            else:
+                print("Another OperatorGroup exists, continuing")
         # Deploy Subscription
-        deploy_prometheus_sub(olm_ns, cat_src)
+        deploy_prometheus_sub(OLM_NS, CAT_SRC)
         # Deploy Oauth Pre-reqs for OCP integration
         deploy_oauth_reqs()
-        # Deploy Prom svc; We create the service first in order to self-generate the secret prometheus-k8s-tls
-        deployer('deploy/monitoring/prometheus/assisted-installer-ocp-prometheus-svc.yaml', 'Prometheus Service on OCP')
+        # Deploy Prom svc;
+        # We create the service first in order to self-generate the secret prometheus-k8s-tls
+        deployer('deploy/monitoring/prometheus/assisted-installer-ocp-prometheus-svc.yaml',
+                 'Prometheus Service on OCP')
         # Deploy Prometheus Instance
-        deployer('deploy/monitoring/prometheus/assisted-installer-ocp-prometheus-subscription-instance.yaml', 'Prometheus Instance on OCP')
+        deployer('deploy/monitoring/prometheus/assisted-installer-ocp-prometheus-subscription-instance.yaml',
+                 'Prometheus Instance on OCP')
         sleep(10)
         utils.check_k8s_rollout('statefulset', 'prometheus-assisted-installer-prometheus')
         # Deploy Prom svc Monitor
-        deployer('deploy/monitoring/prometheus/assisted-installer-prometheus-svc-monitor.yaml', 'Prometheus Service Monitor')
+        deployer('deploy/monitoring/prometheus/assisted-installer-prometheus-svc-monitor.yaml',
+                 'Prometheus Service Monitor')
         # Deploy Prometheus Route
         deploy_prometheus_route()
 
