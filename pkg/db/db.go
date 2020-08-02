2ff9b6c6676d8bbf9d1a8562d6ba7badc711f44d
Yoni Bettan <ybettan@redhat.com>
2020-08-02 14:50:23 +0300

    Removed default values from DBConfig.
    The values of DBConfig fields comes from a secret, if the deployment
    isn't using this secret correctly we want to make sure that subsystem
    tests are failing instead of succeeding by using correct credentials
    originated from default values.
    
    Signed-off-by: Yoni Bettan <ybettan@redhat.com>

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,9 +1,9 @@
 package db
 
 type Config struct {
-	Host string `envconfig:"DB_HOST" default:"postgres"`
-	Port string `envconfig:"DB_PORT" default:"5432"`
-	User string `envconfig:"DB_USER" default:"admin"`
-	Pass string `envconfig:"DB_PASS" default:"admin"`
-	Name string `envconfig:"DB_NAME" default:"installer"`
+	Host string `envconfig:"DB_HOST"`
+	Port string `envconfig:"DB_PORT"`
+	User string `envconfig:"DB_USER"`
+	Pass string `envconfig:"DB_PASS"`
+	Name string `envconfig:"DB_NAME"`
 }
