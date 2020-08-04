26000c94371097e96d16751d8fb45a3307884334
Michael Filanov <mfilanov@redhat.com>
2020-08-04 10:56:00 +0300

    Rename BM_INVENTORY to ASSISTED_SERVICE in build_deploy.sh and openshift/template.yaml

--- build_deploy.sh
+++ build_deploy.sh
@@ -2,17 +2,17 @@
 
 TAG=$(git rev-parse --short=7 HEAD)
 
-BM_INVENTORY_IMAGE="quay.io/app-sre/assisted-service"
+ASSISTED_SERVICE_IMAGE="quay.io/app-sre/assisted-service"
 
-docker build -t "${BM_INVENTORY_IMAGE}:latest" -f Dockerfile.demo .
-docker tag "${BM_INVENTORY_IMAGE}:latest" "${BM_INVENTORY_IMAGE}:${TAG}"
-#docker build -t "${BM_INVENTORY_IMAGE}:latest" -f Dockerfile.assisted-service .
-#docker tag "${BM_INVENTORY_IMAGE}:latest" "${BM_INVENTORY_IMAGE}:${TAG}"
+docker build -t "${ASSISTED_SERVICE_IMAGE}:latest" -f Dockerfile.demo .
+docker tag "${ASSISTED_SERVICE_IMAGE}:latest" "${ASSISTED_SERVICE_IMAGE}:${TAG}"
+#docker build -t "${ASSISTED_SERVICE_IMAGE}:latest" -f Dockerfile.assisted-service .
+#docker tag "${ASSISTED_SERVICE_IMAGE}:latest" "${ASSISTED_SERVICE_IMAGE}:${TAG}"
 
-#BM_INVENTORY_BUILD_IMAGE="quay.io/app-sre/assisted-service-build"
+#ASSISTED_SERVICE_BUILD_IMAGE="quay.io/app-sre/assisted-service-build"
 #
-#docker build -t "${BM_INVENTORY_BUILD_IMAGE}:latest" -f Dockerfile.assisted-service-build .
-#docker tag "${BM_INVENTORY_BUILD_IMAGE}:latest" "${BM_INVENTORY_BUILD_IMAGE}:${TAG}"
+#docker build -t "${ASSISTED_SERVICE_BUILD_IMAGE}:latest" -f Dockerfile.assisted-service-build .
+#docker tag "${ASSISTED_SERVICE_BUILD_IMAGE}:latest" "${ASSISTED_SERVICE_BUILD_IMAGE}:${TAG}"
 
 #OBJ_EXPIRER_IMAGE="quay.io/app-sre/s3-object-expirer"
 #
@@ -23,11 +23,11 @@ DOCKER_CONF="${PWD}/.docker"
 mkdir -p "${DOCKER_CONF}"
 docker --config="${DOCKER_CONF}" login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io
 
-docker --config="${DOCKER_CONF}" push "${BM_INVENTORY_IMAGE}:latest"
-docker --config="${DOCKER_CONF}" push "${BM_INVENTORY_IMAGE}:${TAG}"
+docker --config="${DOCKER_CONF}" push "${ASSISTED_SERVICE_IMAGE}:latest"
+docker --config="${DOCKER_CONF}" push "${ASSISTED_SERVICE_IMAGE}:${TAG}"
 
-#docker --config="${DOCKER_CONF}" push "${BM_INVENTORY_BUILD_IMAGE}:latest"
-#docker --config="${DOCKER_CONF}" push "${BM_INVENTORY_BUILD_IMAGE}:${TAG}"
+#docker --config="${DOCKER_CONF}" push "${ASSISTED_SERVICE_BUILD_IMAGE}:latest"
+#docker --config="${DOCKER_CONF}" push "${ASSISTED_SERVICE_BUILD_IMAGE}:${TAG}"
 #
 #docker --config="${DOCKER_CONF}" push "${OBJ_EXPIRER_IMAGE}:latest"
 #docker --config="${DOCKER_CONF}" push "${OBJ_EXPIRER_IMAGE}:${TAG}"
