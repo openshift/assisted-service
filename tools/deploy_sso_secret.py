cc25ba700dea085995681aca37c65fd586c2d623
Nick Carboni <ncarboni@redhat.com>
2020-08-07 14:20:03 -0400

    Create empty secret when no auth information is provided

--- tools/deploy_sso_secret.py
+++ tools/deploy_sso_secret.py
@@ -9,16 +9,21 @@ parser.add_argument("--id")
 deploy_options = deployment_options.load_deployment_options(parser)
 
 def main():
-    if deploy_options.secret is "" or deploy_options.id is "":
-        return
+    ocm_secret = deploy_options.secret
+    if ocm_secret == "":
+        ocm_secret = '""'
+    ocm_id = deploy_options.id
+    if ocm_id == "":
+        ocm_id = '""'
+
     src_file = os.path.join(os.getcwd(), "deploy/assisted-installer-sso.yaml")
     dst_file = os.path.join(os.getcwd(), "build/assisted-installer-sso.yaml")
     with open(src_file, "r") as src:
         with open(dst_file, "w+") as dst:
             data = src.read()
             data = data.replace('REPLACE_NAMESPACE', deploy_options.namespace)
-            data = data.replace('REPLACE_OCM_SECRET', deploy_options.secret)
-            data = data.replace('REPLACE_OCM_ID', deploy_options.id)
+            data = data.replace('REPLACE_OCM_SECRET', ocm_secret)
+            data = data.replace('REPLACE_OCM_ID', ocm_id)
             print("Deploying {}".format(dst_file))
             dst.write(data)
 
