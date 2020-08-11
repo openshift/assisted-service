b39ec5653a915e8fc46b85ad15fae89959ca6901
Yevgeny Shnaidman <yshnaidm@redhat.com>
2020-08-11 12:05:36 +0300

    MGMT-1764 - move ignition generation to assisted-ignition-generator

--- tools/deploy_assisted_installer_configmap.py
+++ tools/deploy_assisted_installer_configmap.py
@@ -43,7 +43,7 @@ def main():
 
             versions = {"IMAGE_BUILDER": "installer-image-build",
                         "AGENT_DOCKER_IMAGE": "agent",
-                        "KUBECONFIG_GENERATE_IMAGE": "assisted-ignition-generator",
+                        "IGNITION_GENERATE_IMAGE": "assisted-ignition-generator",
                         "INSTALLER_IMAGE": "assisted-installer",
                         "CONTROLLER_IMAGE": "assisted-installer-controller",
                         "CONNECTIVITY_CHECK_IMAGE": "connectivity_check",
