a7c12fbcc09ff19e24986588c28804c72379ea91
Raz <rregev@redhat.com>
2020-08-19 18:47:43 +0300

    MGMT-1245: deploy scality - check for existing pvc size
    Use the logic of pvc_size_utils.py to override the storage size
    of scality PVC if the existing storage value is larger then the
    default.

--- tools/deploy_s3.py
+++ tools/deploy_s3.py
@@ -1,5 +1,6 @@
 import utils
 import deployment_options
+import pvc_size_utils
 
 
 log = utils.get_logger('deploy_s3')
@@ -33,6 +34,13 @@ def deploy_scality_storage(deploy_options):
     docs = utils.load_yaml_file_docs('deploy/s3/scality-storage.yaml')
 
     utils.set_namespace_in_yaml_docs(docs, deploy_options.namespace)
+
+    log.info('Updating pvc size for scality-pv-claim')
+    pvc_size_utils.update_size_in_yaml_docs(
+        ns=deploy_options.namespace,
+        name='scality-pv-claim',
+        docs=docs
+    )
 
     dst_file = utils.dump_yaml_file_docs('build/scality-storage.yaml', docs)
 
