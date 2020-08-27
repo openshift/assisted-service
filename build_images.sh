5482f9792ea155906d526f232a7ad64bf9f4e7f8
Yoni Bettan <ybettan@redhat.com>
2020-08-27 13:28:10 +0300

    Added image building to pr_check.sh. (#222)
    Now we also build `assisted-service` and `assisted-iso-generator` images
    on PRs so we can discover failing builds before merging to master.
    
    Signed-off-by: Yoni Bettan <ybettan@redhat.com>

---
+++ build_images.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# required for `skipper` according to
+# https://github.com/Stratoscale/skipper/blob/upstream/README.md#python3-environment
+export LC_ALL="en_US.UTF-8"
+export LANG="en_US.UTF-8"
+
+TAG=$(git rev-parse --short=7 HEAD)
+ASSISTED_SERVICE_IMAGE=quay.io/app-sre/assisted-service
+ASSISTED_ISO_GENERATOR_IMAGE=quay.io/app-sre/assisted-iso-generator
+
+SERVICE="${ASSISTED_SERVICE_IMAGE}:latest" skipper make update-minimal
+docker tag "${ASSISTED_SERVICE_IMAGE}:latest" "${ASSISTED_SERVICE_IMAGE}:${TAG}"
+
+ISO_CREATION="${ASSISTED_ISO_GENERATOR_IMAGE}:latest" skipper make build-minimal-assisted-iso-generator-image
+docker tag "${ASSISTED_ISO_GENERATOR_IMAGE}:latest" "${ASSISTED_ISO_GENERATOR_IMAGE}:${TAG}"
