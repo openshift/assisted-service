882a49e166721e44c4d13eaa73fce2c54afb20ea
Yoni Bettan <ybettan@redhat.com>
2020-09-01 13:28:37 +0300

    Fixing build_deploy.sh. (#257)
    A variable name was changed in build_images.sh but not in
    build_deploy.sh, this fix should fix the issue.
    
    Signed-off-by: Yoni Bettan <ybettan@redhat.com>

--- build_deploy.sh
+++ build_deploy.sh
@@ -9,6 +9,6 @@ docker --config="${DOCKER_CONF}" login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay
 docker --config="${DOCKER_CONF}" push "${ASSISTED_SERVICE_IMAGE}:latest"
 docker --config="${DOCKER_CONF}" push "${ASSISTED_SERVICE_IMAGE}:${TAG}"
 
-docker --config="${DOCKER_CONF}" push "${ASSISTED_ISO_GENERATOR_IMAGE}:latest"
-docker --config="${DOCKER_CONF}" push "${ASSISTED_ISO_GENERATOR_IMAGE}:${TAG}"
+docker --config="${DOCKER_CONF}" push "${ASSISTED_ISO_CREATE_IMAGE}:latest"
+docker --config="${DOCKER_CONF}" push "${ASSISTED_ISO_CREATE_IMAGE}:${TAG}"
 
