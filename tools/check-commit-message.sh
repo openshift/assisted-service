7174ae7578f0d633b85f1426ebef91df9d17215e
Michael Hrivnak <mhrivnak@redhat.com>
2020-09-17 14:03:47 -0400

    NO-JIRA improves commit message checking
    Instead of assuming "origin" is the remote that corresponds to
    github.com/openshift/assisted-service, the script now finds the remote
    corresponding to that location. This works better for devs who use "origin" as
    the name for their fork's remote.
    
    If the commit is rejected, a message is shown citing the location of the file
    where the commit message is preserved. Previously, it looked to the dev like
    their commit message had just been thrown away.
    
    In the interest of ensuring the dev reads the error message in case of commit
    rejection, the script no longer prints the entire commit message before
    displaying the helpful error message. By not throwing a wall of text at the dev,
    they'll be more likely to see the error info itself.

--- tools/check-commit-message.sh
+++ tools/check-commit-message.sh
@@ -6,9 +6,11 @@ commit_file=${1}
 commit_message="$(cat ${commit_file})"
 valid_commit_regex='^([A-Z]+-[0-9]+|merge|no-jira)'
 
-error_msg="""Aborting commit with message: '${commit_message}'
-Your commit message is missing either a JIRA Issue ('JIRA-1111') or 'Merge'.
+error_msg="""Aborting commit.
+Your commit message is missing a prefix of either a JIRA Issue ('JIRA-1111') or 'Merge'.
 You can ignore the JIRA ticket by prefixing with 'NO-JIRA'.
+
+Your message is preserved at '${commit_file}'
 """
 
 status=$(echo "${commit_message}" | grep -iqE "${valid_commit_regex}")
