90f917630afff206c9f45d1753ddb27c44081de3
osher cohen <oscohen@localhost.localdomain>
2020-09-29 22:13:07 +0300

    OCPBUGSM-17732 Add timeout trensition to host installing status

--- internal/host/common.go
+++ internal/host/common.go
@@ -25,19 +25,20 @@ import (
 )
 
 const (
-	statusInfoDisconnected               = "Host has stopped communicating with the installation service"
-	statusInfoDisabled                   = "Host was manually disabled"
-	statusInfoDiscovering                = "Waiting for host to send hardware details"
-	statusInfoInsufficientHardware       = "Host does not meet the minimum hardware requirements"
-	statusInfoPendingForInput            = "Waiting for user input"
-	statusInfoNotReadyForInstall         = "Host cannot be installed due to failing validation(s)"
-	statusInfoKnown                      = "Host is ready to be installed"
-	statusInfoInstalling                 = "Installation is in progress"
-	statusInfoResettingPendingUserAction = "Host requires booting into the discovery image to complete resetting the installation"
-	statusInfoPreparingForInstallation   = "Host is preparing for installation"
-	statusInfoPreparingTimedOut          = "Host failed to install because its preparation took longer than expected"
-	statusInfoAbortingDueClusterErrors   = "Host is part of a cluster that failed to install"
-	statusInfoInstallationTimedOut       = "Host failed to install because its installation stage $STAGE took longer than expected $MAX_TIME"
+	statusInfoDisconnected                   = "Host has stopped communicating with the installation service"
+	statusInfoDisabled                       = "Host was manually disabled"
+	statusInfoDiscovering                    = "Waiting for host to send hardware details"
+	statusInfoInsufficientHardware           = "Host does not meet the minimum hardware requirements"
+	statusInfoPendingForInput                = "Waiting for user input"
+	statusInfoNotReadyForInstall             = "Host cannot be installed due to failing validation(s)"
+	statusInfoKnown                          = "Host is ready to be installed"
+	statusInfoInstalling                     = "Installation is in progress"
+	statusInfoResettingPendingUserAction     = "Host requires booting into the discovery image to complete resetting the installation"
+	statusInfoPreparingForInstallation       = "Host is preparing for installation"
+	statusInfoPreparingTimedOut              = "Host failed to install because its preparation took longer than expected"
+	statusInfoAbortingDueClusterErrors       = "Host is part of a cluster that failed to install"
+	statusInfoInstallationTimedOut           = "Host failed to install due to timeout while starting installation"
+	statusInfoInstallationInProgressTimedOut = "Host failed to install because its installation stage $STAGE took longer than expected $MAX_TIME"
 )
 
 type UpdateReply struct {
--- internal/host/statemachine.go
+++ internal/host/statemachine.go
@@ -247,10 +247,20 @@ func NewHostStateMachine(th *transitionHandler) stateswitch.StateMachine {
 	sm.AddTransition(stateswitch.TransitionRule{
 		TransitionType: TransitionTypeRefresh,
 		SourceStates: []stateswitch.State{
-			stateswitch.State(models.HostStatusInstallingInProgress)},
+			stateswitch.State(models.HostStatusInstalling)},
 		Condition:        stateswitch.And(th.HasInstallationTimedOut),
 		DestinationState: stateswitch.State(models.HostStatusError),
 		PostTransition:   th.PostRefreshHost(statusInfoInstallationTimedOut),
+	})
+
+	// Time out while host installationInProgress
+	sm.AddTransition(stateswitch.TransitionRule{
+		TransitionType: TransitionTypeRefresh,
+		SourceStates: []stateswitch.State{
+			stateswitch.State(models.HostStatusInstallingInProgress)},
+		Condition:        stateswitch.And(th.HasInstallationInProgressTimedOut),
+		DestinationState: stateswitch.State(models.HostStatusError),
+		PostTransition:   th.PostRefreshHost(statusInfoInstallationInProgressTimedOut),
 	})
 
 	// Noop transitions for cluster error
