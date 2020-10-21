2aa4008bdce0756e5b2b94bcfaa249719e2270d4
Nick Carboni <ncarboni@redhat.com>
2020-10-21 10:46:24 -0400

    NO-ISSUE Add some helper functions for migration tests
    These will make it easier for developers to run just the migrations
    that they need to to test new migrations

--- internal/migrations/migrations_suite_test.go
+++ internal/migrations/migrations_suite_test.go
@@ -1,13 +1,43 @@
-package migrations_test
+package migrations
 
 import (
+	"fmt"
+	"sort"
 	"testing"
 
+	"github.com/jinzhu/gorm"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	gormigrate "gopkg.in/gormigrate.v1"
 )
 
 func TestMigrations(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Migrations Suite")
+}
+
+// migrateToBefore is a helper function for migration tests
+// It runs all the migrations before the given one to simplify setting up a valid test scenario
+// nolint,unused
+func migrateToBefore(db *gorm.DB, migrationID string) error {
+	allMigrations := all()
+
+	id := sort.Search(len(allMigrations), func(i int) bool { return allMigrations[i].ID >= migrationID })
+	if id == len(allMigrations) || allMigrations[id].ID != migrationID {
+		return fmt.Errorf("Failed to find migration %s in migration list", migrationID)
+	}
+
+	toRun := allMigrations[0:id]
+	if len(toRun) > 0 {
+		return gormigrate.New(db, gormigrate.DefaultOptions, allMigrations[0:id]).Migrate()
+	}
+
+	return nil
+}
+
+// migrateTo runs all migrations up to and including migrationID
+// nolint,unused
+func migrateTo(db *gorm.DB, migratoinID string) error {
+	gm := gormigrate.New(db, gormigrate.DefaultOptions, all())
+	return gm.MigrateTo(migratoinID)
 }
