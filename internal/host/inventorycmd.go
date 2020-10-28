1163586d15fb4c5e962cfe902b2115c0294a0a1d
Osher De Paz <osherdepaz@gmail.com>
2020-10-28 19:32:31 +0200

    MGMT-2592: adding mounts to all required files from host for HW detection, and direct ghw library to the new root directory

--- internal/host/inventorycmd.go
+++ internal/host/inventorycmd.go
@@ -2,6 +2,8 @@ package host
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"github.com/sirupsen/logrus"
 
@@ -21,18 +23,41 @@ func NewInventoryCmd(log logrus.FieldLogger, inventoryImage string) *inventoryCm
 }
 
 func (h *inventoryCmd) GetSteps(ctx context.Context, host *models.Host) ([]*models.Step, error) {
-	step := &models.Step{
+	podmanRunCmd := strings.Join([]string{
+		"podman", "run", "--privileged", "--net=host", "--rm", "--quiet",
+		"-v", "/var/log:/var/log",
+		"-v", "/run/udev:/run/udev",
+		"-v", "/dev/disk:/dev/disk",
+		"-v", "/run/systemd/journal/socket:/run/systemd/journal/socket",
+
+		// Enable capturing host's HW using a different root path for GHW library
+		"-v", "/var/log:/host/var/log:ro",
+		"-v", "/proc/meminfo:/host/proc/meminfo:ro",
+		"-v", "/sys/kernel/mm/hugepages:/host/sys/kernel/mm/hugepages:ro",
+		"-v", "/proc/cpuinfo:/host/proc/cpuinfo:ro",
+		"-v", "/root/mtab:/host/etc/mtab:ro",
+		"-v", "/sys/block:/host/sys/block:ro",
+		"-v", "/sys/devices:/host/sys/devices:ro",
+		"-v", "/sys/bus:/host/sys/bus:ro",
+		"-v", "/sys/class:/host/sys/class:ro",
+		"-v", "/run/udev:/host/run/udev:ro",
+		"-v", "/dev/disk:/host/dev/disk:ro",
+
+		h.inventoryImage,
+		"inventory",
+	}, " ")
+
+	// Copying mounts file, which is not available by podman's PID
+	copyMountsFileCmd := "cp /etc/mtab /root/mtab"
+
+	inventoryCmd := &models.Step{
 		StepType: models.StepTypeInventory,
-		Command:  "podman",
+		Command:  "sh",
 		Args: []string{
-			"run", "--privileged", "--net=host", "--rm", "--quiet",
-			"-v", "/var/log:/var/log",
-			"-v", "/run/udev:/run/udev",
-			"-v", "/dev/disk:/dev/disk",
-			"-v", "/run/systemd/journal/socket:/run/systemd/journal/socket",
-			h.inventoryImage,
-			"inventory",
+			"-c",
+			fmt.Sprintf("%v && %v", copyMountsFileCmd, podmanRunCmd),
 		},
 	}
-	return []*models.Step{step}, nil
+
+	return []*models.Step{inventoryCmd}, nil
 }
