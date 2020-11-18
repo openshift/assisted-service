f05e842233a4fa3d8f352768753f9af41baa306c
Nick Carboni <ncarboni@redhat.com>
2020-11-18 17:03:54 -0500

    MGMT-2982 Fix image build race condition
    Before this change it was possible for an image built for a PR
    to end up deployed to the integration environment.
    
    To fix, we should have built the initial image as the git sha
    then only if we needed to push, tag latest.
    
    The race happened like this:
    1. build for master starts
    2. build for pr starts
    3. build for master finishes and tags latest
    4. build for pr finishes and tags latest
    5. push latest
    
    Fixes MGMT-2982

--- build_deploy.sh
+++ build_deploy.sh
@@ -8,9 +8,10 @@ DOCKER_CONF="${PWD}/.docker"
 mkdir -p "${DOCKER_CONF}"
 docker --config="${DOCKER_CONF}" login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io
 
-docker --config="${DOCKER_CONF}" push "${ASSISTED_SERVICE_IMAGE}:latest"
+docker tag "${ASSISTED_SERVICE_IMAGE}:${TAG}" "${ASSISTED_SERVICE_IMAGE}:latest"
 docker --config="${DOCKER_CONF}" push "${ASSISTED_SERVICE_IMAGE}:${TAG}"
+docker --config="${DOCKER_CONF}" push "${ASSISTED_SERVICE_IMAGE}:latest"
 
-docker --config="${DOCKER_CONF}" push "${ASSISTED_ISO_CREATE_IMAGE}:latest"
+docker tag "${ASSISTED_ISO_CREATE_IMAGE}:${TAG}" "${ASSISTED_ISO_CREATE_IMAGE}:latest"
 docker --config="${DOCKER_CONF}" push "${ASSISTED_ISO_CREATE_IMAGE}:${TAG}"
-
+docker --config="${DOCKER_CONF}" push "${ASSISTED_ISO_CREATE_IMAGE}:latest"
