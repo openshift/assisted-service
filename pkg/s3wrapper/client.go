46568eab73cbbae267932a19f4c7b9b895090ef9
Igal Tsoiref <itsoiref@redhat.com>
2020-11-22 15:08:46 +0200

    MGTM-2834 assisted-service on-prem reset doesn't clean old cluster state file (#711)

--- pkg/s3wrapper/client.go
+++ pkg/s3wrapper/client.go
@@ -234,7 +234,7 @@ func (c *S3Client) DeleteObject(ctx context.Context, objectName string) error {
 	if err != nil {
 		if aerr, ok := err.(awserr.Error); ok {
 			if aerr.Code() == s3.ErrCodeNoSuchKey || aerr.Code() == "NotFound" {
-				log.Warnf("Object %s does not exist in bucket %s", objectName, c.cfg.S3Bucket)
+				log.Infof("Object %s does not exist in bucket %s", objectName, c.cfg.S3Bucket)
 				return nil
 			}
 			return errors.Wrap(err, fmt.Sprintf("Failed to delete object %s from bucket %s (code %s)", objectName, c.cfg.S3Bucket, aerr.Code()))
