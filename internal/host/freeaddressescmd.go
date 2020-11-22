bfbda4845c24844d86f393c3c81dcd2ed5a5634e
Omer Tuchfeld <otuchfel@redhat.com>
2020-11-23 01:52:52 +0200

    MGMT-2470 silently fail if free addresses container already running

--- internal/host/freeaddressescmd.go
+++ internal/host/freeaddressescmd.go
@@ -3,12 +3,13 @@ package host
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net"
 
-	"github.com/sirupsen/logrus"
-
+	"github.com/alessio/shellescape"
 	"github.com/openshift/assisted-service/models"
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 )
 
 type freeAddressesCmd struct {
@@ -64,18 +65,33 @@ func (f *freeAddressesCmd) GetSteps(ctx context.Context, host *models.Host) ([]*
 	if err != nil {
 		return nil, err
 	}
+
+	const containerName = "free_addresses_scanner"
+
+	podmanRunCmd := shellescape.QuoteCommand([]string{
+		"podman", "run", "--privileged", "--net=host", "--rm", "--quiet",
+		"--name", containerName,
+		"-v", "/var/log:/var/log",
+		"-v", "/run/systemd/journal/socket:/run/systemd/journal/socket",
+		f.freeAddressesImage,
+		"free_addresses",
+		param,
+	})
+
+	// Sometimes the address scanning takes longer than the interval we wait between invocations.
+	// To avoid flooding the log with "container already exists" errors, we silently fail by manually
+	// checking if it exists and only running if it doesn't
+	checkAlreadyRunningCmd := fmt.Sprintf("podman ps --format '{{.Names}}' | grep -q '^%s$'", containerName)
+
 	step := &models.Step{
 		StepType: models.StepTypeFreeNetworkAddresses,
-		Command:  "podman",
+		Command:  "sh",
 		Args: []string{
-			"run", "--privileged", "--net=host", "--rm", "--quiet",
-			"--name", "free_addresses_scanner",
-			"-v", "/var/log:/var/log",
-			"-v", "/run/systemd/journal/socket:/run/systemd/journal/socket",
-			f.freeAddressesImage,
-			"free_addresses",
-			param,
+			"-c",
+			fmt.Sprintf("%s || %s", checkAlreadyRunningCmd, podmanRunCmd),
 		},
 	}
+
 	return []*models.Step{step}, nil
+
 }
