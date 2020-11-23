ea25e6b734dd73d25d1583e5bcf2210d6a1b9383
Raz Regev <rregev@redhat.com>
2020-11-23 15:50:32 +0200

    MGMT-2411: Added error_utils.go (#721)
    Provides a work around for the error format bug returned
    from the assisted service.
    GetAssistedError function receives error and manually build
    the message from Payload by the error type which is a struct
    of pointers.
    Will be used by assisted installer client when logging the
    error messages.

---
+++ pkg/error/error_utils.go
@@ -0,0 +1,48 @@
+package error
+
+import (
+	"github.com/pkg/errors"
+
+	"github.com/go-openapi/swag"
+	"github.com/openshift/assisted-service/models"
+)
+
+type AssistedServiceErrorAPI interface {
+	Error() string
+	GetPayload() *models.Error
+}
+
+type AssistedServiceInfraErrorAPI interface {
+	Error() string
+	GetPayload() *models.InfraError
+}
+
+func assistedErrorToError(err AssistedServiceErrorAPI) error {
+	payload := err.GetPayload()
+	return errors.Errorf(
+		"AssistedServiceError Code: %s Href: %s ID: %d Kind: %s Reason: %s",
+		swag.StringValue(payload.Code),
+		swag.StringValue(payload.Href),
+		swag.Int32Value(payload.ID),
+		swag.StringValue(payload.Kind),
+		swag.StringValue(payload.Reason))
+}
+
+func infraErrorToError(err AssistedServiceInfraErrorAPI) error {
+	payload := err.GetPayload()
+	return errors.Errorf(
+		"AssistedServiceInfraError Code: %d Message: %s",
+		swag.Int32Value(payload.Code),
+		swag.StringValue(payload.Message))
+}
+
+func GetAssistedError(err error) error {
+	switch err := err.(type) {
+	case AssistedServiceErrorAPI:
+		return assistedErrorToError(err)
+	case AssistedServiceInfraErrorAPI:
+		return infraErrorToError(err)
+	default:
+		return err
+	}
+}
