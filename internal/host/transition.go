b0fc732974a024a10b447fbd27ec72c6d8210bfd
Yuval Goldberg <yuvigoldi@gmail.com>
2020-11-25 15:38:16 +0200

    MGMT-2706 Delete console kernel argument

--- internal/host/transition.go
+++ internal/host/transition.go
@@ -74,6 +74,7 @@ func (th *transitionHandler) PostRegisterHost(sw stateswitch.StateSwitch, args s
 
 	sHost.host.StatusUpdatedAt = strfmt.DateTime(time.Now())
 	sHost.host.StatusInfo = swag.String(statusInfoDiscovering)
+	sHost.host.InstallerArgs = "--delete-karg 'console=tty0'"
 	log.Infof("Register new host %s cluster %s", sHost.host.ID.String(), sHost.host.ClusterID)
 	return params.db.Create(sHost.host).Error
 }
