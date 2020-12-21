1eb69f0a391fa9f511f62f71e49b1850459b2f73
osher cohen <oscohen@localhost.localdomain>
2020-12-21 09:31:44 +0200

    NO-ISSUE Add host count to InstallationStart metric

--- internal/bminventory/inventory_test.go
+++ internal/bminventory/inventory_test.go
@@ -1493,6 +1493,7 @@ var _ = Describe("cluster", func() {
 	}
 	setDefaultMetricInstallationStarted := func(mockMetricApi *metrics.MockAPI) {
 		mockMetricApi.EXPECT().InstallationStarted(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes()
+		mockMetricApi.EXPECT().ClusterHostInstallationCount(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes()
 	}
 	mockHandlePreInstallationError := func(mockClusterApi *cluster.MockAPI, done chan int) {
 		mockClusterApi.EXPECT().HandlePreInstallError(gomock.Any(), gomock.Any(), gomock.Any()).Times(1).
@@ -2668,6 +2669,7 @@ var _ = Describe("cluster", func() {
 			})
 
 			It("success", func() {
+
 				mockAutoAssignSuccess(3)
 				mockClusterRefreshStatusSuccess()
 				mockClusterIsReadyForInstallationSuccess()
