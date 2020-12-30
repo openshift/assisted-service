cdd01d086744077320c0778bd825a3f6062c87fd
Yuval Goldberg <yuvigoldi@hotmail.com>
2020-12-30 12:39:28 +0200

    Revert "MGMT-2245 Extract OCP version from OCP images (#820)" (#840)
    This reverts commit 4634b379daba648d2ce3b649bec28ea067e53a85.

--- internal/oc/release_test.go
+++ internal/oc/release_test.go
@@ -1,17 +1,13 @@
 package oc
 
 import (
-	"fmt"
-	"io/ioutil"
-	os "os"
+	"os"
+	"os/exec"
 	"path/filepath"
-	"strings"
 	"testing"
 
-	gomock "github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/openshift/assisted-service/pkg/executer"
 	logrus "github.com/sirupsen/logrus"
 )
 
@@ -21,190 +17,71 @@ var (
 	releaseImageMirror = "release_image_mirror"
 	cacheDir           = "/tmp"
 	pullSecret         = "pull secret"
-	fullVersion        = "4.6.0-0.nightly-2020-08-31-220837"
-	mcoImage           = "mco_image"
 )
 
 var _ = Describe("oc", func() {
 	var (
-		oc                 Release
-		tempPullSecretFile *os.File
-		ctrl               *gomock.Controller
-		mockExecuter       *executer.MockExecuter
+		oc Release
 	)
 
 	BeforeEach(func() {
-		ctrl = gomock.NewController(GinkgoT())
-		mockExecuter = executer.NewMockExecuter(ctrl)
-		oc = NewRelease(mockExecuter)
-
-		var err error
-		tempPullSecretFile, err = ioutil.TempFile("", "")
-		Expect(err).ShouldNot(HaveOccurred())
-		mockExecuter.EXPECT().TempFile(gomock.Any(), gomock.Any()).Return(tempPullSecretFile, nil).AnyTimes()
+		oc = NewRelease()
+		execCommand = fakeExecCommand
 	})
 
 	AfterEach(func() {
-		os.Remove(tempPullSecretFile.Name())
+		execCommand = exec.Command
 	})
 
-	Context("GetMCOImage", func() {
-		It("mco image from release image", func() {
-			command := fmt.Sprintf(templateGetMCOImage+" --registry-config=%s",
-				false, releaseImage, tempPullSecretFile.Name())
-			args := splitStringToInterfacesArray(command)
-			mockExecuter.EXPECT().Execute(args[0], args[1:]...).Return(mcoImage, "", 0).Times(1)
-
-			mco, err := oc.GetMCOImage(log, releaseImage, "", pullSecret)
-			Expect(mco).Should(Equal(mcoImage))
-			Expect(err).ShouldNot(HaveOccurred())
-		})
-
-		It("mco image from release image mirror", func() {
-			command := fmt.Sprintf(templateGetMCOImage+" --registry-config=%s",
-				true, releaseImageMirror, tempPullSecretFile.Name())
-			args := splitStringToInterfacesArray(command)
-			mockExecuter.EXPECT().Execute(args[0], args[1:]...).Return(mcoImage, "", 0).Times(1)
-
-			mco, err := oc.GetMCOImage(log, releaseImage, releaseImageMirror, pullSecret)
-			Expect(mco).Should(Equal(mcoImage))
-			Expect(err).ShouldNot(HaveOccurred())
-		})
-
-		It("mco image with no release image or mirror", func() {
-			mco, err := oc.GetMCOImage(log, "", "", pullSecret)
-			Expect(mco).Should(BeEmpty())
-			Expect(err).Should(HaveOccurred())
-		})
+	It("mco image from release image", func() {
+		mco, err := oc.GetMCOImage(log, releaseImage, "", pullSecret)
+		Expect(mco).ShouldNot(BeEmpty())
+		Expect(err).ShouldNot(HaveOccurred())
 	})
 
-	Context("GetOpenshiftVersion", func() {
-		It("image version from release image", func() {
-			command := fmt.Sprintf(templateGetVersion+" --registry-config=%s",
-				false, releaseImage, tempPullSecretFile.Name())
-			args := splitStringToInterfacesArray(command)
-			mockExecuter.EXPECT().Execute(args[0], args[1:]...).Return(fullVersion, "", 0).Times(1)
-
-			version, err := oc.GetOpenshiftVersion(log, releaseImage, "", pullSecret)
-			Expect(version).Should(Equal(fullVersion))
-			Expect(err).ShouldNot(HaveOccurred())
-		})
-
-		It("image version from release image mirror", func() {
-			command := fmt.Sprintf(templateGetVersion+" --registry-config=%s",
-				true, releaseImageMirror, tempPullSecretFile.Name())
-			args := splitStringToInterfacesArray(command)
-			mockExecuter.EXPECT().Execute(args[0], args[1:]...).Return(fullVersion, "", 0).Times(1)
-
-			version, err := oc.GetOpenshiftVersion(log, releaseImage, releaseImageMirror, pullSecret)
-			Expect(version).Should(Equal(fullVersion))
-			Expect(err).ShouldNot(HaveOccurred())
-		})
-
-		It("image version with no release image or mirror", func() {
-			version, err := oc.GetOpenshiftVersion(log, "", "", pullSecret)
-			Expect(version).Should(BeEmpty())
-			Expect(err).Should(HaveOccurred())
-		})
+	It("mco image from release image mirror", func() {
+		mco, err := oc.GetMCOImage(log, releaseImage, releaseImageMirror, pullSecret)
+		Expect(mco).ShouldNot(BeEmpty())
+		Expect(err).ShouldNot(HaveOccurred())
 	})
 
-	Context("GetMajorMinorVersion", func() {
-		tests := []struct {
-			fullVersion  string
-			shortVersion string
-			isValid      bool
-		}{
-			{
-				fullVersion:  "4.6.0",
-				shortVersion: "4.6",
-				isValid:      true,
-			},
-			{
-				fullVersion:  "4.6.4",
-				shortVersion: "4.6",
-				isValid:      true,
-			},
-			{
-				fullVersion:  "4.6",
-				shortVersion: "4.6",
-				isValid:      true,
-			},
-			{
-				fullVersion:  "4.6.0-0.nightly-2020-08-31-220837",
-				shortVersion: "4.6",
-				isValid:      true,
-			},
-			{
-				fullVersion: "-44",
-				isValid:     false,
-			},
-		}
-
-		for i := range tests {
-			t := tests[i]
-			It(t.fullVersion, func() {
-				command := fmt.Sprintf(templateGetVersion+" --registry-config=%s",
-					false, releaseImage, tempPullSecretFile.Name())
-				args := splitStringToInterfacesArray(command)
-				mockExecuter.EXPECT().Execute(args[0], args[1:]...).Return(t.fullVersion, "", 0).Times(1)
-
-				version, err := oc.GetMajorMinorVersion(log, releaseImage, "", pullSecret)
-
-				if t.isValid {
-					Expect(err).ShouldNot(HaveOccurred())
-					Expect(version).Should(Equal(t.shortVersion))
-				} else {
-					Expect(err).Should(HaveOccurred())
-					Expect(version).Should(BeEmpty())
-				}
-			})
-		}
+	It("mco image with no release image or mirror", func() {
+		mco, err := oc.GetMCOImage(log, "", "", pullSecret)
+		Expect(mco).Should(BeEmpty())
+		Expect(err).Should(HaveOccurred())
 	})
 
-	Context("Extract", func() {
-		It("extract baremetal-install from release image", func() {
-			command := fmt.Sprintf(templateExtract+" --registry-config=%s",
-				filepath.Join(cacheDir, releaseImage), false, releaseImage, tempPullSecretFile.Name())
-			args := splitStringToInterfacesArray(command)
-			mockExecuter.EXPECT().Execute(args[0], args[1:]...).Return("", "", 0).Times(1)
-
-			path, err := oc.Extract(log, releaseImage, "", cacheDir, pullSecret)
-			filePath := filepath.Join(cacheDir+"/"+releaseImage, "openshift-baremetal-install")
-			Expect(path).To(Equal(filePath))
-			Expect(err).ShouldNot(HaveOccurred())
-		})
-
-		It("extract baremetal-install from release image mirror", func() {
-			command := fmt.Sprintf(templateExtract+" --registry-config=%s",
-				filepath.Join(cacheDir, releaseImageMirror), true, releaseImageMirror, tempPullSecretFile.Name())
-			args := splitStringToInterfacesArray(command)
-			mockExecuter.EXPECT().Execute(args[0], args[1:]...).Return("", "", 0).Times(1)
-
-			path, err := oc.Extract(log, releaseImage, releaseImageMirror, cacheDir, pullSecret)
-			filePath := filepath.Join(cacheDir+"/"+releaseImageMirror, "openshift-baremetal-install")
-			Expect(path).To(Equal(filePath))
-			Expect(err).ShouldNot(HaveOccurred())
-		})
-
-		It("extract baremetal-install with no release image or mirror", func() {
-			path, err := oc.Extract(log, "", "", cacheDir, pullSecret)
-			Expect(path).Should(BeEmpty())
-			Expect(err).Should(HaveOccurred())
-		})
+	It("extract baremetal-install from release image", func() {
+		path, err := oc.Extract(log, releaseImage, "", cacheDir, pullSecret)
+		filePath := filepath.Join(cacheDir+"/"+releaseImage, "openshift-baremetal-install")
+		Expect(path).To(Equal(filePath))
+		Expect(err).ShouldNot(HaveOccurred())
 	})
+
+	It("extract baremetal-install from release image mirror", func() {
+		path, err := oc.Extract(log, releaseImage, releaseImageMirror, cacheDir, pullSecret)
+		filePath := filepath.Join(cacheDir+"/"+releaseImageMirror, "openshift-baremetal-install")
+		Expect(path).To(Equal(filePath))
+		Expect(err).ShouldNot(HaveOccurred())
+	})
+
+	It("extract baremetal-install with no release image or mirror", func() {
+		path, err := oc.Extract(log, "", "", cacheDir, pullSecret)
+		Expect(path).Should(BeEmpty())
+		Expect(err).Should(HaveOccurred())
+	})
+
 })
 
-func splitStringToInterfacesArray(str string) []interface{} {
-	argsAsString := strings.Split(str, " ")
-	argsAsInterface := make([]interface{}, len(argsAsString))
-	for i, v := range argsAsString {
-		argsAsInterface[i] = v
-	}
-
-	return argsAsInterface
+func fakeExecCommand(command string, args ...string) *exec.Cmd {
+	cs := []string{"-test.run=TestHelperProcess", "--", command}
+	cs = append(cs, args...)
+	cmd := exec.Command(os.Args[0], cs...) //nolint:gosec
+	cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
+	return cmd
 }
 
-func TestOC(t *testing.T) {
+func TestSubsystem(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "oc tests")
 }
