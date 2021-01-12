7aa360c9ac4ad66c1afddaf20b18a484a1070268
Michael Filanov <41726511+filanov@users.noreply.github.com>
2021-01-12 18:39:26 +0200

    MGMT-3434 Support cluster update with k8s API (#891)
    Reconcile existing cluster every 10 seconds to the a new status from the backend,
    it's a temporary solution until we implement notifications from the backend
    In case of api error we requeue the request after 10 seconds.

--- internal/controller/api/v1alpha1/cluster_types.go
+++ internal/controller/api/v1alpha1/cluster_types.go
@@ -17,8 +17,9 @@ limitations under the License.
 package v1alpha1
 
 import (
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	corev1 "k8s.io/api/core/v1"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
