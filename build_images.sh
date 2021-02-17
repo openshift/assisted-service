448ad6e32e97c8641e5c82125df094031209ec6c
Yuval Goldberg <yuvigoldi@gmail.com>
2021-02-17 11:22:40 +0200

    NO-ISSUE Replace OpenShift CI golang image with AppSRE golang image

--- build_images.sh
+++ build_images.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e
+set -o nounset
+set -o pipefail
+set -o errexit
+set -o xtrace
 
 # required for `skipper` according to
 # https://github.com/Stratoscale/skipper/blob/upstream/README.md#python3-environment
@@ -9,5 +12,12 @@ export LANG="en_US.UTF-8"
 
 TAG=$(git rev-parse --short=7 HEAD)
 ASSISTED_SERVICE_IMAGE=quay.io/app-sre/assisted-service
+
+GOLANG_ORIGINAL_IMAGE=registry.svc.ci.openshift.org/openshift/release:golang-1.15
+GOLANG_APPSRE_IMAGE=quay.io/app-sre/golang
+
+docker login quay.io/app-sre
+sed -i "s|FROM ${GOLANG_ORIGINAL_IMAGE}|FROM ${GOLANG_APPSRE_IMAGE}|" Dockerfile.assisted-service-build
+sed -i "s|FROM ${GOLANG_ORIGINAL_IMAGE}|FROM ${GOLANG_APPSRE_IMAGE}|" Dockerfile.assisted-service
 
 SERVICE="${ASSISTED_SERVICE_IMAGE}:${TAG}" skipper make update-minimal
--- pr_check.sh
+++ pr_check.sh
@@ -2,12 +2,5 @@
 
 set -e
 
-IMAGE_TEST=assisted-service-test
-
-# run some 'assisted-service' checks inside a golang container
-docker build -t ${IMAGE_TEST} -f Dockerfile.test .
-docker run --rm ${IMAGE_TEST}
-
 # build app-sre image to make sure they are build without errors before merging to master
 source build_images.sh
-
