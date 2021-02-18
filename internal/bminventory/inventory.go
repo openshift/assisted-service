a938d4cf51e453d0d2dae7ce0068616fa4acdbb8
Raz Regev <rregev@redhat.com>
2021-02-18 09:54:41 +0200

    MGMT-3370: Added error logs to RegisterClusterInternal (#1113)
    Some validation failures are not printed to log on cluster
    registration, but just returned as part of the api response.
    Since we use the internal registration logic in the cluster
    controller we can miss some errors that are not logged in
    UpdateClusterInternal.
    In this PR I updated the error logging in case of cluster
    registration failure and made sure the error will be printed.

--- internal/bminventory/inventory.go
+++ internal/bminventory/inventory.go
@@ -557,6 +557,7 @@ func (b *bareMetalInventory) RegisterClusterInternal(
 	url := installer.GetClusterURL{ClusterID: id}
 	log.Infof("Register cluster: %s with id %s", swag.StringValue(params.NewClusterParams.Name), id)
 	success := false
+	var err error
 	defer func() {
 		if success {
 			msg := fmt.Sprintf("Successfully registered cluster %s with id %s",
@@ -564,7 +565,11 @@ func (b *bareMetalInventory) RegisterClusterInternal(
 			log.Info(msg)
 			b.eventsHandler.AddEvent(ctx, id, nil, models.EventSeverityInfo, msg, time.Now())
 		} else {
-			log.Errorf("Failed to registered cluster %s with id %s",
+			errWrapperLog := log
+			if err != nil {
+				errWrapperLog = log.WithError(err)
+			}
+			errWrapperLog.Errorf("Failed to registered cluster %s with id %s",
 				swag.StringValue(params.NewClusterParams.Name), id)
 		}
 	}()
@@ -574,7 +579,7 @@ func (b *bareMetalInventory) RegisterClusterInternal(
 		params.NewClusterParams.HTTPSProxy = params.NewClusterParams.HTTPProxy
 	}
 
-	if err := validateProxySettings(params.NewClusterParams.HTTPProxy,
+	if err = validateProxySettings(params.NewClusterParams.HTTPProxy,
 		params.NewClusterParams.HTTPSProxy,
 		params.NewClusterParams.NoProxy); err != nil {
 		return nil, common.NewApiError(http.StatusBadRequest, err)
@@ -584,22 +589,19 @@ func (b *bareMetalInventory) RegisterClusterInternal(
 
 	if swag.BoolValue(params.NewClusterParams.UserManagedNetworking) {
 		if swag.BoolValue(params.NewClusterParams.VipDhcpAllocation) {
-			err := errors.Errorf("VIP DHCP Allocation cannot be enabled with User Managed Networking")
-			log.WithError(err)
+			err = errors.Errorf("VIP DHCP Allocation cannot be enabled with User Managed Networking")
 			return nil, common.NewApiError(http.StatusBadRequest, err)
 		}
 		if params.NewClusterParams.IngressVip != "" {
-			err := errors.Errorf("Ingress VIP cannot be set with User Managed Networking")
-			log.WithError(err)
+			err = errors.Errorf("Ingress VIP cannot be set with User Managed Networking")
 			return nil, common.NewApiError(http.StatusBadRequest, err)
 		}
 	}
 
 	if swag.StringValue(params.NewClusterParams.HighAvailabilityMode) == models.ClusterHighAvailabilityModeNone {
 		// verify minimal OCP version
-		err := verifyMinimalOpenShiftVersionForSingleNode(swag.StringValue(params.NewClusterParams.OpenshiftVersion))
+		err = verifyMinimalOpenShiftVersionForSingleNode(swag.StringValue(params.NewClusterParams.OpenshiftVersion))
 		if err != nil {
-			log.WithError(err)
 			return nil, common.NewApiError(http.StatusBadRequest, err)
 		}
 		log.Infof("HA mode is None, seeting UserManagedNetworking to true ")
@@ -613,16 +615,15 @@ func (b *bareMetalInventory) RegisterClusterInternal(
 		ntpSource := swag.StringValue(params.NewClusterParams.AdditionalNtpSource)
 
 		if ntpSource != "" && !validations.ValidateAdditionalNTPSource(ntpSource) {
-			err := errors.Errorf("Invalid NTP source: %s", ntpSource)
-			log.WithError(err)
+			err = errors.Errorf("Invalid NTP source: %s", ntpSource)
 			return nil, common.NewApiError(http.StatusBadRequest, err)
 		}
 	}
 
 	if !b.versionsHandler.IsOpenshiftVersionSupported(swag.StringValue(params.NewClusterParams.OpenshiftVersion)) {
-		return nil, common.NewApiError(http.StatusBadRequest,
-			errors.Errorf("Openshift version %s is not supported",
-				swag.StringValue(params.NewClusterParams.OpenshiftVersion)))
+		err = errors.Errorf("Openshift version %s is not supported",
+			swag.StringValue(params.NewClusterParams.OpenshiftVersion))
+		return nil, common.NewApiError(http.StatusBadRequest, err)
 	}
 
 	if kubeKey == nil {
@@ -659,19 +660,20 @@ func (b *bareMetalInventory) RegisterClusterInternal(
 		KubeKeyNamespace: kubeKey.Namespace,
 	}
 
-	if proxyHash, err := computeClusterProxyHash(params.NewClusterParams.HTTPProxy,
+	proxyHash, err := computeClusterProxyHash(params.NewClusterParams.HTTPProxy,
 		params.NewClusterParams.HTTPSProxy,
-		params.NewClusterParams.NoProxy); err != nil {
-		log.Error("Failed to compute cluster proxy hash", err)
+		params.NewClusterParams.NoProxy)
+	if err != nil {
+		err = errors.Wrapf(err, "failed to compute cluster proxy hash")
 		return nil, common.NewApiError(http.StatusInternalServerError, errors.Errorf("Failed to compute cluster proxy hash"))
 	} else {
 		cluster.ProxyHash = proxyHash
 	}
 
 	pullSecret := swag.StringValue(params.NewClusterParams.PullSecret)
-	err := b.secretValidator.ValidatePullSecret(pullSecret, ocm.UserNameFromContext(ctx), b.authHandler)
+	err = b.secretValidator.ValidatePullSecret(pullSecret, ocm.UserNameFromContext(ctx), b.authHandler)
 	if err != nil {
-		log.WithError(err).Errorf("Pull secret for new cluster is invalid")
+		err = errors.Wrap(err, "pull secret for new cluster is invalid")
 		return nil, common.NewApiError(http.StatusBadRequest, secretValidationToUserError(err))
 	}
 	ps, err := b.updatePullSecret(pullSecret, log)
@@ -695,13 +697,12 @@ func (b *bareMetalInventory) RegisterClusterInternal(
 
 	err = b.clusterApi.RegisterCluster(ctx, &cluster)
 	if err != nil {
-		log.Errorf("failed to register cluster %s ", swag.StringValue(params.NewClusterParams.Name))
 		return nil, common.NewApiError(http.StatusInternalServerError, err)
 	}
 
 	if b.ocmClient != nil && b.Config.WithAMSSubscriptions {
-		if err := b.integrateWithAMSClusterRegistration(ctx, &cluster); err != nil {
-			log.WithError(err).Errorf("Cluster %s failed to integrate with AMS on cluster registration", id)
+		if err = b.integrateWithAMSClusterRegistration(ctx, &cluster); err != nil {
+			err = errors.Wrapf(err, "cluster %s failed to integrate with AMS on cluster registration", id)
 			return nil, common.NewApiError(http.StatusInternalServerError, err)
 		}
 	}
