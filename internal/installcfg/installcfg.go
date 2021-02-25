7a085b2cec372dd59a429f244d80ebef959ebeff
Igal Tsoiref <itsoiref@redhat.com>
2021-02-25 16:24:45 +0200

    MGMT-4325 Remove bootstrap in place field from not sno install cfg

--- internal/installcfg/installcfg.go
+++ internal/installcfg/installcfg.go
@@ -81,7 +81,7 @@ type InstallerConfigBaremetal struct {
 		Replicas       int    `yaml:"replicas"`
 	} `yaml:"controlPlane"`
 	Platform              platform         `yaml:"platform"`
-	BootstrapInPlace      bootstrapInPlace `yaml:"bootstrapInPlace"`
+	BootstrapInPlace      bootstrapInPlace `yaml:"bootstrapInPlace,omitempty"`
 	FIPS                  bool             `yaml:"fips"`
 	PullSecret            string           `yaml:"pullSecret"`
 	SSHKey                string           `yaml:"sshKey"`
