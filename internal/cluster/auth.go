297183cf2efaa5bd2d509962c308aaf36db47166
Nick Carboni <ncarboni@redhat.com>
2021-03-09 15:11:19 -0500

    MGMT-4471 Add agent token generation for local auth

--- internal/cluster/auth.go
+++ internal/cluster/auth.go
@@ -1,25 +1,60 @@
 package cluster
 
 import (
+	"os"
+
+	"github.com/dgrijalva/jwt-go"
 	"github.com/openshift/assisted-service/internal/cluster/validations"
 	"github.com/openshift/assisted-service/internal/common"
 	"github.com/openshift/assisted-service/pkg/auth"
 	"github.com/pkg/errors"
 )
 
-func AgentToken(c *common.Cluster, authType auth.AuthType) (string, error) {
-	creds, err := validations.ParsePullSecret(c.PullSecret)
+func AgentToken(c *common.Cluster, authType auth.AuthType) (token string, err error) {
+	switch authType {
+	case auth.TypeRHSSO:
+		token, err = cloudPullSecretToken(c.PullSecret)
+	case auth.TypeLocal:
+		token, err = localJWT(c.ID.String())
+	case auth.TypeNone:
+		token = ""
+	default:
+		err = errors.Errorf("invalid authentication type %v", authType)
+	}
+	return
+}
+
+func cloudPullSecretToken(pullSecret string) (string, error) {
+	creds, err := validations.ParsePullSecret(pullSecret)
 	if err != nil {
 		return "", err
 	}
-	pullSecretToken := ""
-	if authType == auth.TypeRHSSO {
-		r, ok := creds["cloud.openshift.com"]
-		if !ok {
-			return "", errors.Errorf("Pull secret does not contain auth for cloud.openshift.com")
-		}
-		pullSecretToken = r.AuthRaw
+	r, ok := creds["cloud.openshift.com"]
+	if !ok {
+		return "", errors.Errorf("Pull secret does not contain auth for cloud.openshift.com")
+	}
+	return r.AuthRaw, nil
+}
+
+func localJWT(id string) (string, error) {
+	key, ok := os.LookupEnv("EC_PRIVATE_KEY_PEM")
+	if !ok || key == "" {
+		return "", errors.Errorf("EC_PRIVATE_KEY_PEM not found")
 	}
 
-	return pullSecretToken, nil
+	priv, err := jwt.ParseECPrivateKeyFromPEM([]byte(key))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims{
+		"cluster_id": id,
+	})
+
+	tokenString, err := token.SignedString(priv)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
 }
--- internal/cluster/auth_test.go
+++ internal/cluster/auth_test.go
@@ -1,6 +1,16 @@
 package cluster
 
 import (
+	"bytes"
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
+	"encoding/pem"
+	"os"
+
+	"github.com/dgrijalva/jwt-go"
 	"github.com/go-openapi/strfmt"
 	"github.com/google/uuid"
 	. "github.com/onsi/ginkgo"
@@ -10,7 +20,7 @@ import (
 	"github.com/openshift/assisted-service/pkg/auth"
 )
 
-var _ = FDescribe("AgentToken", func() {
+var _ = Describe("AgentToken", func() {
 	var (
 		id strfmt.UUID
 	)
@@ -47,5 +57,81 @@ var _ = FDescribe("AgentToken", func() {
 		token, err := AgentToken(c, auth.TypeNone)
 		Expect(err).ToNot(HaveOccurred())
 		Expect(token).To(Equal(""))
+	})
+
+	It("returns an error if an invalid auth type is configured", func() {
+		c := &common.Cluster{
+			Cluster:    models.Cluster{ID: &id},
+			PullSecret: "{\"auths\":{\"registry.redhat.com\":{\"auth\":\"dG9rZW46dGVzdAo=\",\"email\":\"coyote@acme.com\"}}}",
+		}
+		_, err := AgentToken(c, auth.AuthType("asdf"))
+
+		Expect(err).To(HaveOccurred())
+	})
+
+	It("returns an error for local auth with no private key", func() {
+		c := &common.Cluster{
+			Cluster:    models.Cluster{ID: &id},
+			PullSecret: "{\"auths\":{\"registry.redhat.com\":{\"auth\":\"dG9rZW46dGVzdAo=\",\"email\":\"coyote@acme.com\"}}}",
+		}
+		_, err := AgentToken(c, auth.TypeLocal)
+
+		Expect(err).To(HaveOccurred())
+	})
+
+	Context("with a private key set", func() {
+		var (
+			publicKey crypto.PublicKey
+		)
+
+		BeforeEach(func() {
+			priv, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+			Expect(err).NotTo(HaveOccurred())
+
+			publicKey = priv.Public()
+
+			privBytes, err := x509.MarshalECPrivateKey(priv)
+			Expect(err).NotTo(HaveOccurred())
+
+			block := &pem.Block{
+				Type:  "EC PRIVATE KEY",
+				Bytes: privBytes,
+			}
+			var out bytes.Buffer
+			Expect(pem.Encode(&out, block)).To(Succeed())
+
+			os.Setenv("EC_PRIVATE_KEY_PEM", out.String())
+		})
+
+		AfterEach(func() {
+			os.Unsetenv("EC_PRIVATE_KEY_PEM")
+		})
+
+		validateToken := func(token string, pub crypto.PublicKey) *jwt.Token {
+			parser := &jwt.Parser{ValidMethods: []string{jwt.SigningMethodES256.Alg()}}
+			parsed, err := parser.Parse(token, func(t *jwt.Token) (interface{}, error) { return pub, nil })
+
+			Expect(err).ToNot(HaveOccurred())
+			Expect(parsed.Valid).To(BeTrue())
+
+			return parsed
+		}
+
+		It("creates a valid token", func() {
+			c := &common.Cluster{
+				Cluster:    models.Cluster{ID: &id},
+				PullSecret: "{\"auths\":{\"registry.redhat.com\":{\"auth\":\"dG9rZW46dGVzdAo=\",\"email\":\"coyote@acme.com\"}}}",
+			}
+			tokenString, err := AgentToken(c, auth.TypeLocal)
+			Expect(err).ToNot(HaveOccurred())
+
+			tok := validateToken(tokenString, publicKey)
+			claims, ok := tok.Claims.(jwt.MapClaims)
+			Expect(ok).To(BeTrue())
+
+			clusterID, ok := claims["cluster_id"].(string)
+			Expect(ok).To(BeTrue())
+			Expect(clusterID).To(Equal(id.String()))
+		})
 	})
 })
