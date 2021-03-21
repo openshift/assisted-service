6ce37db0a707d494266b64b9c2bbafe38b5c2e8a
Rewant Soni <rewant.soni@gmail.com>
2021-03-21 16:43:51 +0530

    MGMT-4583: Refactor ocs code (#1308)
    Signed-off-by: Rewant Soni <resoni@redhat.com>

--- internal/operators/ocs/consts.go
+++ internal/operators/ocs/consts.go
@@ -1,7 +1,12 @@
 package ocs
 
+type ocsDeploymentMode string
+
 const (
-	ssdDrive    string = "SSD"
-	hddDrive    string = "HDD"
-	minDiskSize int64  = 5 //5GB is the min disk size for OCS
+	ssdDrive     string            = "SSD"
+	hddDrive     string            = "HDD"
+	minDiskSize  int64             = 5 //5GB is the min disk size for OCS
+	compactMode  ocsDeploymentMode = "Compact"
+	minimalMode  ocsDeploymentMode = "Minimal"
+	standardMode ocsDeploymentMode = "Standard"
 )
