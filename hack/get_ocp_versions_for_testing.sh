ba7b76276d98eb84a1a3fdf93154c07dd3cf6362
Eran Cohen <eranco@redhat.com>
2021-03-21 15:02:05 +0200

    change single-node ocp release (#1349)
    * NO-ISSUE - Update SNO image used for testing
    
    * NO-ISSUE skip 4.8 nightly version validation
    
    Co-authored-by: Omer Tuchfeld <otuchfel@redhat.com>

--- hack/get_ocp_versions_for_testing.sh
+++ hack/get_ocp_versions_for_testing.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT_DIR="$(cd "$(dirname "${__dir}")" && pwd)"
-OPENSHIFT_SNO_IMAGE="registry.svc.ci.openshift.org/sno-dev/openshift-bip:0.6.0"
+OPENSHIFT_SNO_IMAGE="quay.io/openshift-release-dev/ocp-release-nightly:4.8.0-0.nightly-2021-03-16-221720"
 
 # Append single node image as 4.8 for local deployments
 "${PROJECT_ROOT_DIR}"/tools/handle_ocp_versions.py --src ./default_ocp_versions.json --ocp-override ${OPENSHIFT_SNO_IMAGE} --name-override single-node-4.8-alpha --version-override 4.8 --skip-verify | tr -d "\n\t "
