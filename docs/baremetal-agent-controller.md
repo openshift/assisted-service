030f62fc3ac387c40060e4281e3a3c557264ba25
Flavio Percoco <flaper87@gmail.com>
2021-04-02 09:46:09 +0100

    MGMT-3697: BareMetal Agent controller (#1279)
    This commit add s the first implementation for the BareMetalHost Agent
    controller (BMAC). The commit covers the following parts of the
    implementation:
    
    - Find the right pairs of BMH/Agent using their MAC addresses
    - Set the Image.URL in the BMH copying it from the InstallEnv's status.
    - Reconcile the Agent's spec by copying the following attributes from
      the BMH's annotations:
          - Role: master/worker
          - Hostname (optional for user to set)
          - MachineConfigPool (optional for user to set)
    - Reconcile the BareMetalHost hardware details by copying the Agent's
      inventory data to the BMH's hardwaredetails annotation.
    
    TODO:
    
    - Use the Agent's state to filter out obsolete agent resources. We only
      want to reconcile agents that are in a specific state.
    
    - Inspect the `RootDeviceHints` from the BMH and set the
      InstallationDiskPath on the agent.
    
    - Gracefully handle the case where the BareMetalHost resource doesn't
      exist
    
    Signed-off-by: Flavio Percoco <flavio@redhat.com>
    
    Co-authored-by: Flavio Percoco <flavio@redhat.com>

---
+++ docs/baremetal-agent-controller.md
@@ -0,0 +1,154 @@
+Baremetal Agent Controller (a.k.a BMAC)
+==
+
+BMAC is a Kubernetes controller responsible for reconciling [BareMetalHost][bmo] and Agent (defined
+and maintained in this repo) resources for the agent-based deployment scenario.
+
+Testing
+==
+
+The testing environment for BMAC consists in:
+
+- [Downstream dev-scripts](https://github.com/openshift-metal3/dev-scripts/) deployment
+- [Baremetal Operator][bmo]: It defines the BareMetalHost custom resource
+- [Assisted Installer Operator](./operator.md): To deploy and manage the  assisted installer
+  deployment. Read the operator docs to know more about its dependencies and installation process.
+
+
+Each of the components listed above provide their own documentation on how to deploy and configure
+them. However, you can find below a set of recommended configs that can be used for each of these
+components:
+
+Dev Scripts
+===
+
+```bash
+# One worker, with enough memory and CPU, should be enough.
+# Use more if resources are not a problem
+export NUM_WORKERS=1
+# Add extra workers so we can use it for the deployment
+export NUM_EXTRA_WORKERS=1
+# At the time of this writing, this requires the 1195 PR
+# mentioned below.
+export PROVISIONING_NETWORK_PROFILE=Disabled
+# By default dev-scripts uses the metal3 BMO image
+# clone the downstream version and set the path here.
+export BAREMETAL_OPERATOR_LOCAL_IMAGE=/path/to/your/local/clone
+```
+
+The config above should provide you with an environment that is ready to be used for the operator,
+assited installer, and BMAC tests. Here are a few tips that would help simplifying the environment
+and the steps required:
+
+- Clone [baremetal-operator][bmo] somewhere and set the BAREMETAL_OPERATOR_LOCAL_IMAGE in your config.
+
+Once dev-script is up and running, modify the worker(s) and add 2 more disks (10GB should be enough)
+as they are required by the Assisted Installer Operator.
+
+Baremetal Operator
+==
+
+The [baremetal-operator][bmo] will define the BareMetalHost custom resource required by the agent
+based install process. Setting the `BAREMETAL_OPERATOR_LOCAL_IMAGE` should build and run the BMO
+already. However, it's recommended to run the [local-bmo][local-bmo] script to facilitated the
+deployment and monitoring of the BMO. Here's what using [local-bmo][local-bmo] looks like:
+
+It's possible to disable inspection for the master (and workers) nodes before running the local-bmo
+script. This will make the script less noisy which will make debugging easier.
+
+```bash
+./metal3-dev/pause-control-plane.sh
+```
+The pause-control-plane script only pauses the control plane. You can do the same for the worker
+nodes with the following command
+
+```bash
+for host in $(oc get baremetalhost -n openshift-machine-api -o name | grep -e '-worker-'); do
+    oc annotate --overwrite -n openshift-machine-api "$host" \
+       'baremetalhost.metal3.io/paused=""'
+done
+```
+
+The steps mentioned above are optional, but recommended. They will make debugging easier. Let's now
+run [local-bmo][local-bmo] and move on. This script will tail the logs so do it in a separate buffer
+so that it can be kept running.
+
+```bash
+# Notee variable is different from the one in your dev-script
+# config file. You can set it to the same path, tho.
+export BAREMETAL_OPERATOR_PATH=/path/to/your/local/clone
+./metal3-dev/local-bmo.sh
+
+```
+
+Assited Installer Operator
+===
+
+Once the dev-script environment is up-and-running, and the [bmo][bmo] has been deployed, you can
+proceed to deploying the Assisted Installer Operator following [this documentation](./operator).
+At the end of this process you will have the assisted-service running under the `assisted-installer`
+namespace.
+
+Take notice of the [assisted-service-patch-image][aspi-custom] kustomization if you are testing a local
+assisted-service image. More on this in the operator docs. Another, more hacky option, is to
+manually modify the image for the `assisted-service` deployment after it's been deployed. Favor the
+kustomization way over this.
+
+
+```
+[dev@edge-10 dev-scripts]$ oc get pods
+NAME                                READY   STATUS    RESTARTS   AGE
+assisted-service-7d45ccf4f8-88pw4   1/1     Running   0          18h
+ocp-metal-ui-7967c757b4-fdxph       1/1     Running   0          21h
+postgres-6bd4dc768f-54pgz           1/1     Running   0          21h
+```
+
+Tailing the logs for the `assisted-service` pod should not show any errors.
+
+Creating BareMetalHost resources
+===
+
+The [baremetal operator][bmo] creates the `BareMetalHost` resources for the existing nodes
+automatically. For scenarios using extra worker nodes (like SNO), it will be necessary to create
+`BareMetalHost` resources manually. Lucklily enough, `dev-scripts` is one step ahead and it has
+prepared the manifest for us already.
+
+```
+less ocp/ostest/extra_host_manifests.yaml
+```
+
+BMAC will disable inspection for this `BareMetalHost` resource automatically but, if you want to get
+ahead and save some time, you can disable it by modifying the manifest generated by `dev-scripts`
+and adding the following annotation:
+
+```
+  labels:
+    # optionally add the installenvs annotation, if needed for BMAC
+    installenvs.adi.openshift.io: "bmac-test"
+
+  annotations:
+    # BMAC will add this annotation if not present
+    inspect.metal3.io: disabled
+```
+
+The above is an optional step. If skipped, the `BareMetalHost` will boot IPA and spend some time in
+the inspecting phase when the manifest is applied. There's an [open PR](https://github.com/openshift-metal3/dev-scripts/pull/1193/)
+that will allow setting an environment variable to have inspection disabled by `dev-scripts` by default.
+
+
+[bmo]: https://github.com/openshift/baremetal-operator
+[local-bmo]: https://github.com/openshift-metal3/dev-scripts/blob/master/metal3-dev/local-bmo.sh
+[aspi-custom]: https://github.com/openshift/assisted-service/blob/master/config/default/assisted-service-patch-image.yaml
+
+
+Creating ClusterDeployment and InstallEnv
+==
+
+Before deploying the ClusterDeployment, make sure you have created a secret with your pull-secret.
+In this environment it's called `my-pull-secret`
+
+```
+kubectl create secret -n assisted-installer generic my-pull-secret --from-file=.dockerconfigjson=pull_secret.json
+```
+
+At this point, it's possible to deploy the SNO cluster by modifying and applying [the SNO manifest](./crds/clusterDeployment-SNO.yaml)
---
+++ internal/controller/controllers/bmh_agent_controller_test.go
@@ -0,0 +1,334 @@
+package controllers
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	bmh_v1alpha1 "github.com/metal3-io/baremetal-operator/apis/metal3.io/v1alpha1"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/openshift/assisted-service/internal/common"
+	"github.com/openshift/assisted-service/internal/controller/api/v1alpha1"
+	"github.com/openshift/assisted-service/models"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+var _ = Describe("bmac reconcile", func() {
+	var (
+		c        client.Client
+		bmhr     *BMACReconciler
+		ctx      = context.Background()
+		mockCtrl *gomock.Controller
+	)
+
+	BeforeEach(func() {
+		c = fakeclient.NewFakeClientWithScheme(scheme.Scheme)
+		mockCtrl = gomock.NewController(GinkgoT())
+		bmhr = &BMACReconciler{
+			Client: c,
+			Scheme: scheme.Scheme,
+			Log:    common.GetTestLog(),
+		}
+	})
+
+	AfterEach(func() {
+		mockCtrl.Finish()
+	})
+
+	It("bmh reconcile: no labels", func() {
+		host := newBMH("bmh-reconcile", &bmh_v1alpha1.BareMetalHostSpec{})
+		Expect(c.Create(ctx, host)).To(BeNil())
+
+		result, err := bmhr.Reconcile(newBMHRequest(host))
+		Expect(err).To(BeNil())
+		Expect(result).To(Equal(ctrl.Result{}))
+	})
+
+	Describe("queue bmh request for agent", func() {
+		var host *bmh_v1alpha1.BareMetalHost
+		var agent *v1alpha1.Agent
+
+		BeforeEach(func() {
+			macStr := "12-34-56-78-9A-BC"
+			agent = newAgent("bmac-agent", testNamespace, v1alpha1.AgentSpec{})
+			agent.Status.Inventory = v1alpha1.HostInventory{
+				ReportTime: &metav1.Time{Time: time.Now()},
+				Memory: v1alpha1.HostMemory{
+					PhysicalBytes: 2,
+				},
+				Interfaces: []v1alpha1.HostInterface{
+					{
+						Name: "eth0",
+						IPV4Addresses: []string{
+							"1.2.3.4",
+						},
+						IPV6Addresses: []string{
+							"1001:db8::10/120",
+						},
+						MacAddress: macStr,
+					},
+				},
+			}
+			Expect(c.Create(ctx, agent)).To(BeNil())
+
+			host = newBMH("bmh-reconcile", &bmh_v1alpha1.BareMetalHostSpec{BootMACAddress: macStr})
+			Expect(c.Create(ctx, host)).To(BeNil())
+
+		})
+
+		AfterEach(func() {
+			Expect(c.Delete(ctx, host)).ShouldNot(HaveOccurred())
+			Expect(c.Delete(ctx, agent)).ShouldNot(HaveOccurred())
+		})
+
+		Context("findBMH, when both agent and bmh exist,", func() {
+			It("should return the agent if their MAC address matches", func() {
+				result, err := bmhr.findBMH(context.Background(), agent)
+				Expect(err).To(BeNil())
+				Expect(result).To(Equal(host))
+			})
+
+			It("should return nil if there is no match", func() {
+				agent = newAgent("bmac-agent-no-MAC", testNamespace, v1alpha1.AgentSpec{})
+				Expect(c.Create(ctx, agent)).To(BeNil())
+
+				result, err := bmhr.findBMH(context.Background(), agent)
+				Expect(err).To(BeNil())
+				Expect(result).To(BeNil())
+			})
+		})
+	})
+
+	Describe("Reconcile a BMH with an installEnv label", func() {
+		var host *bmh_v1alpha1.BareMetalHost
+		BeforeEach(func() {
+			host = newBMH("bmh-reconcile", &bmh_v1alpha1.BareMetalHostSpec{})
+			labels := make(map[string]string)
+			labels[BMH_INSTALL_ENV_LABEL] = "testInstallEnv"
+			host.ObjectMeta.Labels = labels
+			Expect(c.Create(ctx, host)).To(BeNil())
+		})
+
+		AfterEach(func() {
+			Expect(c.Delete(ctx, host)).ShouldNot(HaveOccurred())
+		})
+
+		Context("with a non-existing installenv", func() {
+			It("should return without failures", func() {
+				result, err := bmhr.Reconcile(newBMHRequest(host))
+				Expect(err).To(BeNil())
+				Expect(result).To(Equal(ctrl.Result{}))
+			})
+		})
+
+		Context("with an existing installEnv without ISODownloadURL", func() {
+			It("should requeue the reconcile", func() {
+				installEnv := newInstallEnvImage("testInstallEnv", testNamespace, v1alpha1.InstallEnvSpec{})
+				Expect(c.Create(ctx, installEnv)).To(BeNil())
+
+				result, err := bmhr.Reconcile(newBMHRequest(host))
+				Expect(err).To(BeNil())
+				Expect(result.RequeueAfter).To(Equal(time.Minute))
+			})
+		})
+
+		Context("with an existing installEnv with ISODownloadURL", func() {
+			var installEnv *v1alpha1.InstallEnv
+			var isoImageURL string
+
+			BeforeEach(func() {
+				isoImageURL = "http://buzz.lightyear.io/discovery-image.iso"
+				installEnv = newInstallEnvImage("testInstallEnv", testNamespace, v1alpha1.InstallEnvSpec{})
+				installEnv.Status = v1alpha1.InstallEnvStatus{ISODownloadURL: isoImageURL}
+				Expect(c.Create(ctx, installEnv)).To(BeNil())
+			})
+
+			AfterEach(func() {
+				Expect(c.Delete(ctx, installEnv)).ShouldNot(HaveOccurred())
+			})
+
+			It("should disable the BMH hardware inspection", func() {
+				result, err := bmhr.Reconcile(newBMHRequest(host))
+				Expect(err).To(BeNil())
+				Expect(result).To(Equal(ctrl.Result{}))
+
+				updatedHost := &bmh_v1alpha1.BareMetalHost{}
+				err = c.Get(ctx, types.NamespacedName{Name: host.Name, Namespace: testNamespace}, updatedHost)
+				Expect(err).To(BeNil())
+				Expect(updatedHost.ObjectMeta.Annotations).To(HaveKey(BMH_INSPECT_ANNOTATION))
+				Expect(updatedHost.ObjectMeta.Annotations[BMH_INSPECT_ANNOTATION]).To(Equal("disabled"))
+
+			})
+
+			It("should set the ISODownloadURL in the BMH", func() {
+				result, err := bmhr.Reconcile(newBMHRequest(host))
+				Expect(err).To(BeNil())
+				Expect(result).To(Equal(ctrl.Result{}))
+
+				updatedHost := &bmh_v1alpha1.BareMetalHost{}
+				err = c.Get(ctx, types.NamespacedName{Name: "bmh-reconcile", Namespace: testNamespace}, updatedHost)
+				Expect(err).To(BeNil())
+				Expect(updatedHost.Spec.Image.URL).To(Equal(isoImageURL))
+			})
+		})
+	})
+
+	Describe("Reconcile a BMH with a non-approved matching agent", func() {
+		var host *bmh_v1alpha1.BareMetalHost
+		var agent *v1alpha1.Agent
+
+		BeforeEach(func() {
+			macStr := "12-34-56-78-9A-BC"
+			agent = newAgent("bmac-agent", testNamespace, v1alpha1.AgentSpec{})
+			agent.Status.Inventory = v1alpha1.HostInventory{
+				Interfaces: []v1alpha1.HostInterface{
+					{
+						Name:       "eth0",
+						MacAddress: macStr,
+					},
+				},
+			}
+			Expect(c.Create(ctx, agent)).To(BeNil())
+
+			image := &bmh_v1alpha1.Image{URL: "http://buzz.lightyear.io/discovery-image.iso"}
+			host = newBMH("bmh-reconcile", &bmh_v1alpha1.BareMetalHostSpec{Image: image, BootMACAddress: macStr})
+			annotations := make(map[string]string)
+			annotations[BMH_AGENT_ROLE] = "master"
+			annotations[BMH_AGENT_HOSTNAME] = "happy-meal"
+			annotations[BMH_AGENT_MACHINE_CONFIG_POOL] = "number-8"
+			host.ObjectMeta.SetAnnotations(annotations)
+			Expect(c.Create(ctx, host)).To(BeNil())
+
+		})
+
+		AfterEach(func() {
+			Expect(c.Delete(ctx, host)).ShouldNot(HaveOccurred())
+			Expect(c.Delete(ctx, agent)).ShouldNot(HaveOccurred())
+		})
+
+		Context("when an agent matches", func() {
+			It("should not fail on missing role", func() {
+				updatedHost := &bmh_v1alpha1.BareMetalHost{}
+				err := c.Get(ctx, types.NamespacedName{Name: host.Name, Namespace: testNamespace}, updatedHost)
+				Expect(err).To(BeNil())
+				updatedHost.ObjectMeta.Annotations[BMH_AGENT_ROLE] = "without-purpose"
+				Expect(c.Update(ctx, updatedHost)).To(BeNil())
+
+				result, err := bmhr.Reconcile(newBMHRequest(updatedHost))
+				Expect(err).To(BeNil())
+				Expect(result).To(Equal(ctrl.Result{}))
+
+				updatedAgent := &v1alpha1.Agent{}
+				err = c.Get(ctx, types.NamespacedName{Name: agent.Name, Namespace: agent.Namespace}, updatedAgent)
+				Expect(err).To(BeNil())
+				Expect(string(updatedAgent.Spec.Role)).To(Equal("without-purpose"))
+			})
+
+			It("should approve it", func() {
+				result, err := bmhr.Reconcile(newBMHRequest(host))
+				Expect(err).To(BeNil())
+				Expect(result).To(Equal(ctrl.Result{}))
+
+				updatedAgent := &v1alpha1.Agent{}
+				err = c.Get(ctx, types.NamespacedName{Name: agent.Name, Namespace: agent.Namespace}, updatedAgent)
+				Expect(err).To(BeNil())
+				Expect(updatedAgent.Spec.Approved).To(Equal(true))
+			})
+
+			It("should add a lable referring to the bmh", func() {
+				result, err := bmhr.Reconcile(newBMHRequest(host))
+				Expect(err).To(BeNil())
+				Expect(result).To(Equal(ctrl.Result{}))
+
+				updatedAgent := &v1alpha1.Agent{}
+				err = c.Get(ctx, types.NamespacedName{Name: agent.Name, Namespace: agent.Namespace}, updatedAgent)
+				Expect(err).To(BeNil())
+				Expect(updatedAgent.ObjectMeta.Labels[AGENT_BMH_LABEL]).To(Equal(host.Name))
+			})
+
+			It("should set the agent spec based on the BMH annotations", func() {
+				result, err := bmhr.Reconcile(newBMHRequest(host))
+				Expect(err).To(BeNil())
+				Expect(result).To(Equal(ctrl.Result{}))
+
+				updatedAgent := &v1alpha1.Agent{}
+				err = c.Get(ctx, types.NamespacedName{Name: agent.Name, Namespace: agent.Namespace}, updatedAgent)
+				Expect(err).To(BeNil())
+				Expect(updatedAgent.Spec.Role).To(Equal(models.HostRoleMaster))
+				Expect(updatedAgent.Spec.Hostname).To(Equal("happy-meal"))
+				Expect(updatedAgent.Spec.MachineConfigPool).To(Equal("number-8"))
+			})
+		})
+	})
+
+	Describe("Reconcile a BMH with an approved matching agent", func() {
+		var host *bmh_v1alpha1.BareMetalHost
+		var agent *v1alpha1.Agent
+
+		BeforeEach(func() {
+			macStr := "12-34-56-78-9A-BC"
+			agent = newAgent("bmac-agent", testNamespace, v1alpha1.AgentSpec{Approved: true})
+			agent.Status.Inventory = v1alpha1.HostInventory{
+				Memory: v1alpha1.HostMemory{
+					PhysicalBytes: 2,
+				},
+				Interfaces: []v1alpha1.HostInterface{
+					{
+						Name: "eth0",
+						IPV4Addresses: []string{
+							"1.2.3.4",
+						},
+						IPV6Addresses: []string{
+							"1001:db8::10/120",
+						},
+						MacAddress: macStr,
+					},
+				},
+				Disks: []v1alpha1.HostDisk{
+					{Path: "/dev/sda", Bootable: true},
+					{Path: "/dev/sdb", Bootable: false},
+				},
+			}
+			Expect(c.Create(ctx, agent)).To(BeNil())
+
+			image := &bmh_v1alpha1.Image{URL: "http://buzz.lightyear.io/discovery-image.iso"}
+			host = newBMH("bmh-reconcile", &bmh_v1alpha1.BareMetalHostSpec{Image: image, BootMACAddress: macStr})
+			Expect(c.Create(ctx, host)).To(BeNil())
+
+		})
+
+		AfterEach(func() {
+			Expect(c.Delete(ctx, host)).ShouldNot(HaveOccurred())
+			Expect(c.Delete(ctx, agent)).ShouldNot(HaveOccurred())
+		})
+
+		Context("when an agent matches", func() {
+			It("should set the metal3 hardwaredetails annotation if the Agent inventory is set", func() {
+				result, err := bmhr.Reconcile(newBMHRequest(host))
+				Expect(err).To(BeNil())
+				Expect(result).To(Equal(ctrl.Result{}))
+
+				updatedHost := &bmh_v1alpha1.BareMetalHost{}
+				err = c.Get(ctx, types.NamespacedName{Name: host.Name, Namespace: testNamespace}, updatedHost)
+				Expect(err).To(BeNil())
+				Expect(updatedHost.ObjectMeta.Annotations).To(HaveKey(BMH_HARDWARE_DETAILS_ANNOTATION))
+
+				var hDetails bmh_v1alpha1.HardwareDetails
+				err = json.Unmarshal([]byte(updatedHost.ObjectMeta.Annotations[BMH_HARDWARE_DETAILS_ANNOTATION]), &hDetails)
+				Expect(err).To(BeNil())
+				Expect(hDetails.NIC).To(HaveLen(2))
+				Expect(hDetails.Storage).To(HaveLen(2))
+				Expect(hDetails.RAMMebibytes).To(BeEquivalentTo(agent.Status.Inventory.Memory.PhysicalBytes / (1024 * 1024)))
+				Expect(hDetails.CPU.Arch).To(Equal(agent.Status.Inventory.Cpu.Architecture))
+				Expect(hDetails.CPU.Model).To(Equal(agent.Status.Inventory.Cpu.ModelName))
+			})
+		})
+	})
+})
