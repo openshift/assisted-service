54baef6c24feb4e7f29d7cbd32739d3fc3bc8889
Sudarshan Acharya <suachary@redhat.com>
2021-04-15 12:45:09 -0500

    MGMT-4494: Create bmh in day2 cluster (#1424)

---
+++ internal/controller/controllers/spoke_cluster.go
@@ -0,0 +1,32 @@
+package controllers
+
+import (
+	"github.com/pkg/errors"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/kubectl/pkg/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func getSpokeClient(secret *corev1.Secret) (client.Client, error) {
+	if secret.Data == nil {
+		return nil, errors.Errorf("Secret %s/%s  does not contain any data", secret.Namespace, secret.Name)
+	}
+	kubeconfigData, ok := secret.Data["kubeconfig"]
+	if !ok || len(kubeconfigData) == 0 {
+		return nil, errors.Errorf("Secret data for %s/%s  does not contain kubeconfig", secret.Namespace, secret.Name)
+	}
+	clientConfig, err := clientcmd.NewClientConfigFromBytes(kubeconfigData)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get clientconfig from kubeconfig data in secret")
+	}
+	restConfig, err := clientConfig.ClientConfig()
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get restconfig for spoke kube client")
+	}
+	targetClient, err := client.New(restConfig, client.Options{Scheme: scheme.Scheme})
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get spoke kube client")
+	}
+	return targetClient, nil
+}
