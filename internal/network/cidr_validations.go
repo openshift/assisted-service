fc74d6886dd29aab14377267627be331de90d9d1
Vitaliy Emporopulo <vemporop@redhat.com>
2021-04-19 13:10:11 +0300

    OCPBUGSM-25356 Handle net prefix for single node
    Handle network prefix in case a cluster has a single node. In such
    case the entire machine CIDR or its part (but no less than 128
    addresses) can be allocated to the node.

--- internal/network/cidr_validations.go
+++ internal/network/cidr_validations.go
@@ -86,12 +86,17 @@ func VerifyClusterCidrSize(hostNetworkPrefix int, clusterNetworkCIDR string, num
 	if hostNetworkPrefix > bits-MinMaskDelta {
 		return errors.Errorf("Host prefix, now %d, must be less than or equal to %d to allow at least 128 addresses", hostNetworkPrefix, bits-MinMaskDelta)
 	}
-	requestedNumHosts := max(4, numberOfHosts)
+	var requestedNumHosts int
+	if numberOfHosts == 1 {
+		requestedNumHosts = 1
+	} else {
+		requestedNumHosts = max(4, numberOfHosts)
+	}
 	// 63 to avoid overflow
 	possibleNumHosts := uint64(1) << min(63, max(hostNetworkPrefix-clusterNetworkPrefix, 0))
 	if uint64(requestedNumHosts) > possibleNumHosts {
 		return errors.Errorf("Cluster network CIDR prefix %d does not contain enough addresses for %d hosts each one with %d prefix (%d addresses)",
-			clusterNetworkPrefix, requestedNumHosts, hostNetworkPrefix, uint64(1)<<min(63, bits-hostNetworkPrefix))
+			clusterNetworkPrefix, numberOfHosts, hostNetworkPrefix, uint64(1)<<min(63, bits-hostNetworkPrefix))
 	}
 	return nil
 }
