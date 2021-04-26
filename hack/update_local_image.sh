43e811cdcb9b5f633ea44478656bedd7b5d830de
Michael Levy <milevy@redhat.com>
2021-04-26 16:53:00 +0300

    Mgmt 4967 (#1548)
    * NO-ISSUE fixing a mac issue use a different kubeconfig file for the container
    The minikube start command updates the kubeconfig file apiserver url to localhost instead of the minikube internal ip(since in MAC the minikube ip isn't accessable from the MAC itself but from the container)
    The container cannot access the apiserver since it needs the minikube ip.
    The walk around is to create a new dedicate file for the container, update the URL to the minikube ip and set it to the new env variable CONTAINER_KUBE_CONFIG
    
    * MGMT-4967 Create a standard way to run a debugger on the service
    Create a new Dockerfile with dlv that extends from the current assisted-service image
    Compiling our code with debug information
    Add a new DEBUG env variable which controls the compilation behavior

--- hack/update_local_image.sh
+++ hack/update_local_image.sh
@@ -7,14 +7,42 @@ set -o xtrace
 
 CLUSTER_CONTEXT=$(${KUBECTL} config current-context | cut -d'-' -f1)
 
+
+# This file is responsible to the update the local k8s (for example minikube) with the new image.
+# For minikube the faster option is to build the image after changing the docker environment to the minikube docker.
+# For k3d we can just import the image.
+# TODO Create a local docker registry push the image to it and remove this file
 case "${CLUSTER_CONTEXT}" in
   "minikube")
+
+    # The kubectl current-context (CLUSTER_CONTEXT) indicates we're pointing at a minikube cluster, but there's
+    # no available minikube binary. This typically happens when running on a skipper container running on top
+    # of macOS where the minikube command cannot be mapped from the host to the skipper container (since the
+    # skipper container expects a binary compiled for Linux).
+    if ! command -v minikube &> /dev/null
+    then
+      # The SKIPPER_UID environment variable is an indication that we are running on a skipper container.
+      # We don't want to change the user's host but to ask the user to install.
+      if [[ ! -v SKIPPER_UID ]]; then
+        echo "ERROR: minikube command is not installed or not in your PATH"
+        exit 1
+      fi
+
+      # If it's in skipper, it's fine to install minikube for them since it's just a disposable container
+      # We download and install the latest minikube RPM on the skipper container.
+      # TODO: This may cause problems when the latest minikube version doesn't match the one installed on the
+      # mac host, but it's better than nothing for now
+      echo "minikube is not mapped on skipper (probably a MACOS) - installing"
+      curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-latest.x86_64.rpm
+      rpm -ivh minikube-latest.x86_64.rpm
+    fi
+
     eval $(SHELL=${SHELL:-/bin/sh} minikube docker-env) && \
-        docker build ${CONTAINER_BUILD_PARAMS} -f Dockerfile.assisted-service . -t ${SERVICE}
+        make update-${DEBUG:+debug-}minimal
     ;;
 
   "k3d")
-    docker build ${CONTAINER_BUILD_PARAMS} -f Dockerfile.assisted-service . -t ${SERVICE}
+    make update-${DEBUG:+debug-}minimal
     k3d image import ${SERVICE}
     ;;
 
