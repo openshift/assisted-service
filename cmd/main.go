0d0f478724fe71db66ba3f20e218e577b5f6f7ab
Moti Asayag <masayag@redhat.com>
2021-04-27 22:28:37 +0300

    MGMT-3512 Skip garbage collection jobs in operator deployment (#1582)
    In operator deployment, when cluster entity is removed from the
    database by the garbage collector, the clusterdeployment controller
    will attempt to reconcile the clusterdeployment CR by re-creating a new
    cluster entity in the database for it.
    Therefore, the garbage collection job is not useful in operator
    deployment and needs to be skipped.
    
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- cmd/main.go
+++ cmd/main.go
@@ -325,7 +325,7 @@ func main() {
 	} else {
 		crdUtils = controllers.NewDummyCRDUtils()
 	}
-	if Options.EnableDeregisterInactiveGC || Options.EnableDeletedUnregisteredGC {
+	if !Options.EnableKubeAPI && (Options.EnableDeregisterInactiveGC || Options.EnableDeletedUnregisteredGC) {
 		gc := garbagecollector.NewGarbageCollectors(Options.GCConfig, db, log.WithField("pkg", "garbage_collector"), hostApi, clusterApi, objectHandler, lead)
 
 		if Options.EnableDeregisterInactiveGC {
