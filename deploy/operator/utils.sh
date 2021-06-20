272d841b8c133d3509843e0fca0f83397cb37fc1
Osher De Paz <osherdepaz@gmail.com>
2021-06-20 21:19:33 +0300

    NO-ISSUE: increase timeout of waiting for pod for disconnected flow

--- deploy/operator/utils.sh
+++ deploy/operator/utils.sh
@@ -35,7 +35,7 @@ function wait_for_pod() {
     namespace="${2:-}"
     selector="${3:-}"
 
-    wait_for_condition "pod" "Ready" "20m" "${namespace}" "${selector}"
+    wait_for_condition "pod" "Ready" "22m" "${namespace}" "${selector}"
 }
 
 function hash() {
