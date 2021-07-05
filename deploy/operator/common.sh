7bc89fcbdb3cd412e40df1dc74a520f1a5bd5399
Yuval Goldberg <yuvigoldi@gmail.com>
2021-07-05 11:46:15 +0300

    MGMT-5171: Call set_ca_bundle.py script from the correct path (#2161)

--- deploy/operator/common.sh
+++ deploy/operator/common.sh
@@ -6,8 +6,24 @@ if [ -z "${DISKS:-}" ]; then
 fi
 
 export DISCONNECTED="${DISCONNECTED:-false}"
+if [ "${DISCONNECTED}" = "true" ]; then
+    export LOCAL_REGISTRY="${LOCAL_REGISTRY_DNS_NAME}:${LOCAL_REGISTRY_PORT}"
+fi
+
+##############
+# Deployment #
+##############
+
 export ASSISTED_DEPLOYMENT_METHOD="${ASSISTED_DEPLOYMENT_METHOD:-from_index_image}"
 export HIVE_DEPLOYMENT_METHOD="${HIVE_DEPLOYMENT_METHOD:-with_olm}"
+
+export ASSISTED_NAMESPACE="${ASSISTED_NAMESPACE:-assisted-installer}"
+export SPOKE_NAMESPACE="${SPOKE_NAMESPACE:-assisted-spoke-cluster}"
+export HIVE_NAMESPACE="${HIVE_NAMESPACE:-hive}"
+
+############
+# Versions #
+############
 
 export OPENSHIFT_VERSIONS="${OPENSHIFT_VERSIONS:-$(cat ${__root}/data/default_ocp_versions.json)}"
 
@@ -15,7 +31,6 @@ if [ -z "${ASSISTED_OPENSHIFT_INSTALL_RELEASE_IMAGE:-}" ]; then
     export ASSISTED_OPENSHIFT_INSTALL_RELEASE_IMAGE=$(echo ${OPENSHIFT_VERSIONS} | jq -rc '[.[].release_image]|max')
 fi
 
-export LOCAL_REGISTRY="${LOCAL_REGISTRY_DNS_NAME}:${LOCAL_REGISTRY_PORT}"
 export ASSISTED_OPENSHIFT_VERSION="${ASSISTED_OPENSHIFT_VERSION:-openshift-v4.8.0}"
 
 OPENSHIFT_VERSIONS=$(echo ${OPENSHIFT_VERSIONS} |
@@ -27,7 +42,3 @@ OPENSHIFT_VERSIONS=$(echo ${OPENSHIFT_VERSIONS} |
                 rhcos_rootfs:  .value.rhcos_rootfs}
     }
     )')
-
-export ASSISTED_NAMESPACE="${ASSISTED_NAMESPACE:-assisted-installer}"
-export SPOKE_NAMESPACE="${SPOKE_NAMESPACE:-assisted-spoke-cluster}"
-export HIVE_NAMESPACE="${HIVE_NAMESPACE:-hive}"
