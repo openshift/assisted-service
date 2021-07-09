659c6f91f5be932e3023aba3e1ebdee0bad10d27
David Zager <david.j.zager@gmail.com>
2021-07-09 18:51:37 -0400

    NO-ISSUE: Use spectral image directly when needed (#2192)
    Instead of installing spectral in our build container, just use the
    container image if the binary isn't available.

--- hack/setup_env.sh
+++ hack/setup_env.sh
@@ -46,9 +46,6 @@ function assisted_service() {
   curl --retry 5 -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh \
     | sh -s -- -b $(go env GOPATH)/bin v1.36.0
 
-  #TODO: Add this back when https://github.com/stoplightio/spectral/issues/1745 is resolved
-  #curl --retry 5 -L https://raw.githack.com/stoplightio/spectral/master/scripts/install.sh | sh
-
   ARCH=$(case $(arch) in x86_64) echo -n amd64 ;; aarch64) echo -n arm64 ;; *) echo -n $(arch) ;; esac)
   OS=$(uname | awk '{print tolower($0)}')
   OPERATOR_SDK_DL_URL=https://github.com/operator-framework/operator-sdk/releases/download/v1.7.2
