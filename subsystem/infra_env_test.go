b94166bde7f99ca39783259ed24863b2a75b7bd4
Yevgeny Shnaidman <yshnaidm@redhat.com>
2021-08-26 01:07:39 +0300

    MGMT-7692: add subsystem test for infra env V2 scenarios
    Adds subsystems test for infra-env scenarious. The tests are only for infra-env CRUDs APIs.
    Tests for infra-env + host, infra-env + host + cluster scenarious will be added in
    a different PRs

--- subsystem/infra_env_test.go
+++ subsystem/infra_env_test.go
@@ -3,19 +3,16 @@ package subsystem
 import (
 	"context"
 	"io/ioutil"
+	"os"
+	"time"
 
 	"github.com/go-openapi/strfmt"
 	"github.com/go-openapi/swag"
+	"github.com/google/uuid"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/openshift/assisted-service/client/installer"
 	"github.com/openshift/assisted-service/models"
-)
-
-// #nosec
-const (
-	infraEnvSshPublicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC50TuHS7aYci+U+5PLe/aW/I6maBi9PBDucLje6C6gtArfjy7udWA1DCSIQd+DkHhi57/s+PmvEjzfAfzqo+L+/8/O2l2seR1pPhHDxMR/rSyo/6rZP6KIL8HwFqXHHpDUM4tLXdgwKAe1LxBevLt/yNl8kOiHJESUSl+2QSf8z4SIbo/frDD8OwOvtfKBEG4WCb8zEsEuIPNF/Vo/UxPtS9pPTecEsWKDHR67yFjjamoyLvAzMAJotYgyMoxm8PTyCgEzHk3s3S4iO956d6KVOEJVXnTVhAxrtLuubjskd7N4hVN7h2s4Z584wYLKYhrIBL0EViihOMzY4mH3YE4KZusfIx6oMcggKX9b3NHm0la7cj2zg0r6zjUn6ZCP4gXM99e5q4auc0OEfoSfQwofGi3WmxkG3tEozCB8Zz0wGbi2CzR8zlcF+BNV5I2LESlLzjPY5B4dvv5zjxsYoz94p3rUhKnnPM2zTx1kkilDK5C5fC1k9l/I/r5Qk4ebLQU= oscohen@localhost.localdomain"
-	infraEnvPullSecret   = "{\"auths\":{\"cloud.openshift.com\":{\"auth\":\"dXNlcjpwYXNzd29yZAo=\",\"email\":\"r@r.com\"}}}"
 )
 
 var _ = Describe("Infra_Env", func() {
@@ -33,8 +30,8 @@ var _ = Describe("Infra_Env", func() {
 			InfraenvCreateParams: &models.InfraEnvCreateParams{
 				Name:             swag.String("test-infra-env"),
 				OpenshiftVersion: swag.String(openshiftVersion),
-				PullSecret:       swag.String(infraEnvPullSecret),
-				SSHAuthorizedKey: swag.String(infraEnvSshPublicKey),
+				PullSecret:       swag.String(pullSecret),
+				SSHAuthorizedKey: swag.String(sshPublicKey),
 				ImageType:        models.ImageTypeFullIso,
 			},
 		})
@@ -45,6 +42,74 @@ var _ = Describe("Infra_Env", func() {
 
 	JustBeforeEach(func() {
 		infraEnvID = infraEnv.ID
+	})
+
+	It("download full-iso image success", func() {
+		file, err := ioutil.TempFile("", "tmp")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer os.Remove(file.Name())
+		_, err = userBMClient.Installer.DownloadInfraEnvDiscoveryImage(ctx, &installer.DownloadInfraEnvDiscoveryImageParams{InfraEnvID: infraEnvID}, file)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("update infra env", func() {
+		time.Sleep(time.Second * 10)
+		newSshKey := "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDi8KHZYGyPQjECHwytquI3rmpgoUn6M+lkeOD2nEKvYElLE5mPIeqF0izJIl56u" +
+			"ar2wda+3z107M9QkatE+dP4S9/Ltrlm+/ktAf4O6UoxNLUzv/TGHasb9g3Xkt8JTkohVzVK36622Sd8kLzEc61v1AonLWIADtpwq6/GvH" +
+			"MAuPK2R/H0rdKhTokylKZLDdTqQ+KUFelI6RNIaUBjtVrwkx1j0htxN11DjBVuUyPT2O1ejWegtrM0T+4vXGEA3g3YfbT2k0YnEzjXXqng" +
+			"qbXCYEJCZidp3pJLH/ilo4Y4BId/bx/bhzcbkZPeKlLwjR8g9sydce39bzPIQj+b7nlFv1Vot/77VNwkjXjYPUdUPu0d1PkFD9jKDOdB3f" +
+			"AC61aG2a/8PFS08iBrKiMa48kn+hKXC4G4D5gj/QzIAgzWSl2tEzGQSoIVTucwOAL/jox2dmAa0RyKsnsHORppanuW4qD7KAcmas1GHrAq" +
+			"IfNyDiU2JR50r1jCxj5H76QxIuM= root@ocp-edge34.lab.eng.tlv2.redhat.com"
+		updateParams := &installer.UpdateInfraEnvParams{
+			InfraEnvID: infraEnvID,
+			InfraEnvUpdateParams: &models.InfraEnvUpdateParams{
+				ImageType:              models.ImageTypeMinimalIso,
+				IgnitionConfigOverride: `{"ignition": {"version": "3.1.0"}, "storage": {"files": [{"path": "/tmp/example", "contents": {"source": "data:text/plain;base64,aGVscGltdHJhcHBlZGluYXN3YWdnZXJzcGVj"}}]}}`,
+				SSHAuthorizedKey:       swag.String(newSshKey),
+				Proxy:                  &models.Proxy{HTTPProxy: swag.String("http://proxy.proxy"), HTTPSProxy: nil, NoProxy: swag.String("proxy.proxy")},
+			},
+		}
+
+		res, err := userBMClient.Installer.UpdateInfraEnv(ctx, updateParams)
+		Expect(err).NotTo(HaveOccurred())
+		updateInfraEnv := res.Payload
+		Expect(updateInfraEnv.SSHAuthorizedKey).To(Equal(newSshKey))
+		Expect(swag.StringValue(updateInfraEnv.Proxy.HTTPProxy)).To(Equal("http://proxy.proxy"))
+		Expect(swag.StringValue(updateInfraEnv.Proxy.HTTPSProxy)).To(Equal(""))
+		Expect(swag.StringValue(updateInfraEnv.Proxy.NoProxy)).To(Equal("proxy.proxy"))
+		Expect(updateInfraEnv.Type).To(Equal(models.ImageTypeMinimalIso))
+	})
+
+	It("download minimal-iso image success", func() {
+		time.Sleep(time.Second * 10)
+		_, err := userBMClient.Installer.UpdateInfraEnv(ctx,
+			&installer.UpdateInfraEnvParams{InfraEnvID: infraEnvID,
+				InfraEnvUpdateParams: &models.InfraEnvUpdateParams{ImageType: models.ImageTypeMinimalIso}})
+		Expect(err).NotTo(HaveOccurred())
+		file, err := ioutil.TempFile("", "tmp")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer os.Remove(file.Name())
+		_, err = userBMClient.Installer.DownloadInfraEnvDiscoveryImage(ctx, &installer.DownloadInfraEnvDiscoveryImageParams{InfraEnvID: infraEnvID}, file)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("download minimal-initrd success", func() {
+		time.Sleep(time.Second * 10)
+		_, err := userBMClient.Installer.UpdateInfraEnv(ctx,
+			&installer.UpdateInfraEnvParams{InfraEnvID: infraEnvID,
+				InfraEnvUpdateParams: &models.InfraEnvUpdateParams{ImageType: models.ImageTypeMinimalIso}})
+		Expect(err).NotTo(HaveOccurred())
+		file, err := ioutil.TempFile("", "tmp")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer os.Remove(file.Name())
+		_, _, err = userBMClient.Installer.DownloadMinimalInitrd(ctx, &installer.DownloadMinimalInitrdParams{InfraEnvID: infraEnvID}, file)
+		Expect(err).NotTo(HaveOccurred())
 	})
 
 	It("download infra-env files discovery ignition file", func() {
@@ -71,5 +136,25 @@ var _ = Describe("Infra_Env", func() {
 		Expect(err).NotTo(HaveOccurred())
 		Expect(len(resp.Payload)).To(Equal(1))
 		Expect(resp.Payload[0]).To(Equal(infraEnv))
+	})
+
+	It("deregister empty infra-env", func() {
+		_, err := userBMClient.Installer.DeregisterInfraEnv(ctx, &installer.DeregisterInfraEnvParams{InfraEnvID: infraEnvID})
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("deregister non-empty infra-env shold fail", func() {
+		hostID := strToUUID(uuid.New().String())
+		// register to infra-env
+		_, err := agentBMClient.Installer.V2RegisterHost(context.Background(), &installer.V2RegisterHostParams{
+			InfraEnvID: infraEnvID,
+			NewHostParams: &models.HostCreateParams{
+				HostID: hostID,
+			},
+		})
+		Expect(err).NotTo(HaveOccurred())
+		_, err = userBMClient.Installer.DeregisterInfraEnv(ctx, &installer.DeregisterInfraEnvParams{InfraEnvID: infraEnvID})
+
+		Expect(err).To(HaveOccurred())
 	})
 })
