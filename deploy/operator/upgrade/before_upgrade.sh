0c7e4c55e74fc14c906d8b788096cd0ddf99643e
Pawan Pinjarkar <pawan.pinjarkar@gmail.com>
2021-09-21 18:02:36 -0400

    MGMT-7282: Use community operators to install AI operator for ocm-2.3

--- deploy/operator/upgrade/before_upgrade.sh
+++ deploy/operator/upgrade/before_upgrade.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export SERVICE_IMAGE=""
-export INDEX_IMAGE="${INDEX_IMAGE_INSTALL_OVERRIDE:-quay.io/ocpmetal/assisted-service-index:ocm-2.3}"
+export CATALOG_SOURCE_NAME="${CATALOG_SOURCE_NAME:-community-operators}"
+export CHANNEL="${CHANNEL_INSTALL_OVERRIDE:-ocm-2.3}"
 export ASSISTED_UPGRADE_OPERATOR="${ASSISTED_UPGRADE_OPERATOR:-true}"
 export ASSISTED_STOP_AFTER_AGENT_DISCOVERY="${ASSISTED_STOP_AFTER_AGENT_DISCOVERY:-false}"
