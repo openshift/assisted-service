fbaf3a63351fc56c7a48ad90cece9ab1e64cba81
Mat Kowalski <mko@redhat.com>
2021-09-23 13:30:08 +0200

    Bug 2007106: Extend dual-stack network subnet validations
    This commit extends the validations performed for dual-stack OCP
    clusters by making sure the following requirements are met if a cluster
    is dual-stack
    
    * exactly 2 machine networks are provided (if any)
    * exactly 2 service networks are provided
    * exactly 2 cluster networks are provided
    * for every list containing 2 networks, the first one has to be IPv4 and
      the second one IPv6
    
    Closes-bug: #2007106
    Contributes-to: #2005440
    Closes: OCPBUGSM-35447
    Contributes-to: OCPBUGSM-35273

--- internal/network/utils.go
+++ internal/network/utils.go
@@ -66,10 +66,70 @@ func GetConfiguredAddressFamilies(cluster *common.Cluster) (ipv4 bool, ipv6 bool
 	return
 }
 
+func GetAddressFamilies(networks interface{}) (ipv4 bool, ipv6 bool, err error) {
+	addresses := make([]models.Subnet, 0)
+	ipv4 = false
+	ipv6 = false
+
+	switch v := networks.(type) {
+	case []*models.ClusterNetwork:
+		for _, net := range v {
+			addresses = append(addresses, net.Cidr)
+		}
+	case []*models.ServiceNetwork:
+		for _, net := range v {
+			addresses = append(addresses, net.Cidr)
+		}
+	case []*models.MachineNetwork:
+		for _, net := range v {
+			addresses = append(addresses, net.Cidr)
+		}
+	default:
+		return false, false, errors.New("Provided invalid network list")
+	}
+
+	for _, cidr := range addresses {
+		if IsIPV4CIDR(string(cidr)) {
+			ipv4 = true
+		} else {
+			ipv6 = true
+		}
+	}
+
+	return ipv4, ipv6, nil
+}
+
 func IsMachineCidrAvailable(cluster *common.Cluster) bool {
 	return common.IsSliceNonEmpty(cluster.MachineNetworks)
 }
 
 func GetMachineCidrById(cluster *common.Cluster, index int) string {
 	return string(cluster.MachineNetworks[index].Cidr)
+}
+
+func DerefMachineNetworks(obj interface{}) []*models.MachineNetwork {
+	switch v := obj.(type) {
+	case []*models.MachineNetwork:
+		return v
+	default:
+		return nil
+	}
+}
+
+func DerefServiceNetworks(obj interface{}) []*models.ServiceNetwork {
+	switch v := obj.(type) {
+	case []*models.ServiceNetwork:
+		return v
+	default:
+		return nil
+	}
+}
+
+func DerefClusterNetworks(obj interface{}) []*models.ClusterNetwork {
+	switch v := obj.(type) {
+	case []*models.ClusterNetwork:
+		return v
+	default:
+		return nil
+	}
 }
