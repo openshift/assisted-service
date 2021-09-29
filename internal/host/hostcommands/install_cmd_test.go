openshift/assisted-service

commit 8cf51023fecb0ef0d213b8d0d216176f903f0190

Bug 2007515: Pass copy-network when infraEnv.StaticNetworkConfig is set (#2662)
Originally, assisted-service would add the `--copy-network` argument to
the coreos-install command when the cluster.StaticNetworkConfigured flag
was set. This flag, however, is only been set to true for cloud-managed
deployments.

The cluster.StaticNetworkConfigured flag became a cached value of data
that should be considered dynamic, which broke the abstraction and the
separation of concerns of the various objects managed internally.
Specifically, it put in the cluster data that should only belong to the
infraenv.

This commit removes the use of `cluster.StaticNetworkConfigured` when
deciding to pass the `--copy-network` flag in favor of just checking
that the `infraenv.StaticNetworkConfig` is not empty.

The field in the model is not being removed in this commit as this will
be backported and it'd be preferable to not do database migrations in
backports. A follow-up commit will take care of removing the DB column.

Signed-off-by: Flavio Percoco <flavio@redhat.com>

Co-authored-by: Flavio Percoco <flavio@redhat.com>

internal/host/hostcommands/install_cmd.go
@@ -73,7 +73,12 @@ func (i *installCmd) GetSteps(ctx context.Context, host *models.Host) ([]*models
 		return nil, err
 	}
 
-	fullCmd, err := i.getFullInstallerCommand(cluster, host, bootdevice)
+	infraEnv, err := common.GetInfraEnvByClusterFromDB(i.db, *cluster.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	fullCmd, err := i.getFullInstallerCommand(cluster, host, infraEnv, bootdevice)
 	if err != nil {
 		return nil, err
 	}
@@ -93,7 +98,7 @@ func (i *installCmd) GetSteps(ctx context.Context, host *models.Host) ([]*models
 	return []*models.Step{step}, nil
 }
 
-func (i *installCmd) getFullInstallerCommand(cluster *common.Cluster, host *models.Host, bootdevice string) (string, error) {
+func (i *installCmd) getFullInstallerCommand(cluster *common.Cluster, host *models.Host, infraEnv *common.InfraEnv, bootdevice string) (string, error) {
 	role := host.Role
 	if host.Bootstrap {
 		role = models.HostRoleBootstrap
@@ -159,7 +164,7 @@ func (i *installCmd) getFullInstallerCommand(cluster *common.Cluster, host *mode
 		installerCmd = append(installerCmd, "--cacert", common.HostCACertPath)
 	}
 
-	hostInstallerArgs, err := constructHostInstallerArgs(cluster, host, i.log)
+	hostInstallerArgs, err := constructHostInstallerArgs(cluster, host, infraEnv, i.log)
 	if err != nil {
 		return "", err
 	}
@@ -263,9 +268,10 @@ func (i *installCmd) getDiskUnbootableCmd(ctx context.Context, host models.Host)
 	set --copy-network, function will set only one such argument. It also append an arg that
 	controls DHCP depending on the IP stack being used.
 */
-func constructHostInstallerArgs(cluster *common.Cluster, host *models.Host, log logrus.FieldLogger) (string, error) {
+func constructHostInstallerArgs(cluster *common.Cluster, host *models.Host, infraEnv *common.InfraEnv, log logrus.FieldLogger) (string, error) {
 
 	var installerArgs []string
+
 	if host.InstallerArgs != "" {
 		err := json.Unmarshal([]byte(host.InstallerArgs), &installerArgs)
 		if err != nil {
@@ -278,7 +284,7 @@ func constructHostInstallerArgs(cluster *common.Cluster, host *models.Host, log 
 		return "", err
 	}
 
-	if cluster.StaticNetworkConfigured && !funk.Contains(installerArgs, "--copy-network") {
+	if (cluster.StaticNetworkConfigured || infraEnv.StaticNetworkConfig != "") && !funk.Contains(installerArgs, "--copy-network") {
 		// network not configured statically or
 		// installer args already contain command for network configuration
 		installerArgs = append(installerArgs, "--copy-network")

commit 72700cdbd08860ff883883a35f2ee68844e4c376

Bug 2007539: Pass copy-network when infraEnv.StaticNetworkConfig is set (#2663)
* Bug 2007515: Pass copy-network when infraEnv.StaticNetworkConfig is set (#2662)

Originally, assisted-service would add the `--copy-network` argument to
the coreos-install command when the cluster.StaticNetworkConfigured flag
was set. This flag, however, is only been set to true for cloud-managed
deployments.

The cluster.StaticNetworkConfigured flag became a cached value of data
that should be considered dynamic, which broke the abstraction and the
separation of concerns of the various objects managed internally.
Specifically, it put in the cluster data that should only belong to the
infraenv.

This commit removes the use of `cluster.StaticNetworkConfigured` when
deciding to pass the `--copy-network` flag in favor of just checking
that the `infraenv.StaticNetworkConfig` is not empty.

The field in the model is not being removed in this commit as this will
be backported and it'd be preferable to not do database migrations in
backports. A follow-up commit will take care of removing the DB column.

Signed-off-by: Flavio Percoco <flavio@redhat.com>

Co-authored-by: Flavio Percoco <flavio@redhat.com>
(cherry picked from commit 8cf51023fecb0ef0d213b8d0d216176f903f0190)

* Bug 2007515: Fix regression introduced by copy-network bugfix (#2667)

Commit 8cf51023fecb0ef0d213b8d0d216176f903f0190 (#2662) introduced a bug
which broke late binding.

The `GetInfraEnvByClusterFromDB` function introduced by that commit
wrongly assumed that every infra env must have a corresponding cluster
ID. This is not the case for late-binding infra-envs where infra envs
can have no cluster ID.

This commit gets rid of the `GetInfraEnvByClusterFromDB` function and instead fetches
the infra-env database object during install command creation by taking the infraenv
ID from the host and using the existing `GetInfraEnvFromDB` function.

All "modern" hosts are guaranteed to have an infra env ID.

internal/host/hostcommands/install_cmd.go
@@ -73,7 +73,15 @@ func (i *installCmd) GetSteps(ctx context.Context, host *models.Host) ([]*models
 		return nil, err
 	}
 
-	fullCmd, err := i.getFullInstallerCommand(cluster, host, bootdevice)
+	var infraEnv *common.InfraEnv = nil
+	if host.InfraEnvID != "" {
+		infraEnv, err = common.GetInfraEnvFromDB(i.db, host.InfraEnvID)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	fullCmd, err := i.getFullInstallerCommand(cluster, host, infraEnv, bootdevice)
 	if err != nil {
 		return nil, err
 	}
@@ -93,7 +101,7 @@ func (i *installCmd) GetSteps(ctx context.Context, host *models.Host) ([]*models
 	return []*models.Step{step}, nil
 }
 
-func (i *installCmd) getFullInstallerCommand(cluster *common.Cluster, host *models.Host, bootdevice string) (string, error) {
+func (i *installCmd) getFullInstallerCommand(cluster *common.Cluster, host *models.Host, infraEnv *common.InfraEnv, bootdevice string) (string, error) {
 	role := host.Role
 	if host.Bootstrap {
 		role = models.HostRoleBootstrap
@@ -159,7 +167,7 @@ func (i *installCmd) getFullInstallerCommand(cluster *common.Cluster, host *mode
 		installerCmd = append(installerCmd, "--cacert", common.HostCACertPath)
 	}
 
-	hostInstallerArgs, err := constructHostInstallerArgs(cluster, host, i.log)
+	hostInstallerArgs, err := constructHostInstallerArgs(cluster, host, infraEnv, i.log)
 	if err != nil {
 		return "", err
 	}
@@ -263,9 +271,10 @@ func (i *installCmd) getDiskUnbootableCmd(ctx context.Context, host models.Host)
 	set --copy-network, function will set only one such argument. It also append an arg that
 	controls DHCP depending on the IP stack being used.
 */
-func constructHostInstallerArgs(cluster *common.Cluster, host *models.Host, log logrus.FieldLogger) (string, error) {
+func constructHostInstallerArgs(cluster *common.Cluster, host *models.Host, infraEnv *common.InfraEnv, log logrus.FieldLogger) (string, error) {
 
 	var installerArgs []string
+
 	if host.InstallerArgs != "" {
 		err := json.Unmarshal([]byte(host.InstallerArgs), &installerArgs)
 		if err != nil {
@@ -278,7 +287,8 @@ func constructHostInstallerArgs(cluster *common.Cluster, host *models.Host, log 
 		return "", err
 	}
 
-	if cluster.StaticNetworkConfigured && !funk.Contains(installerArgs, "--copy-network") {
+	hasStaticNetwork := (infraEnv != nil && infraEnv.StaticNetworkConfig != "") || cluster.StaticNetworkConfigured
+	if hasStaticNetwork && !funk.Contains(installerArgs, "--copy-network") {
 		// network not configured statically or
 		// installer args already contain command for network configuration
 		installerArgs = append(installerArgs, "--copy-network")

internal/host/hostcommands/install_cmd_test.go
@@ -53,6 +53,7 @@ var _ = Describe("installcmd", func() {
 		installCmd        *installCmd
 		clusterId         strfmt.UUID
 		infraEnvId        strfmt.UUID
+		infraEnv          common.InfraEnv
 		stepReply         []*models.Step
 		stepErr           error
 		ctrl              *gomock.Controller
@@ -74,7 +75,8 @@ var _ = Describe("installcmd", func() {
 		installCmd = NewInstallCmd(common.GetTestLog(), db, mockValidator, mockRelease, instructionConfig, mockEvents, mockVersions)
 		cluster = createClusterInDb(db, models.ClusterHighAvailabilityModeFull)
 		clusterId = *cluster.ID
-		infraEnvId = strfmt.UUID(uuid.New().String())
+		infraEnv = createInfraEnvInDb(db, clusterId)
+		infraEnvId = infraEnv.ID
 		host = createHostInDb(db, infraEnvId, clusterId, models.HostRoleMaster, false, "")
 	})
 
@@ -279,6 +281,7 @@ var _ = Describe("installcmd arguments", func() {
 		mockEvents   *events.MockHandler
 		mockVersions *versions.MockHandler
 		infraEnvId   strfmt.UUID
+		infraEnv     common.InfraEnv
 	)
 
 	mockImages := func() {
@@ -289,7 +292,8 @@ var _ = Describe("installcmd arguments", func() {
 	BeforeSuite(func() {
 		db, dbName = common.PrepareTestDB()
 		cluster = createClusterInDb(db, models.ClusterHighAvailabilityModeNone)
-		infraEnvId = strfmt.UUID(uuid.New().String())
+		infraEnv = createInfraEnvInDb(db, *cluster.ID)
+		infraEnvId = infraEnv.ID
 		host = createHostInDb(db, infraEnvId, *cluster.ID, models.HostRoleMaster, false, "")
 		ctrl = gomock.NewController(GinkgoT())
 		validator = hardware.NewMockValidator(ctrl)
@@ -441,7 +445,7 @@ var _ = Describe("installcmd arguments", func() {
 		})
 
 		It("empty installer args with static ip config", func() {
-			db.Model(&cluster).Update("static_network_configured", true)
+			db.Model(&infraEnv).Update("static_network_config", "{'test': 'test'}")
 			host.InstallerArgs = ""
 			stepReply, err := installCmd.GetSteps(ctx, &host)
 			Expect(err).NotTo(HaveOccurred())
@@ -450,7 +454,7 @@ var _ = Describe("installcmd arguments", func() {
 		})
 
 		It("non-empty installer args with static ip config", func() {
-			db.Model(&cluster).Update("static_network_configured", true)
+			db.Model(&infraEnv).Update("static_network_config", "{'test': 'test'}")
 			host.InstallerArgs = `["--append-karg","nameserver=8.8.8.8","-n"]`
 			stepReply, err := installCmd.GetSteps(ctx, &host)
 			Expect(err).NotTo(HaveOccurred())
@@ -556,9 +560,10 @@ var _ = Describe("installcmd arguments", func() {
 
 var _ = Describe("construct host install arguments", func() {
 	var (
-		cluster *common.Cluster
-		host    *models.Host
-		log     = common.GetTestLog()
+		cluster  *common.Cluster
+		host     *models.Host
+		infraEnv *common.InfraEnv
+		log      = common.GetTestLog()
 	)
 	BeforeEach(func() {
 		clusterID := strfmt.UUID(uuid.New().String())
@@ -568,10 +573,21 @@ var _ = Describe("construct host install arguments", func() {
 				ImageInfo: &models.ImageInfo{},
 			},
 		}
+
+		infraEnvID := strfmt.UUID(uuid.New().String())
+		infraEnv = &common.InfraEnv{
+			InfraEnv: models.InfraEnv{
+				ID:        infraEnvID,
+				ClusterID: clusterID,
+			},
+		}
+
 		hostID := strfmt.UUID(uuid.New().String())
 		host = &models.Host{
-			ID: &hostID,
+			ID:         &hostID,
+			InfraEnvID: infraEnvID,
 		}
+
 	})
 	It("ip=<nic>:dhcp6 added when machine CIDR is IPv6", func() {
 		cluster.MachineNetworks = []*models.MachineNetwork{{Cidr: "2001:db8::/64"}}
@@ -583,7 +599,7 @@ var _ = Describe("construct host install arguments", func() {
 				}
 			]
 		}`
-		args, err := constructHostInstallerArgs(cluster, host, log)
+		args, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(args).To(Equal(`["--append-karg","ip=eth0:dhcp6"]`))
 	})
@@ -597,7 +613,7 @@ var _ = Describe("construct host install arguments", func() {
 				}
 			]
 		}`
-		args, err := constructHostInstallerArgs(cluster, host, log)
+		args, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(args).To(Equal(""))
 	})
@@ -611,7 +627,7 @@ var _ = Describe("construct host install arguments", func() {
 				}
 			]
 		}`
-		args, err := constructHostInstallerArgs(cluster, host, log)
+		args, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(args).To(Equal(`["--append-karg","ip=eth1:dhcp"]`))
 	})
@@ -625,7 +641,7 @@ var _ = Describe("construct host install arguments", func() {
 				}
 			]
 		}`
-		args, err := constructHostInstallerArgs(cluster, host, log)
+		args, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(args).To(Equal(`["--append-karg","ip=eth1:dhcp"]`))
 	})
@@ -643,7 +659,7 @@ var _ = Describe("construct host install arguments", func() {
 				}
 			]
 		}`
-		args, err := constructHostInstallerArgs(cluster, host, log)
+		args, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(args).To(Equal(`["--append-karg","ip=eth1:dhcp"]`))
 	})
@@ -657,7 +673,7 @@ var _ = Describe("construct host install arguments", func() {
 				}
 			]
 		}`
-		args, err := constructHostInstallerArgs(cluster, host, log)
+		args, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(args).To(Equal(""))
 	})
@@ -682,7 +698,7 @@ var _ = Describe("construct host install arguments", func() {
 				}
 			]
 		}`
-		args, err := constructHostInstallerArgs(cluster, host, log)
+		args, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(args).To(Equal(`["--append-karg","ip=eth0:dhcp6"]`))
 	})
@@ -707,7 +723,7 @@ var _ = Describe("construct host install arguments", func() {
 				}
 			]
 		}`
-		args, err := constructHostInstallerArgs(cluster, host, log)
+		args, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(args).To(Equal(""))
 	})
@@ -732,7 +748,7 @@ var _ = Describe("construct host install arguments", func() {
 				}
 			]
 		}`
-		args, err := constructHostInstallerArgs(cluster, host, log)
+		args, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(args).To(Equal(`["--append-karg","ip=eth1:dhcp"]`))
 	})
@@ -757,14 +773,14 @@ var _ = Describe("construct host install arguments", func() {
 				}
 			]
 		}`
-		args, err := constructHostInstallerArgs(cluster, host, log)
+		args, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(args).To(Equal(""))
 	})
 	It("ip=<nic>:dhcp and copy-network added with static config", func() {
 		cluster.MachineNetworks = []*models.MachineNetwork{{Cidr: "192.186.10.0/24"}}
+		infraEnv.StaticNetworkConfig = "something"
 		cluster.ImageInfo.StaticNetworkConfig = "something"
-		cluster.StaticNetworkConfigured = true
 		host.Inventory = `{
 			"interfaces":[
 				{
@@ -773,13 +789,14 @@ var _ = Describe("construct host install arguments", func() {
 				}
 			]
 		}`
-		args, err := constructHostInstallerArgs(cluster, host, log)
+		args, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(args).To(Equal(`["--append-karg","ip=eth1:dhcp","--copy-network"]`))
 	})
 	It("ip=<nic>:dhcp added with static config and copy-network set by the user", func() {
 		host.InstallerArgs = `["--copy-network"]`
 		cluster.MachineNetworks = []*models.MachineNetwork{{Cidr: "192.186.10.0/24"}}
+		infraEnv.StaticNetworkConfig = "something"
 		cluster.ImageInfo.StaticNetworkConfig = "something"
 		host.Inventory = `{
 			"interfaces":[
@@ -789,7 +806,7 @@ var _ = Describe("construct host install arguments", func() {
 				}
 			]
 		}`
-		args, err := constructHostInstallerArgs(cluster, host, log)
+		args, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(args).To(Equal(`["--copy-network","--append-karg","ip=eth0:dhcp"]`))
 	})
@@ -804,7 +821,7 @@ var _ = Describe("construct host install arguments", func() {
 				}
 			]
 		}`
-		args, err := constructHostInstallerArgs(cluster, host, log)
+		args, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(args).To(Equal(`["--copy-network","--append-karg","ip=ens3:dhcp"]`))
 	})
@@ -819,7 +836,7 @@ var _ = Describe("construct host install arguments", func() {
 				}
 			]
 		}`
-		args, err := constructHostInstallerArgs(cluster, host, log)
+		args, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(args).To(Equal(`["--append-karg","rd.break=cmdline","--append-karg","ip=eth1:dhcp6"]`))
 	})
@@ -834,7 +851,7 @@ var _ = Describe("construct host install arguments", func() {
 				}
 			]
 		}`
-		args, err := constructHostInstallerArgs(cluster, host, log)
+		args, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(args).To(Equal(`["--append-karg","rd.break=cmdline","--append-karg","ip=eth2:dhcp"]`))
 	})
@@ -842,7 +859,7 @@ var _ = Describe("construct host install arguments", func() {
 		kargs := `["--append-karg","ip=dhcp"]`
 		host.InstallerArgs = kargs
 		cluster.MachineNetworks = []*models.MachineNetwork{{Cidr: "2001:db8::/120"}}
-		args, err := constructHostInstallerArgs(cluster, host, log)
+		args, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(args).To(Equal(kargs))
 	})
@@ -850,7 +867,7 @@ var _ = Describe("construct host install arguments", func() {
 		kargs := `["--append-karg","ip=dhcp6"]`
 		host.InstallerArgs = kargs
 		cluster.MachineNetworks = []*models.MachineNetwork{{Cidr: "192.186.10.0/24"}}
-		args, err := constructHostInstallerArgs(cluster, host, log)
+		args, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(args).To(Equal(kargs))
 	})
@@ -858,7 +875,7 @@ var _ = Describe("construct host install arguments", func() {
 		kargs := `["--append-karg","ip=eth0:any"]`
 		host.InstallerArgs = kargs
 		cluster.MachineNetworks = []*models.MachineNetwork{{Cidr: "192.186.10.0/24"}}
-		args, err := constructHostInstallerArgs(cluster, host, log)
+		args, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(args).To(Equal(kargs))
 	})
@@ -866,7 +883,7 @@ var _ = Describe("construct host install arguments", func() {
 		kargs := `["--delete-karg","ip=dhcp"]`
 		host.InstallerArgs = kargs
 		cluster.MachineNetworks = []*models.MachineNetwork{{Cidr: "2001:db8::/120"}}
-		args, err := constructHostInstallerArgs(cluster, host, log)
+		args, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(args).To(Equal(kargs))
 	})
@@ -874,12 +891,12 @@ var _ = Describe("construct host install arguments", func() {
 		kargs := `["--delete-karg","ip=dhcp6"]`
 		host.InstallerArgs = kargs
 		cluster.MachineNetworks = []*models.MachineNetwork{{Cidr: "192.186.10.0/24"}}
-		args, err := constructHostInstallerArgs(cluster, host, log)
+		args, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(args).To(Equal(kargs))
 	})
 	It("error if machine CIDR not given and no hosts", func() {
-		_, err := constructHostInstallerArgs(cluster, host, log)
+		_, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).To(HaveOccurred())
 	})
 	It("error if machine CIDR not given and no bootsrap", func() {
@@ -889,7 +906,7 @@ var _ = Describe("construct host install arguments", func() {
 				Inventory: `{"interfaces":[{"ipv4_addresses":["192.186.10.12/24"]}]}`,
 			},
 		}
-		_, err := constructHostInstallerArgs(cluster, host, log)
+		_, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).To(HaveOccurred())
 	})
 	It("ip argument not set in day2 when ipv4", func() {
@@ -902,7 +919,7 @@ var _ = Describe("construct host install arguments", func() {
 					}
 				]
 			}`
-		args, err := constructHostInstallerArgs(cluster, host, log)
+		args, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(args).To(Equal(""))
 	})
@@ -916,7 +933,7 @@ var _ = Describe("construct host install arguments", func() {
 					}
 				]
 			}`
-		args, err := constructHostInstallerArgs(cluster, host, log)
+		args, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(args).To(Equal(""))
 	})
@@ -930,14 +947,14 @@ var _ = Describe("construct host install arguments", func() {
 					}
 				]
 			}`
-		args, err := constructHostInstallerArgs(cluster, host, log)
+		args, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).NotTo(HaveOccurred())
 		Expect(args).To(Equal(""))
 	})
 	It("error when inventory corrupted", func() {
 		cluster.Kind = swag.String(models.ClusterKindAddHostsCluster)
 		host.Inventory = ""
-		_, err := constructHostInstallerArgs(cluster, host, log)
+		_, err := constructHostInstallerArgs(cluster, host, infraEnv, log)
 		Expect(err).To(HaveOccurred())
 	})
 })
@@ -996,6 +1013,18 @@ func createClusterInDb(db *gorm.DB, haMode string) common.Cluster {
 	}}
 	Expect(db.Create(&cluster).Error).ShouldNot(HaveOccurred())
 	return cluster
+}
+
+func createInfraEnvInDb(db *gorm.DB, clusterId strfmt.UUID) common.InfraEnv {
+	infraEnvId := strfmt.UUID(uuid.New().String())
+	infraEnv := common.InfraEnv{
+		InfraEnv: models.InfraEnv{
+			ID:        infraEnvId,
+			ClusterID: clusterId,
+		},
+	}
+	Expect(db.Create(&infraEnv).Error).ShouldNot(HaveOccurred())
+	return infraEnv
 }
 
 func createHostInDb(db *gorm.DB, infraEnvId, clusterId strfmt.UUID, role models.HostRole, bootstrap bool, hostname string) models.Host {

internal/host/hostcommands/instruction_manager_test.go
@@ -82,6 +82,14 @@ var _ = Describe("instruction_manager", func() {
 					BaseDNSDomain:     "test.com",
 				}}
 			Expect(db.Create(&cluster).Error).ShouldNot(HaveOccurred())
+
+			infraEnv := common.InfraEnv{
+				InfraEnv: models.InfraEnv{
+					ID:        infraEnvId,
+					ClusterID: clusterId,
+				},
+			}
+			Expect(db.Create(&infraEnv).Error).ShouldNot(HaveOccurred())
 		})
 		Context("get_next_steps", func() {
 			It("invalid_host_state", func() {
@@ -163,6 +171,14 @@ var _ = Describe("instruction_manager", func() {
 				BaseDNSDomain:     "test.com",
 			}}
 			Expect(db.Create(&cluster).Error).ShouldNot(HaveOccurred())
+
+			infraEnv := common.InfraEnv{
+				InfraEnv: models.InfraEnv{
+					ID:        infraEnvId,
+					ClusterID: clusterId,
+				},
+			}
+			Expect(db.Create(&infraEnv).Error).ShouldNot(HaveOccurred())
 		})
 		Context("get_next_steps", func() {
 			It("invalid_host_state", func() {
@@ -304,6 +320,14 @@ var _ = Describe("instruction_manager", func() {
 					MachineNetworks:   common.TestIPv4Networking.MachineNetworks,
 				}}
 				Expect(db.Create(&cluster).Error).ShouldNot(HaveOccurred())
+
+				infraEnv := common.InfraEnv{
+					InfraEnv: models.InfraEnv{
+						ID:        infraEnvId,
+						ClusterID: clusterId,
+					},
+				}
+				Expect(db.Create(&infraEnv).Error).ShouldNot(HaveOccurred())
 			})
 			It("Should not filter out any step when: HostState=installing DisabledSteps=execute.", func() {
 				instMng = createInstMngWithDisabledSteps([]models.StepType{models.StepTypeExecute})