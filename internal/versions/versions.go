00a1ca9eabb4867a26b96b415b3e07ab9a25096e
Daniel Erez <danielerez@gmail.com>
2021-10-03 13:55:39 +0300

    OCPBUGSM-35272: empty cpu architecture implies default
    If 'cpuArchitecture' is not specified under osImages in AgentServiceConfig,
    an empty string is specified in OS_IMAGES[*].
    Hance, handling an empty string as missing property - default to x86_64.
    
    Also added the property to the CRD sample to make it explicit.
    
    [*]
    https://github.com/openshift/assisted-service/blob/824eef89ac58cf89a474d9272e9d54b4b353c911/internal/controller/controllers/agentserviceconfig_controller.go#L1206

--- internal/versions/versions.go
+++ internal/versions/versions.go
@@ -313,10 +313,14 @@ func (h *handler) GetCPUArchitectures(openshiftVersion string) ([]string, error)
 			if release.OpenshiftVersion == nil {
 				return nil, errors.Errorf("Missing openshift_version in OsImage")
 			}
-			if release.CPUArchitecture == nil {
-				return nil, errors.Errorf("Missing cpu_architecture in OsImage")
+			if swag.StringValue(release.CPUArchitecture) == "" {
+				// Empty or missing property implies default CPU architecture
+				defaultArch := common.DefaultCPUArchitecture
+				release.CPUArchitecture = &defaultArch
 			}
-			cpuArchitectures = append(cpuArchitectures, *release.CPUArchitecture)
+			if !funk.Contains(cpuArchitectures, *release.CPUArchitecture) {
+				cpuArchitectures = append(cpuArchitectures, *release.CPUArchitecture)
+			}
 		}
 	}
 
