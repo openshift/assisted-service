69073a78b7164c769f10acd2ea07320299b8f5a6
Mat Kowalski <mko@redhat.com>
2021-10-08 16:24:06 +0200

    Bug 2005440: Use current and new cluster data for dual-stack check
    This commit extends the dual-stack validator to use both existing
    cluster data and new cluster params when checking whether a
    dual-stackness is requested.
    
    The required behaviour comes from the fact that when using Kube API and
    creating a new cluster, the initial payload contains empty Machine
    Networks and only afterwards the reconcile loop is creating a cluster
    update request with desired Machine Networks. This means that during
    creation of the cluster we never have Cluster/Service Networks and
    Machine Networks available in the same payload, so that a single
    validation is not enough.
    
    In order to overcome this issue, dual-stackness validation takes into
    account the current payload as well as the previous state of the
    cluster. This allows us to handle a scenario where Cluster/Service
    Networks are updated separately from Machine Networks, even if all of
    them were passed in the same CR.
    
    Closes-bug: #2005440

--- internal/bminventory/inventory.go
+++ internal/bminventory/inventory.go
@@ -392,6 +392,7 @@ func (b *bareMetalInventory) setDefaultRegisterClusterParams(_ context.Context, 
 	return params
 }
 
+//nolint:gocyclo // This function is complex but we allow it for the consistency of cherry-picks
 func (b *bareMetalInventory) RegisterClusterInternal(
 	ctx context.Context,
 	kubeKey *types.NamespacedName,
@@ -422,6 +423,9 @@ func (b *bareMetalInventory) RegisterClusterInternal(
 	}()
 
 	if err = validations.ValidateIPAddresses(b.IPv6Support, params.NewClusterParams); err != nil {
+		return nil, common.NewApiError(http.StatusBadRequest, err)
+	}
+	if err = validations.ValidateDualStackNetworks(params.NewClusterParams, false); err != nil {
 		return nil, common.NewApiError(http.StatusBadRequest, err)
 	}
 
@@ -2245,6 +2249,11 @@ func (b *bareMetalInventory) updateClusterInternal(ctx context.Context, v1Params
 		return nil, common.NewApiError(http.StatusNotFound, err)
 	}
 
+	alreadyDualStack := network.CheckIfClusterIsDualStack(cluster)
+	if err = validations.ValidateDualStackNetworks(v2Params.ClusterUpdateParams, alreadyDualStack); err != nil {
+		return nil, common.NewApiError(http.StatusBadRequest, err)
+	}
+
 	if v2Params, err = b.validateAndUpdateProxyParams(ctx, &v2Params, &cluster.OpenshiftVersion); err != nil {
 		return nil, common.NewApiError(http.StatusBadRequest, err)
 	}
@@ -2366,6 +2375,11 @@ func (b *bareMetalInventory) v2UpdateClusterInternal(ctx context.Context, params
 	if cluster, err = common.GetClusterFromDB(tx, params.ClusterID, common.UseEagerLoading); err != nil {
 		log.WithError(err).Errorf("failed to get cluster: %s", params.ClusterID)
 		return nil, common.NewApiError(http.StatusNotFound, err)
+	}
+
+	alreadyDualStack := network.CheckIfClusterIsDualStack(cluster)
+	if err = validations.ValidateDualStackNetworks(params.ClusterUpdateParams, alreadyDualStack); err != nil {
+		return nil, common.NewApiError(http.StatusBadRequest, err)
 	}
 
 	if params, err = b.validateAndUpdateProxyParams(ctx, &params, &cluster.OpenshiftVersion); err != nil {
