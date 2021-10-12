3d88c648df74f007ea9e1525774503c65b545570
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2021-10-12 10:36:10 -0700

    Bug 2012901: Allow Agent/AssistedController to authenticate for V2DownloadClusterCredentials (#2758)
    This PR add securiry definition agentAuth to the V2DownloadClusterCredentials in swagger
    to allow this API to be used and authenticated from the agent/assisted-controller
    
    Co-authored-by: Yevgeny Shnaidman <yshnaidm@redhat.com>

--- restapi/embedded_spec.go
+++ restapi/embedded_spec.go
@@ -5211,6 +5211,9 @@ func init() {
           },
           {
             "urlAuth": []
+          },
+          {
+            "agentAuth": []
           }
         ],
         "description": "Downloads credentials relating to the installed/installing cluster.",
@@ -18039,6 +18042,9 @@ func init() {
           },
           {
             "urlAuth": []
+          },
+          {
+            "agentAuth": []
           }
         ],
         "description": "Downloads credentials relating to the installed/installing cluster.",
