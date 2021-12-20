254699200fb2f35cee163feef9bbd4cede9f955f
Eran Cohen <eranco@redhat.com>
2021-12-20 17:05:18 +0200

    MGMT-8623: capi-provider-agent fail to add host to hypershift due to API VIP connectivity failure Updated ignition-endpoint ca_certificate description to say that the content should be base64 encoded

--- restapi/embedded_spec.go
+++ restapi/embedded_spec.go
@@ -11661,7 +11661,7 @@ func init() {
       "type": "object",
       "properties": {
         "ca_certificate": {
-          "description": "A CA certficate to be used when contacting the URL via https.",
+          "description": "base64 encoded CA certficate to be used when contacting the URL via https.",
           "type": "string",
           "x-nullable": true
         },
@@ -25069,7 +25069,7 @@ func init() {
       "type": "object",
       "properties": {
         "ca_certificate": {
-          "description": "A CA certficate to be used when contacting the URL via https.",
+          "description": "base64 encoded CA certficate to be used when contacting the URL via https.",
           "type": "string",
           "x-nullable": true
         },
