05833929acad369037837a7cb54f38671f772666
Igal Tsoiref <itsoiref@redhat.com>
2022-01-03 17:40:34 +0200

    MGMT-8498: fixing issue with not mounting pki due to old next_step_runner code. Removig v2_next_step_runner_cmd.go as it is duplicate of next_step_runner_cmd.go and changing ext_step_runner_cmd.go to use infra env. This will allow us to have one file with better name and will allow not to make mistakes cause of code duplication

--- internal/bminventory/inventory.go
+++ internal/bminventory/inventory.go
@@ -3684,7 +3684,7 @@ func (b *bareMetalInventory) generateV2NextStepRunnerCommand(ctx context.Context
 			params.NewHostParams.HostID.String(), params.InfraEnvID.String(), params.NewHostParams.DiscoveryAgentVersion, currentImageTag)
 	}
 
-	config := hostcommands.V2NextStepRunnerConfig{
+	config := hostcommands.NextStepRunnerConfig{
 		ServiceBaseURL:       b.ServiceBaseURL,
 		InfraEnvID:           params.InfraEnvID.String(),
 		HostID:               params.NewHostParams.HostID.String(),
@@ -3692,7 +3692,7 @@ func (b *bareMetalInventory) generateV2NextStepRunnerCommand(ctx context.Context
 		NextStepRunnerImage:  b.AgentDockerImg,
 		SkipCertVerification: b.SkipCertVerification,
 	}
-	command, args := hostcommands.V2GetNextStepRunnerCommand(&config)
+	command, args := hostcommands.GetNextStepRunnerCommand(&config)
 	return &models.HostRegistrationResponseAO1NextStepRunnerCommand{
 		Command: command,
 		Args:    *args,
