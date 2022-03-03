f8bdfb46b907e64aa6a18f79fbee6dc23ef63d46
Igal Tsoiref <itsoiref@redhat.com>
2022-03-03 14:15:52 +0200

    WIP: new agent protocol

--- internal/host/hostcommands/reset_installation_cmd.go
+++ internal/host/hostcommands/reset_installation_cmd.go
@@ -1,10 +1,7 @@
 package hostcommands
 
 import (
-	"bytes"
 	"context"
-	"html/template"
-
 	"github.com/openshift/assisted-service/models"
 	"github.com/sirupsen/logrus"
 )
@@ -20,22 +17,9 @@ func NewResetInstallationCmd(log logrus.FieldLogger) *resetInstallationCmd {
 }
 
 func (h *resetInstallationCmd) GetSteps(ctx context.Context, host *models.Host) ([]*models.Step, error) {
-	var cmdStr string
-	if host.Bootstrap {
-		cmdStr += "systemctl stop bootkube.service; rm -rf /etc/kubernetes/manifests/* /etc/kubernetes/static-pod-resources/* /opt/openshift/*.done; "
-	}
-	cmdStr += "/usr/bin/podman rm --all -f; systemctl restart agent; "
-	t, err := template.New("cmd").Parse(cmdStr)
-	if err != nil {
-		return nil, err
-	}
-	buf := &bytes.Buffer{}
-	if err := t.Execute(buf, nil); err != nil {
-		return nil, err
-	}
 	step := &models.Step{}
 	step.StepType = models.StepTypeResetInstallation
-	step.Command = "bash"
-	step.Args = []string{"-c", buf.String()}
+	step.Command = ""
+	step.Args = []string{}
 	return []*models.Step{step}, nil
 }
