3a09b1c79e7eef200926ae52b8f1f929bbc0048b
Ori Amizur <oamizur@redhat.com>
2022-03-28 13:03:00 +0300

    MGMT-9776: [Assisted-4.10] [Staging] multi cluster - installation failing New API was added to get cluster hosts by using filters. The assisted installer should use this API, but agentAuth was missing from API definition. (X-Secret-Key header). This fixes the missing definition.

--- restapi/embedded_spec.go
+++ restapi/embedded_spec.go
@@ -1354,6 +1354,9 @@ func init() {
               "read-only-admin",
               "user"
             ]
+          },
+          {
+            "agentAuth": []
           }
         ],
         "description": "Get a list of cluster hosts according to supplied filters.",
@@ -10178,6 +10181,9 @@ func init() {
               "read-only-admin",
               "user"
             ]
+          },
+          {
+            "agentAuth": []
           }
         ],
         "description": "Get a list of cluster hosts according to supplied filters.",
