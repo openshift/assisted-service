e5d0c81a83fb9a76634be884075c07a4c5a06d2d
Mat Kowalski <mko@redhat.com>
2022-03-29 09:55:54 +0200

    MGMT-9710: Provide extended networking defaults in default-config API
    This PR extends the default-config API to contain more defaults
    regarding network configuration. This is driven by the fact that
    Assisted UI allows now to create single- as well as dual-stack clusters
    with multiple combinations of IPv4 and IPv6, therefore it's useful to
    have an endpoint that will provide suggested values.
    
    Closes: MGMT-9710

--- subsystem/cluster_default_config_test.go
+++ subsystem/cluster_default_config_test.go
@@ -6,6 +6,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/openshift/assisted-service/client/installer"
+	"github.com/openshift/assisted-service/models"
 )
 
 var _ = Describe("V2GetClusterDefaultConfig", func() {
@@ -14,5 +15,25 @@ var _ = Describe("V2GetClusterDefaultConfig", func() {
 		res, err := userBMClient.Installer.V2GetClusterDefaultConfig(context.Background(), &installer.V2GetClusterDefaultConfigParams{})
 		Expect(err).NotTo(HaveOccurred())
 		Expect(res.GetPayload().InactiveDeletionHours).To(Equal(int64(Options.DeregisterInactiveAfter.Hours())))
+	})
+	It("Default IPv4 networks", func() {
+		res, err := userBMClient.Installer.V2GetClusterDefaultConfig(context.Background(), &installer.V2GetClusterDefaultConfigParams{})
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(res.GetPayload().ClusterNetworksIPV4[0].Cidr).To(Equal(models.Subnet("10.128.0.0/14")))
+		Expect(res.GetPayload().ClusterNetworksIPV4[0].HostPrefix).To(Equal(int64(23)))
+		Expect(res.GetPayload().ServiceNetworksIPV4[0].Cidr).To(Equal(models.Subnet("172.30.0.0/16")))
+	})
+	It("Default dual-stack networks", func() {
+		res, err := userBMClient.Installer.V2GetClusterDefaultConfig(context.Background(), &installer.V2GetClusterDefaultConfigParams{})
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(res.GetPayload().ClusterNetworksDualstack[0].Cidr).To(Equal(models.Subnet("10.128.0.0/14")))
+		Expect(res.GetPayload().ClusterNetworksDualstack[0].HostPrefix).To(Equal(int64(23)))
+		Expect(res.GetPayload().ServiceNetworksDualstack[0].Cidr).To(Equal(models.Subnet("172.30.0.0/16")))
+
+		Expect(res.GetPayload().ClusterNetworksDualstack[1].Cidr).To(Equal(models.Subnet("fd01::/48")))
+		Expect(res.GetPayload().ClusterNetworksDualstack[1].HostPrefix).To(Equal(int64(64)))
+		Expect(res.GetPayload().ServiceNetworksDualstack[1].Cidr).To(Equal(models.Subnet("fd02::/112")))
 	})
 })
