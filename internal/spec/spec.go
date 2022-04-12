a5203e397ec822e1f863e106027438f39372cabc
Omer Tuchfeld <omer@tuchfeld.dev>
2022-04-12 06:38:08 +0200

    MGMT-9799: Serve API spec on `/v2/openapi` and not just `/openapi` (#3644)
    We have just one API spec file (swagger.json) containing the definition
    for all of our currently supported versions (today it's just v2, but for
    a while we had both v1 and v2 and in the future we may have both v2 and
    v3).
    
    Today we serve our API spec on `<base_path>/openapi` - the API spec
    contains the definitions for all currently supported versions of our API
    (we're basically serving our swagger file itself on this endpoint).
    
    We should also serve the exact same API spec (containing the definitions
    for all versions) on <base_path>/<version>/openapi because API UIs such
    as the one at https://api.openshift.com/ always try to access this
    endpoint but with some version in the path e.g:
    
    `https://api.openshift.com/api/assisted-install/v2/openapi`
    
    instead of
    
    `https://api.openshift.com/api/assisted-install/openapi`
    
    This commit makes it so that we'll also serve the exact same file in:
    
    `https://api.openshift.com/api/assisted-install/v2/openapi`
    
    Which will make [this](https://api.openshift.com/?urls.primaryName=assisted-service%20service) webpage work

--- internal/spec/spec.go
+++ internal/spec/spec.go
@@ -6,14 +6,32 @@ import (
 
 	"github.com/openshift/assisted-service/client"
 	"github.com/openshift/assisted-service/restapi"
+	"github.com/thoas/go-funk"
 )
 
-var openapiPath = path.Join(client.DefaultBasePath, "openapi")
+var openapiPaths []string
+
+func init() {
+	openapiPaths = append(openapiPaths,
+		// We serve our API spec on <base_path>/openapi - the API spec contains the
+		// definitions for all currently supported versions of our API
+		path.Join(client.DefaultBasePath, "openapi"),
+
+		// We also serve the exact same API spec (containing the definitions for all
+		// versions) on <base_path>/<version>/openapi because API UIs such as the one at
+		// https://api.openshift.com/ always try to access this endpoint but with some
+		// version in the path
+		path.Join(client.DefaultBasePath, "v2", "openapi"),
+
+		// Add future versions here, e.g.:
+		// path.Join(client.DefaultBasePath, "v3", "openapi"),
+	)
+}
 
 // WithSpecMiddleware returns middleware which responds to the openapi endpoint
 func WithSpecMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodGet && r.URL.Path == openapiPath {
+		if r.Method == http.MethodGet && funk.ContainsString(openapiPaths, r.URL.Path) {
 			_, _ = w.Write(restapi.SwaggerJSON)
 			return
 		}
