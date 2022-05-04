36e13cf2ef0315479a661fd98186fc46c47748ff
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2022-05-04 00:44:03 -0700

    NO-ISSUE - Add the ability to override image-service image when deploying the assisted-image-service (#3758)
    Co-authored-by: Elior Erez <eerez@redhat.com>

--- tools/deploy_image_service.py
+++ tools/deploy_image_service.py
@@ -1,8 +1,10 @@
-import os
-import utils
 import argparse
+import os
+
 import yaml
+
 import deployment_options
+import utils
 
 parser = argparse.ArgumentParser()
 deploy_options = deployment_options.load_deployment_options(parser)
@@ -11,13 +13,17 @@ log = utils.get_logger('deploy-image-service')
 SRC_FILE = os.path.join(os.getcwd(), 'deploy/assisted-image-service.yaml')
 DST_FILE = os.path.join(os.getcwd(), 'build', deploy_options.namespace, 'assisted-image-service.yaml')
 
+
 def main():
     utils.verify_build_directory(deploy_options.namespace)
 
     with open(SRC_FILE, "r") as src:
+        log.info(f"Loading source template file for assisted-image-service: {SRC_FILE}")
         raw_data = src.read()
         raw_data = raw_data.replace('REPLACE_NAMESPACE', f'"{deploy_options.namespace}"')
+        raw_data = raw_data.replace('REPLACE_IMAGE_SERVICE_IMAGE', os.environ.get("IMAGE_SERVICE"))
         data = yaml.safe_load(raw_data)
+        log.info(data)
 
     with open(DST_FILE, "w+") as dst:
         yaml.dump(data, dst, default_flow_style=False)
@@ -31,6 +37,7 @@ def main():
         namespace=deploy_options.namespace,
         file=DST_FILE
     )
+
 
 if __name__ == "__main__":
     main()
