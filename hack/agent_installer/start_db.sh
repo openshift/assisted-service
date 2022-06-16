71976433981cbb7459526c22432e9e5d368da794
Pawan Pinjarkar <pawan.pinjarkar@gmail.com>
2022-06-16 19:08:59 -0400

    Use `exec postgres` to keep the database container up and running. Similar to the upstream docker image (Ref below), first the db server is started with pg_ctl, created user and database, then stop the db server with pg_ctl and start the db server with `exec postgres`.
    Ref:
    https://github.com/docker-library/postgres/blob/780680ebfa85d8220627985c0a16ecfd79d44a0f/10/bullseye/docker-entrypoint.sh#L323-L328
    https://github.com/sclorg/postgresql-container/blob/3525b011dd8e44e081cdfb7b55aa12719eeae5b7/src/root/usr/bin/run-postgresql#L33-L58

--- hack/agent_installer/start_db.sh
+++ hack/agent_installer/start_db.sh
@@ -9,5 +9,5 @@ set -o errexit
 createuser -s admin -h localhost
 createdb installer -h localhost
 
-# To keep the db container up and running
-sleep infinity
+/usr/bin/pg_ctl -D /tmp/postgres/data/ -l /tmp/postgres/logfile stop
+exec postgres -D /tmp/postgres/data/
