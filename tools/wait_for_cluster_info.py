d88972b7729025317dfa308bae5d79fbb291b8ab
Omer Tuchfeld <omer@tuchfeld.dev>
2022-06-17 11:11:32 +0200

    [release-ocm-2.4] BUG 2097820: CI - Retry cluster-info readiness  (#3967)
    * BUG-2097700: CI - Retry cluster-info readiness
    
    Several[1] jobs are failing for this, and it looks like
    the cluster was just not ready on time. This is an attempt
    to retry cluster-info several times before we fail the job.
    
    [1] one example: periodic-ci-openshift-assisted-service-release-ocm-2.5-subsystem-kubeapi-aws-periodic
    
    * BUG-2097700: Call cluster-info with a namespace
    
    In some cases, the user does not own the cluster and thus cannot
    run cluster-info without the scope of a namespace.
    
    Co-authored-by: Nir Magnezi <nmagnezi@redhat.com>

---
+++ tools/wait_for_cluster_info.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import datetime
+
+import utils
+import deployment_options
+from retry import retry
+
+TRIES = 10
+DELAY = 5
+
+
+@retry(exceptions=RuntimeError, tries=TRIES, delay=DELAY)
+def is_cluster_info_ready(kubectl_cmd):
+    cmd = f"{kubectl_cmd} cluster-info"
+    print(f"{datetime.datetime.now()} DEBUG - {cmd}")
+    try:
+        res = utils.check_output(cmd)
+    except RuntimeError:
+        print(f"{datetime.datetime.now()} DEBUG - cluster is not ready yet.")
+        raise
+    print(res)
+    return
+
+
+def main():
+    deploy_options = deployment_options.load_deployment_options()
+    is_cluster_info_ready(kubectl_cmd=utils.get_kubectl_command(namespace=deploy_options.namespace))
+
+
+if __name__ == '__main__':
+    main()
