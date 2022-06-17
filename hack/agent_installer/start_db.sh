ade2d2af9f55855785bb37bcb3d151058750b9c8
Pawan Pinjarkar <pawan.pinjarkar@gmail.com>
2022-06-17 11:51:35 -0400

    Pass wait flag explicitly just to be safe

--- hack/agent_installer/start_db.sh
+++ hack/agent_installer/start_db.sh
@@ -4,10 +4,9 @@ set -o nounset
 set -euo pipefail
 set -o errexit
 
-/usr/bin/pg_ctl -D /tmp/postgres/data/ -l /tmp/postgres/logfile start 
-
+/usr/bin/pg_ctl -D /tmp/postgres/data/ -l /tmp/postgres/logfile start -w
 createuser -s admin -h localhost
 createdb installer -h localhost
 
-/usr/bin/pg_ctl -D /tmp/postgres/data/ -l /tmp/postgres/logfile stop
+/usr/bin/pg_ctl -D /tmp/postgres/data/ -l /tmp/postgres/logfile stop -w
 exec postgres -D /tmp/postgres/data/
