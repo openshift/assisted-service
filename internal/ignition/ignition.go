1c2bf0ad62200fce1b1d862ab2ecba672ca8e814
Igal Tsoiref <itsoiref@redhat.com>
2022-07-03 22:41:44 +0300

    MGMT-11115: Add setting bootstrap kubelet ip os env for installer generate Currently installer has an env variable for bootstrap kubelet ip, it will be change once https://github.com/openshift/enhancements/pull/1181 will be done but till then we can set it in assisted-service. This is second part of a fix for https://bugzilla.redhat.com/show_bug.cgi?id=2070318 This code should be changed after https://github.com/openshift/enhancements/pull/1181 will be accepted.

--- internal/ignition/ignition.go
+++ internal/ignition/ignition.go
@@ -313,12 +313,24 @@ func (g *installerGenerator) Generate(ctx context.Context, installConfig []byte,
 		log.WithError(wrapped).Errorf("GenerateInstallConfig")
 		return wrapped
 	}
+
 	envVars := append(os.Environ(),
 		"OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE="+g.releaseImage,
 		"OPENSHIFT_INSTALL_INVOKER="+g.installInvoker,
 	)
 	if g.clusterTLSCertOverrideDir != "" {
 		envVars = append(envVars, "OPENSHIFT_INSTALL_LOAD_CLUSTER_CERTS=true")
+	}
+
+	// setting bootstrap kubelet node ip
+	if !common.IsSingleNodeCluster(g.cluster) {
+		log.Debugf("Adding bootstrap ip to env vars")
+		bootstrapIp, errB := network.GetPrimaryMachineCIDRIP(common.GetBootstrapHost(g.cluster), g.cluster)
+		if errB != nil {
+			log.WithError(errB).Warn("Failed to get bootstrap primary ip for kubelet service update.")
+			return errB
+		}
+		envVars = append(envVars, "OPENSHIFT_INSTALL_BOOTSTRAP_NODE_IP="+bootstrapIp)
 	}
 
 	// write installConfig to install-config.yaml so openshift-install can read it
