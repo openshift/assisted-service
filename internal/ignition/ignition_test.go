54e72f31e8cb2ab347e09bea28df90a8e2027843
Bob Fournier <bfournie@redhat.com>
2022-07-14 13:00:46 -0400

    fixes for logging

--- internal/ignition/ignition_test.go
+++ internal/ignition/ignition_test.go
@@ -1499,8 +1499,6 @@ var _ = Describe("ICSP file for oc extract", func() {
 	It("valid file created and readable", func() {
 		var cfg installcfg.InstallerConfigBaremetal
 		expected := "apiVersion: operator.openshift.io/v1alpha1\nkind: ImageContentSourcePolicy\nmetadata:\n  creationTimestamp: null\n  name: image-policy\nspec:\n  repositoryDigestMirrors:\n  - mirrors:\n    - mirrorhost1.example.org:5000/localimages\n    - mirrorhost2.example.org:5000/localimages\n    source: registry.ci.org\n"
-		g := NewGenerator(workDir, installerCacheDir, cluster, "", "", "", "", nil, log,
-			mockOperatorManager, mockProviderRegistry, "", "").(*installerGenerator)
 
 		imageContentSourceList := make([]installcfg.ImageContentSource, 1)
 		imageContentSourceList[0] = installcfg.ImageContentSource{
@@ -1510,7 +1508,7 @@ var _ = Describe("ICSP file for oc extract", func() {
 		cfg.ImageContentSources = imageContentSourceList
 		data, err := yaml.Marshal(&cfg)
 		Expect(err).ShouldNot(HaveOccurred())
-		icspFile, err := g.getIcspFileFromInstallConfig(data)
+		icspFile, err := getIcspFileFromInstallConfig(data, log)
 		Expect(err).ShouldNot(HaveOccurred())
 		Expect(string(icspFile)).Should(BeARegularFile())
 
@@ -1522,12 +1520,10 @@ var _ = Describe("ICSP file for oc extract", func() {
 	It("filename is empty string", func() {
 		var cfg installcfg.InstallerConfigBaremetal
 		expected := ""
-		g := NewGenerator(workDir, installerCacheDir, cluster, "", "", "", "", nil, log,
-			mockOperatorManager, mockProviderRegistry, "", "").(*installerGenerator)
 
 		data, err := yaml.Marshal(&cfg)
 		Expect(err).ShouldNot(HaveOccurred())
-		icspFile, err := g.getIcspFileFromInstallConfig(data)
+		icspFile, err := getIcspFileFromInstallConfig(data, log)
 		Expect(err).ShouldNot(HaveOccurred())
 		Expect(string(icspFile)).Should(Equal(expected))
 	})
