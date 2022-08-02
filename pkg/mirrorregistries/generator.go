f92b4c8c16df3d5f72425a67297b7c71790bd96f
Mateusz Kowalski <mko@redhat.com>
2022-08-03 01:12:55 +0200

    MGMT-8890: Handle registries.conf without any mirrors correctly (#4230)
    Currently the code assumes that existence of containers.conf file means
    there are always registry mirrors configured. It has been observed that
    the default file has no `[[registry]]` section but is still a correct
    file. For such a scenario we want to proceed as the file did not exist
    instead of failing.
    
    Contributes-to: MGMT-8890

--- pkg/mirrorregistries/generator.go
+++ pkg/mirrorregistries/generator.go
@@ -2,7 +2,7 @@ package mirrorregistries
 
 import (
 	"fmt"
-	"io/ioutil"
+	"os"
 
 	"github.com/openshift/assisted-service/internal/common"
 	"github.com/pelletier/go-toml"
@@ -17,10 +17,15 @@ type MirrorRegistriesConfigBuilder interface {
 }
 
 type mirrorRegistriesConfigBuilder struct {
+	MirrorRegistriesConfigPath      string
+	MirrorRegistriesCertificatePath string
 }
 
 func New() MirrorRegistriesConfigBuilder {
-	return &mirrorRegistriesConfigBuilder{}
+	return &mirrorRegistriesConfigBuilder{
+		MirrorRegistriesConfigPath:      common.MirrorRegistriesConfigPath,
+		MirrorRegistriesCertificatePath: common.MirrorRegistriesCertificatePath,
+	}
 }
 
 type RegistriesConf struct {
@@ -28,26 +33,40 @@ type RegistriesConf struct {
 	Mirror   string
 }
 
+// We consider mirror registries to be configured if the following conditions are all met
+//   * CA bundle file (e.g. /etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem) exists
+//   * registry configuration file (e.g. /etc/containers/registries.conf) exists
+//   * registry configuration contains "[[registry]]" section
 func (m *mirrorRegistriesConfigBuilder) IsMirrorRegistriesConfigured() bool {
 	_, err := m.GetMirrorCA()
 	if err != nil {
 		return false
 	}
-	_, err = m.GetMirrorRegistries()
-	return err == nil
+	contents, err := m.GetMirrorRegistries()
+	if err != nil {
+		return false
+	}
+
+	tomlTree, err := toml.Load(string(contents))
+	if err != nil {
+		return false
+	}
+
+	_, ok := tomlTree.Get("registry").([]*toml.Tree)
+	return ok
 }
 
 // return error if the path is actually an empty dir, which will indicate that
 // the mirror registries are not configured.
 // empty dir is due to the way we mao configmap in the assisted-service pod
 func (m *mirrorRegistriesConfigBuilder) GetMirrorCA() ([]byte, error) {
-	return readFile(common.MirrorRegistriesCertificatePath)
+	return os.ReadFile(m.MirrorRegistriesCertificatePath)
 }
 
 // returns error if the file is not present, which will also indicate that
 // mirror registries are not confgiured
 func (m *mirrorRegistriesConfigBuilder) GetMirrorRegistries() ([]byte, error) {
-	return readFile(common.MirrorRegistriesConfigPath)
+	return os.ReadFile(m.MirrorRegistriesConfigPath)
 }
 
 func (m *mirrorRegistriesConfigBuilder) ExtractLocationMirrorDataFromRegistries() ([]RegistriesConf, error) {
@@ -66,28 +85,24 @@ func extractLocationMirrorDataFromRegistries(registriesConfToml string) ([]Regis
 
 	registriesTree, ok := tomlTree.Get("registry").([]*toml.Tree)
 	if !ok {
-		return nil, fmt.Errorf("Failed to cast registry key to toml Tree")
+		return nil, fmt.Errorf("failed to cast registry key to toml Tree, registriesConfToml: %s", registriesConfToml)
 	}
 	registriesConfList := make([]RegistriesConf, len(registriesTree))
 	for i, registryTree := range registriesTree {
 		location, ok := registryTree.Get("location").(string)
 		if !ok {
-			return nil, fmt.Errorf("Failed to cast location key to string")
+			return nil, fmt.Errorf("failed to cast location key to string, registriesConfToml: %s", registriesConfToml)
 		}
 		mirrorTree, ok := registryTree.Get("mirror").([]*toml.Tree)
 		if !ok {
-			return nil, fmt.Errorf("Failed to cast mirror key to toml Tree")
+			return nil, fmt.Errorf("failed to cast mirror key to toml Tree, registriesConfToml: %s", registriesConfToml)
 		}
 		mirror, ok := mirrorTree[0].Get("location").(string)
 		if !ok {
-			return nil, fmt.Errorf("Failed to cast mirror location key to string")
+			return nil, fmt.Errorf("failed to cast mirror location key to string, registriesConfToml: %s", registriesConfToml)
 		}
 		registriesConfList[i] = RegistriesConf{Location: location, Mirror: mirror}
 	}
 
 	return registriesConfList, nil
-}
-
-func readFile(filePath string) ([]byte, error) {
-	return ioutil.ReadFile(filePath)
 }
