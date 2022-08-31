dc3ea753343378f3aa19c7131206cb01010c602c
Mat Kowalski <mko@redhat.com>
2022-08-31 16:54:20 +0200

    NO-ISSUE: Specify Hive channel explicitly for RHACM 2.4

--- deploy/operator/setup_hive.sh
+++ deploy/operator/setup_hive.sh
@@ -50,6 +50,7 @@ spec:
   name: hive-operator
   source: community-operators
   sourceNamespace: openshift-marketplace
+  channel: ocm-2.4
 EOCR
 
     wait_for_operator "hive-operator" "${HIVE_NAMESPACE}"
