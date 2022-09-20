c12792d18c5358aa584889016d383753eb0189ef
Elior Erez <eerez@redhat.com>
2022-09-20 13:43:59 +0300

    MGMT-12106: Completely remove ovirt provider using `make generate`

--- vendor/github.com/ovirt/go-ovirt-client/const.go
+++
@@ -1,5 +0,0 @@
-package ovirtclient
-
-// DefaultBlankTemplateID returns the ID for the factory-default blank template. This should not be used
-// as the template may be deleted from the oVirt engine. Instead, use the API call to find the blank template.
-const DefaultBlankTemplateID TemplateID = "00000000-0000-0000-0000-000000000000"
