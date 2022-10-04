b579feaf93b78e7c0c8ebdb83486a6eb932c8874
Riccardo piccoli <rpiccoli@redhat.com>
2022-10-04 12:51:35 +0200

    NO-ISSUE: reduce build image size and leverage buildkit cache

--- hack/setup_env.sh
+++ hack/setup_env.sh
@@ -5,6 +5,10 @@ set -o pipefail
 set -o errexit
 set -o xtrace
 
+CACHE_DIR=/tmp/.cache
+
+mkdir -p ${CACHE_DIR}
+
 function get_package_manager() {
     PACKAGE_MANAGER=$( command -v dnf &> /dev/null && echo "dnf" || echo "yum")
     echo $PACKAGE_MANAGER
@@ -15,9 +19,23 @@ function print_help() {
   echo "Usage: bash ${0} (${ALL_FUNCS})"
 }
 
+function download() {
+    # This function will leverage buildkit cache, and avoid downloading the file again
+    # if we already have a file with the same name in the cache directory
+    # Buildkit mounts /tmp/.cache as cache dir
+    url=$1
+    output=$2
+    cached_filename=$(echo -n "$url" | sha256sum | awk '{ print $1 }')
+    cache_full_path="${CACHE_DIR}/${cached_filename}"
+    [ -f "${cache_full_path}" ] || curl --retry 5 -L "$url" -o "$cache_full_path"
+    cp "$cache_full_path" "${output}"
+}
+
 function golang() {
   echo "Installing golang..."
-  curl --retry 5 -L https://storage.googleapis.com/golang/getgo/installer_linux -o /tmp/golang_installer
+
+  download https://storage.googleapis.com/golang/getgo/installer_linux /tmp/golang_installer
+
   chmod u+x /tmp/golang_installer
   /tmp/golang_installer
   rm /tmp/golang_installer
@@ -30,30 +48,31 @@ function golang() {
 
 function spectral() {
   echo "Installing spectral..."
-  curl --retry 5 -L https://github.com/stoplightio/spectral/releases/download/v5.9.1/spectral-linux -o /usr/local/bin/spectral
+  download https://github.com/stoplightio/spectral/releases/download/v5.9.1/spectral-linux /usr/local/bin/spectral
   chmod +x /usr/local/bin/spectral
 }
 
 function jq() {
   echo "Installing jq..."
-  curl --retry 5 -L https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 --output /usr/local/bin/jq
+  download https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 /usr/local/bin/jq
   chmod +x /usr/local/bin/jq
 }
 
 function awscli() {
   echo "Installing aws-cli..."
-  curl --retry 5 -L "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip" && unzip /tmp/awscliv2.zip && \
+  download "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" "/tmp/awscliv2.zip" && unzip /tmp/awscliv2.zip && \
   ./aws/install && rm -f /tmp/awscliv2.zip
+  rm -rf ./aws
 }
 
 function podman_remote() {
   # podman-remote 4 cannot run against podman server 3 so installing them both side by side
-  curl --retry 5 -L https://github.com/containers/podman/releases/download/v3.4.4/podman-remote-static.tar.gz -o "podman-remote3.tar.gz" && \
+  download https://github.com/containers/podman/releases/download/v3.4.4/podman-remote-static.tar.gz "podman-remote3.tar.gz" && \
   tar -zxvf podman-remote3.tar.gz && \
   mv podman-remote-static /usr/local/bin/podman-remote3 && \
   rm -f podman-remote3.tar.gz
 
-  curl --retry 5 -L https://github.com/containers/podman/releases/download/v4.1.1/podman-remote-static.tar.gz -o "podman-remote4.tar.gz" && \
+  download https://github.com/containers/podman/releases/download/v4.1.1/podman-remote-static.tar.gz "podman-remote4.tar.gz" && \
   tar -zxvf podman-remote4.tar.gz && \
   mv podman-remote-static /usr/local/bin/podman-remote4 && \
   rm -f podman-remote4.tar.gz
@@ -72,7 +91,7 @@ function assisted_service() {
   ARCH=$(case $(arch) in x86_64) echo -n amd64 ;; aarch64) echo -n arm64 ;; *) echo -n $(arch) ;; esac)
 
   latest_kubectl_version=$(curl --retry 5 -L -s https://dl.k8s.io/release/stable.txt)
-  curl --retry 5 -L "https://dl.k8s.io/release/${latest_kubectl_version}/bin/linux/amd64/kubectl" -o /tmp/kubectl && \
+  download "https://dl.k8s.io/release/${latest_kubectl_version}/bin/linux/amd64/kubectl" /tmp/kubectl && \
     install -o root -g root -m 0755 /tmp/kubectl /usr/local/bin/kubectl && \
     rm -f /tmp/kubectl
 
@@ -89,9 +108,8 @@ function assisted_service() {
 
   OS=$(uname | awk '{print tolower($0)}')
   OPERATOR_SDK_DL_URL=https://github.com/operator-framework/operator-sdk/releases/download/v1.10.1
-  curl --retry 5 -LO ${OPERATOR_SDK_DL_URL}/operator-sdk_${OS}_${ARCH}
-  chmod +x operator-sdk_${OS}_${ARCH}
-  install operator-sdk_${OS}_${ARCH} /usr/local/bin/operator-sdk
+  download ${OPERATOR_SDK_DL_URL}/operator-sdk_${OS}_${ARCH} /usr/local/bin/operator-sdk
+  chmod +x /usr/local/bin/operator-sdk
 
   go get golang.org/x/tools/cmd/goimports@v0.1.5 \
         sigs.k8s.io/controller-tools/cmd/controller-gen@v0.6.2
