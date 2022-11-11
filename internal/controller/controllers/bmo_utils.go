ded240ac46d76e124ee56d4d97638a9b6c7e3f62
Nick Carboni <ncarboni@redhat.com>
2022-11-11 15:44:16 -0500

    Update BMO and use GetIronicIP from there
    https://issues.redhat.com/browse/MGMT-12001

--- internal/controller/controllers/bmo_utils.go
+++ internal/controller/controllers/bmo_utils.go
@@ -7,12 +7,13 @@ import (
 	v1 "github.com/openshift/api/config/v1"
 	"github.com/openshift/assisted-service/internal/common"
 	"github.com/openshift/assisted-service/internal/network"
+	osclientset "github.com/openshift/client-go/config/clientset/versioned"
 	metal3iov1alpha1 "github.com/openshift/cluster-baremetal-operator/api/v1alpha1"
+	"github.com/openshift/cluster-baremetal-operator/provisioning"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -21,12 +22,14 @@ const MinimalVersionForConvergedFlow = "4.12.0-0.alpha"
 type BMOUtils struct {
 	// The methods of this receiver get called once before the cache is initialized hence we check the API directly
 	c              client.Reader
+	osClient       *osclientset.Clientset
+	kubeClient     *kubernetes.Clientset
 	log            logrus.FieldLogger
 	kubeAPIEnabled bool
 }
 
-func NewBMOUtils(client client.Reader, log logrus.FieldLogger, kubeAPIEnabled bool) *BMOUtils {
-	return &BMOUtils{client, log, kubeAPIEnabled}
+func NewBMOUtils(client client.Reader, osClient *osclientset.Clientset, kubeClient *kubernetes.Clientset, log logrus.FieldLogger, kubeAPIEnabled bool) *BMOUtils {
+	return &BMOUtils{client, osClient, kubeClient, log, kubeAPIEnabled}
 }
 
 // +kubebuilder:rbac:groups=config.openshift.io,resources=clusteroperators,verbs=get;list;watch
@@ -57,14 +60,13 @@ func (r *BMOUtils) ConvergedFlowAvailable() bool {
 	return available
 }
 
-// TODO: replace this with public function in BMO https://github.com/openshift/cluster-baremetal-operator/pull/261
 func (r *BMOUtils) GetIronicServiceURL() (string, error) {
 	provisioningInfo, err := r.readProvisioningCR()
 	if err != nil {
 		r.log.WithError(err).Error("unable to get provisioning CR")
 		return "", err
 	}
-	ironicIP, err := r.getIronicIP(*provisioningInfo)
+	ironicIP, _, err := provisioning.GetIronicIP(r.kubeClient, provisioningInfo.Namespace, &provisioningInfo.Spec, r.osClient)
 	if err != nil || ironicIP == "" {
 		r.log.WithError(err).Error("unable to determine Ironic's IP")
 		return "", err
@@ -84,15 +86,6 @@ func (r *BMOUtils) readProvisioningCR() (*metal3iov1alpha1.Provisioning, error) 
 	return instance, nil
 }
 
-func (r *BMOUtils) getIronicIP(info metal3iov1alpha1.Provisioning) (string, error) {
-	// this is how BMO get the IronicIP
-	config := info.Spec
-	if config.ProvisioningNetwork != metal3iov1alpha1.ProvisioningNetworkDisabled && !config.VirtualMediaViaExternalNetwork {
-		return config.ProvisioningIP, nil
-	}
-	return r.getPodHostIP(info.Namespace)
-}
-
 func getUrlFromIP(ipAddr string) string {
 	if network.IsIPv6Addr(ipAddr) {
 		return "https://" + fmt.Sprintf("[%s]", ipAddr)
@@ -102,41 +95,4 @@ func getUrlFromIP(ipAddr string) string {
 	} else {
 		return ""
 	}
-}
-
-func (r *BMOUtils) getPodHostIP(targetNamespace string) (string, error) {
-	metal3AppName := "metal3"
-	stateService := "metal3-state"
-	cboLabelName := "baremetal.openshift.io/cluster-baremetal-operator"
-
-	labelSelector := &metav1.LabelSelector{
-		MatchLabels: map[string]string{
-			"k8s-app":    metal3AppName,
-			cboLabelName: stateService,
-		}}
-
-	selector, err := metav1.LabelSelectorAsSelector(labelSelector)
-	if err != nil {
-		return "", err
-	}
-
-	listOptions := client.ListOptions{LabelSelector: selector,
-		Namespace: targetNamespace,
-	}
-	podList := &corev1.PodList{}
-	err = r.c.List(context.Background(), podList, &listOptions)
-	if err != nil {
-		return "", err
-	}
-	var hostIP string
-	switch len(podList.Items) {
-	case 0:
-		err = fmt.Errorf("failed to find a pod with the given label")
-	case 1:
-		hostIP = podList.Items[0].Status.HostIP
-	default:
-		// We expect only one pod with the above LabelSelector
-		err = fmt.Errorf("there should be only one pod listed for the given label")
-	}
-	return hostIP, err
 }
