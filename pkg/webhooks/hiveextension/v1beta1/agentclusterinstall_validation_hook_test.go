21c9c382cfac04084d2e15cfd5a96b3afb00d1e7
slavie <slavie@redhat.com>
2022-12-25 18:30:36 +0200

    MGMT-12794: allow to edit ACI post install
    To support gitops ZTP ArgoCD flow we should allow editing the ACI CR
    after the installation has finished.
    
    From the perspective of assisted service, this has no meaning,
    but for the gitops flow on day-2, it is important because they are going to use the edited CR as a template for future actions.

--- pkg/webhooks/hiveextension/v1beta1/agentclusterinstall_validation_hook_test.go
+++ pkg/webhooks/hiveextension/v1beta1/agentclusterinstall_validation_hook_test.go
@@ -146,7 +146,7 @@ var _ = Describe("ACI web validate", func() {
 			expectedAllowed: false,
 		},
 		{
-			name: "Test AgentClusterInstall.Spec is immutable (updates not allowed) Install finished",
+			name: "Test AgentClusterInstall.Spec updates allowed on Install finished",
 			newSpec: hiveext.AgentClusterInstallSpec{
 				SSHPublicKey: "somekey",
 			},
@@ -160,10 +160,10 @@ var _ = Describe("ACI web validate", func() {
 				SSHPublicKey: "someotherkey",
 			},
 			operation:       admissionv1.Update,
-			expectedAllowed: false,
+			expectedAllowed: true,
 		},
 		{
-			name: "Test AgentClusterInstall.Spec is immutable (updates not allowed) Install failed",
+			name: "Test AgentClusterInstall.Spec updates allowed after Install failed",
 			newSpec: hiveext.AgentClusterInstallSpec{
 				SSHPublicKey: "somekey",
 			},
@@ -177,7 +177,7 @@ var _ = Describe("ACI web validate", func() {
 				SSHPublicKey: "someotherkey",
 			},
 			operation:       admissionv1.Update,
-			expectedAllowed: false,
+			expectedAllowed: true,
 		},
 		{
 			name: "Test AgentClusterInstall.Spec.ClusterMetadata is mutable (updates are allowed) Install started",
