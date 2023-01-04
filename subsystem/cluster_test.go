2d215c995f4fe5de55dd4730cb74375587b4c0ad
Juan Hernández <juan.hernandez@redhat.com>
2023-01-04 13:57:09 +0100

    MGMT-13132: Respond with 409 when rejecting registration (#4858)
    Currently when an agent tries to register while the cluster is
    installing the service will return a 400 response code. As a result the
    agent will retry the request after one minute. To avoid that this patch
    changes the response code to 409, which will cause the agent to freeze.
    
    Related: https://issues.redhat.com/browse/MGMT-13132
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>
    
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- subsystem/cluster_test.go
+++ subsystem/cluster_test.go
@@ -1772,7 +1772,7 @@ var _ = Describe("cluster install", func() {
 					HostID: strToUUID(uuid.New().String()),
 				},
 			})
-			Expect(err).To(BeAssignableToTypeOf(installer.NewV2RegisterHostBadRequest()))
+			Expect(err).To(BeAssignableToTypeOf(installer.NewV2RegisterHostConflict()))
 		})
 
 		It("register host while cluster in error state", func() {
@@ -1786,7 +1786,7 @@ var _ = Describe("cluster install", func() {
 					HostID: strToUUID(uuid.New().String()),
 				},
 			})
-			Expect(err).To(BeAssignableToTypeOf(installer.NewV2RegisterHostBadRequest()))
+			Expect(err).To(BeAssignableToTypeOf(installer.NewV2RegisterHostConflict()))
 		})
 
 		It("fail installation if there is only a single worker that manages to install", func() {
