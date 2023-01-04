7eecf688636e7330083a259948773c8b7e889cdc
Paul Maidment <pmaidmen@redhat.com>
2023-01-04 12:54:34 +0200

    MGMT-12273: Updated logging in PreNetworkConfig script.
    In situations where users are struggling to determine why their network configuration is not taking effect it would be helpful to log a bit more verbosely from the service that sets up the network config files. Specifically if a host does not match any of the provided mappings we currently write "None of host directories are a match for the current host" which is not particularly helpful. It should at least say something about possible mac address mismatches. We probably could also find a few more places to log something useful.
    
    This PR attempts to address these concerns by adding more verbose logging.

--- internal/constants/scripts.go
+++ internal/constants/scripts.go
@@ -32,8 +32,9 @@ const PreNetworkConfigScript = `#!/bin/bash
 NMCONNECTIONS_DIR=/etc/assisted/network
 MAC_NIC_MAPPING_FILE=mac_interface.ini
 
-if [ ! -d "$NMCONNECTIONS_DIR" ]
+if [[ ! -d "$NMCONNECTIONS_DIR" ]]
 then
+  echo "Error (exiting): Expected to find the directory $NMCONNECTIONS_DIR on the host but this was not present."
   exit 0
 fi
 
@@ -55,10 +56,12 @@ if [[ -f /etc/initrd-release ]]; then
 else
   ETC_NETWORK_MANAGER="/etc/NetworkManager/system-connections"
 fi
+echo "Info: ETC_NETWORK_MANAGER was set to $ETC_NETWORK_MANAGER"
 
 # remove default connection file create by NM(nm-initrd-generator). This is a WA until
 # NM is back to supporting priority between nmconnections
 rm -f ${ETC_NETWORK_MANAGER}/*
+echo "Info: Removing default connection files in $ETC_NETWORK_MANAGER"
 
 # Create a map of host mac addresses to their network interfaces
 function map_host_macs_to_interfaces() {
@@ -73,10 +76,17 @@ function map_host_macs_to_interfaces() {
 function find_host_directory_by_mac_address() {
   for d in $(ls -d ${NMCONNECTIONS_DIR}/host*)
   do
+
     mapping_file="${d}/${MAC_NIC_MAPPING_FILE}"
-    if [ ! -f $mapping_file ]
+    if [[ ! -f "$mapping_file" ]]
     then
-      echo "Mapping file '$mapping_file' is missing. Skipping on directory '$d'"
+      echo "Warning: Mapping file $mapping_file is missing. Skipping on directory $d"
+      continue
+    fi
+
+    if [[ -z "$(ls -A $d/*.nmconnection)" ]]
+    then
+      echo "Warning: Host directory does not contain any nmconnection files, skipping"
       continue
     fi
 
@@ -87,19 +97,22 @@ function find_host_directory_by_mac_address() {
       then
         host_dir=$(mktemp -d)
         cp ${d}/* $host_dir
+        echo "Info: Found host directory: $d, configuration copied to $host_dir"
         return
       fi
+      echo "Info: No match found in host macs for '$mac_address'"
     done
   done
 
   if [ -z "$host_dir" ]
   then
-    echo "None of host directories are a match for the current host"
+    echo "Error: None of the host directories contained a mac-address to host mapping for the current host"
     exit 0
   fi
 }
 
 function set_logical_nic_mac_mapping() {
+  echo "Info: Checking '$mapping_file' for logical nic to mac mappings"
   # initialize logical_nic_mac_map with mapping file entries
   readarray -t lines < "${mapping_file}"
   for line in "${lines[@]}"
@@ -113,10 +126,12 @@ function set_logical_nic_mac_mapping() {
 # Replace logical interface name in nmconnection files with the interface name from the mapping file
 # of host's directory. Replacement is done based on mac-address matching
 function update_interface_names_by_mapping_file() {
+  echo "Info: Updaing logical interface names in nmconnection files with interface name from mapping files"
 
   # iterate over host's nmconnection files and replace logical interface name with host's nic name
   for nmconn_file in $(ls -1 ${host_dir}/*.nmconnection)
   do
+    echo "Info: Updaing logical interface name in nmconnection files with interface name from mapping file $host_dir/$nmconn_file"
     # iterate over mapping to find nmconnection files with logical interface name
     for nic in "${!logical_nic_mac_map[@]}"
     do
@@ -129,18 +144,20 @@ function update_interface_names_by_mapping_file() {
         host_iface=${host_macs_to_hw_iface[$mac]}
         if [ -z "$host_iface" ]
         then
-          echo "Mapping file contains MAC Address '$mac' (for logical interface name '$nic') that doesn't exist on the host"
+          echo "Warning: Mapping file contains MAC Address '$mac' (for logical interface name '$nic') that doesn't exist on the host"
           continue
         fi
 
         # replace logical interface name with host interface name
         sed -i -e "s/=$nic$/=$host_iface/g" -e "s/=$nic\./=$host_iface\./g" $nmconn_file
+        echo "Info: Using logical interface name '$nic' for interface with Mac address '$mac', updated $nmconn_file)"
       fi
     done
   done
 }
 
 function copy_nmconnection_files_to_nm_config_dir() {
+  echo "Info: Copying nmconnection files to $ETC_NETWORK_MANAGER"
   for nmconn_file in $(ls -1 ${host_dir}/*.nmconnection)
   do
     # rename nmconnection files based on the actual interface name
@@ -155,16 +172,22 @@ function copy_nmconnection_files_to_nm_config_dir() {
       if [ ! -z "$host_iface" ]
       then
         mv $nmconn_file "${dir_path}/${host_iface}.${extension}"
+        echo "Info: Copied $nmconn_file to $dir_path/$host_iface/$extension"
+        continue
       fi
+      echo "Warning: Mapping for '$mac_address' was not present while attempting to copy $nmconn_file to $dir_path/$host_iface/$extension"
     fi
   done
 
   cp ${host_dir}/*.nmconnection ${ETC_NETWORK_MANAGER}/
+  echo "Info: Copied all nmconnection files from $host_dir to $ETC_NETWORK_MANAGER"
 }
 
+echo "PreNetworkConfig Start"
 map_host_macs_to_interfaces
 find_host_directory_by_mac_address
 set_logical_nic_mac_mapping
 update_interface_names_by_mapping_file
 copy_nmconnection_files_to_nm_config_dir
+echo "PreNetworkConfig End"
 `
