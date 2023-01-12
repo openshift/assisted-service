d36dc07aa24a518a7d2cac385163b2d7c7da7f0d
Osher De Paz <odepaz@redhat.com>
2023-01-12 13:03:05 +0200

    MGMT-11799: change all 'go get' commands (#4887)
    Since they have the tendency to change existing runtime dependencies,
    this changes all occurrences of 'go get' to 'go install' for wherever we
    want to just install a testing-related dependency.

--- hack/setup_env.sh
+++ hack/setup_env.sh
@@ -79,14 +79,14 @@ function assisted_service() {
   chmod +x operator-sdk_${OS}_${ARCH}
   install operator-sdk_${OS}_${ARCH} /usr/local/bin/operator-sdk
 
-  go get github.com/onsi/ginkgo/ginkgo@v1.16.4 \
-    golang.org/x/tools/cmd/goimports@v0.1.5 \
-    github.com/golang/mock/mockgen@v1.5.0 \
-    github.com/vektra/mockery/.../@v1.1.2 \
-    gotest.tools/gotestsum@v1.6.3 \
-    github.com/axw/gocov/gocov \
-    sigs.k8s.io/controller-tools/cmd/controller-gen@v0.6.2 \
-    github.com/AlekSi/gocov-xml@v0.0.0-20190121064608-3a14fb1c4737
+  go install -mod=mod github.com/onsi/ginkgo/ginkgo@v1.16.4
+  go install -mod=mod golang.org/x/tools/cmd/goimports@v0.1.5
+  go install -mod=mod github.com/golang/mock/mockgen@v1.5.0
+  go install -mod=mod github.com/vektra/mockery/v2@v2.12.3
+  go install -mod=mod gotest.tools/gotestsum@v1.6.3
+  go install -mod=mod github.com/axw/gocov/gocov@latest
+  go install -mod=mod sigs.k8s.io/controller-tools/cmd/controller-gen@v0.6.2
+  go install -mod=mod github.com/AlekSi/gocov-xml@v0.0.0-20190121064608-3a14fb1c4737
 
   python3 -m pip install --upgrade pip
   python3 -m pip install -r ./dev-requirements.txt
