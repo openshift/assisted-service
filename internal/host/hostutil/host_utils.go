fb7f8ab5f7434e03f8a63f455773877dad7c8b8f
Zane Bitter <zbitter@redhat.com>
2023-05-10 22:33:36 +1200

    OCPBUGS-13356: Fix 'vendor' root device hint evaluation

--- internal/host/hostutil/host_utils.go
+++ internal/host/hostutil/host_utils.go
@@ -207,7 +207,7 @@ func GetAcceptableDisksWithHints(disks []*models.Disk, hints *bmh_v1alpha1.RootD
 				continue
 			}
 
-			if hints.Vendor != "" && !strings.Contains(disk.Vendor, hints.Model) {
+			if hints.Vendor != "" && !strings.Contains(disk.Vendor, hints.Vendor) {
 				continue
 			}
 
