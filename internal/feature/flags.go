430b86e11580c8717b224bece08417a151ce777c
Juan Hernández <jhernand@redhat.com>
2023-06-15 15:52:51 +0200

    MGMT-14769: Enable upgrade agent by default (#5276)
    The upgrade agent feature has been explicitly enabled in the SaaS
    environments. This patch enables it by default in all the other
    environments. It will still be possible to disable it setting the
    `ENABLE_UPGRADE_AGENT` environment variable to `false`.
    
    Related: https://issues.redhat.com/browse/MGMT-14769
    Related: https://issues.redhat.com/browse/MGMT-10678
    
    Signed-off-by: Juan Hernandez <jhernand@redhat.com>

--- internal/feature/flags.go
+++ internal/feature/flags.go
@@ -3,7 +3,7 @@ package feature
 // Flags contains the values of the feature flags.
 type Flags struct {
 	// EnableUpgradeAgent is a boolean flag to enable or disable the upgrade agent feature:
-	EnableUpgradeAgent bool `envconfig:"ENABLE_UPGRADE_AGENT" default:"false"`
+	EnableUpgradeAgent bool `envconfig:"ENABLE_UPGRADE_AGENT" default:"true"`
 
 	// EnableRejectUnknownFields is a boolean flag to enable or disable rejecting unknown fields
 	// in JSON request bodies.
