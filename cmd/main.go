d920d7ea235d73d58596fbbf06de7ad21f5ba59d
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2023-06-20 19:33:26 +0000

    OCPBUGS-13738 enforce additional ntp sources added into chrony (#5308)
    Chrony configuration applied into machine config manifest based on the
    reply from the agent, meaning that when user add additional ntp source
    first the service will ask the agent to add it then return the reply to
    the service it will be stored in the DB and used to create the machine
    config chrony configuration
    
    Because NTP is a soft validation, in ZTP flow there are cases that the
    installation will start before the agent return a reply to the service,
    it will mean that those sources will not be applied, to avoid that when
    building the manifests in addition to taking the agent reply the
    additional ntp sources will be added directly from the DB
    
    The downside of this solution is that if the source is incorrect it
    will appear in /etc/chrony.conf but not in `chronyc sources` and it will
    not be resolved as the regular reply from the agent
    
    It will not harm the cluster installation or affect the cluster after
    the installation if the source is incorrect
    
    Co-authored-by: Michael Filanov <mfilanov@redhat.com>

--- cmd/main.go
+++ cmd/main.go
@@ -382,7 +382,7 @@ func main() {
 	hostApi := host.NewManager(log.WithField("pkg", "host-state"), db, notificationStream, eventsHandler, hwValidator,
 		instructionApi, &Options.HWValidatorConfig, metricsManager, &Options.HostConfig, lead, operatorsManager, providerRegistry, Options.EnableKubeAPI, objectHandler, versionHandler)
 	dnsApi := dns.NewDNSHandler(Options.BMConfig.BaseDNSDomains, log)
-	manifestsGenerator := network.NewManifestsGenerator(manifestsApi, Options.ManifestsGeneratorConfig)
+	manifestsGenerator := network.NewManifestsGenerator(manifestsApi, Options.ManifestsGeneratorConfig, db)
 	clusterApi := cluster.NewManager(Options.ClusterConfig, log.WithField("pkg", "cluster-state"), db,
 		notificationStream, eventsHandler, uploadClient, hostApi, metricsManager, manifestsGenerator, lead, operatorsManager, ocmClient, objectHandler, dnsApi, authHandler)
 	infraEnvApi := infraenv.NewManager(log.WithField("pkg", "host-state"), db, objectHandler)
