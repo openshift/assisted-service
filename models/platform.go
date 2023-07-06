fcc7514f2c9da9b20f98f1cc574e3a88e33291fd
Avishay Traeger <avishayt@users.noreply.github.com>
2023-07-06 14:35:36 +0300

    MGMT-15064: Update is_external API description (#5336)

--- models/platform.go
+++ models/platform.go
@@ -19,7 +19,8 @@ import (
 // swagger:model platform
 type Platform struct {
 
-	// Indicates if the underlying platform type is external.
+	// Used by the service to indicate that the platform-specific components are not included in
+	// OpenShift and must be provided as manifests separately.
 	// Read Only: true
 	IsExternal *bool `json:"is_external,omitempty"`
 
