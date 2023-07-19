59da9ccc86af1a0676f2ad684015efea2a7b444f
CrystalChun <cchun@redhat.com>
2023-07-19 06:22:55 -0700

    OCPBUGS-16077: Fix DNS validation
    https://issues.redhat.com/browse/OCPBUGS-16077
    Both the TLD (base domain) validation and the full
    domain validation were incorrect. Originally,
    the TLD didn't allow numbers and dashes; the
    full domain validation didn't allow multiple
    dashes. This fixes their regex to allow
    these valid domains.

--- pkg/validations/validations.go
+++ pkg/validations/validations.go
@@ -16,8 +16,8 @@ import (
 )
 
 const (
-	baseDomainRegex          = "^([a-z0-9]+(-[a-z0-9]+)*)+$"
-	dnsNameRegex             = "^([a-z0-9]+(-[a-z0-9]+)*[.])+[a-z]{2,}$"
+	baseDomainRegex          = `^[a-z\d][\-]*[a-z\d]+$`
+	dnsNameRegex             = `^([a-z\d]([\-]*[a-z\d]+)*\.)+[a-z\d]+[\-]*[a-z\d]+$`
 	hostnameRegex            = `^[a-z0-9][a-z0-9\-\.]{0,61}[a-z0-9]$`
 	installerArgsValuesRegex = `^[A-Za-z0-9@!#$%*()_+-=//.,";':{}\[\]]+$`
 )
