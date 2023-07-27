ef66b7d57f985b96bddbee1b7039622a3e9a75c9
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2023-07-27 17:43:25 +0000

    MGMT-15047: use installer to deploy on external platform (#5393)
    Use the installer to deploy the external platform, and cleanup the
    previous hack.
    Remove the feature set as the external platform is now stable.
    
    Co-authored-by: Adrien Gentil <agentil@redhat.com>

--- internal/provider/external/installConfig.go
+++ internal/provider/external/installConfig.go
@@ -2,7 +2,6 @@ package external
 
 import (
 	"github.com/go-openapi/swag"
-	configv1 "github.com/openshift/api/config/v1"
 	"github.com/openshift/assisted-service/internal/common"
 	"github.com/openshift/assisted-service/internal/host/hostutil"
 	"github.com/openshift/assisted-service/internal/installcfg"
@@ -11,13 +10,11 @@ import (
 )
 
 func (p externalProvider) AddPlatformToInstallConfig(cfg *installcfg.InstallerConfigBaremetal, cluster *common.Cluster) error {
-	// workaround until openshift installer supports external platform
 	cfg.Platform = installcfg.Platform{
-		None: &installcfg.PlatformNone{},
+		External: &installcfg.ExternalInstallConfigPlatform{
+			PlatformName: string(p.Name()),
+		},
 	}
-
-	// Enable feature set "TechPreviewNoUpgrade" in order to access External platform feature
-	cfg.FeatureSet = configv1.TechPreviewNoUpgrade
 
 	cfg.Networking.MachineNetwork = provider.GetMachineNetworkForUserManagedNetworking(p.Log, cluster)
 	if cluster.NetworkType != nil {
