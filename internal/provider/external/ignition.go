4156577719add3434659684d98b2bf645a7b4324
Adrien Gentil <agentil@redhat.com>
2023-09-14 14:01:27 +0200

    [release-ocm-2.8] MGMT-15034: MGMT-15047: use installer to deploy on external platform (#5490)
    * MGMT-15034: Fix patch of infrastructure CR with external platform (#5312)
    
    Patch directly the Infrastructure CR from PostCreateManifestsHook
    function which makes it easier to test.
    
    This part of the code is a workaround until the openshift installer
    supports external platform.
    
    * MGMT-15047: use installer to deploy on external platform (#5381)
    
    Use the installer to deploy the external platform, and cleanup the
    previous hack.
    Remove the feature set as the external platform is now stable.

--- internal/provider/external/ignition.go
+++ internal/provider/external/ignition.go
@@ -1,35 +1,7 @@
 package external
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
-
 	"github.com/openshift/assisted-service/internal/common"
-)
-
-// Workaround as openshift installer does not support the external platform.
-// The trick is to generate the manifests with platform=none and patch
-// the insfrastructure object with the external platform.
-const (
-	clusterInfraPatchTemplate = `---
-	- op: replace
-	  path: /spec/platformSpec
-	  value:
-		type: External
-		external:
-		  platformName: %s
-	- op: replace
-	  path: /status/platform
-	  value: External
-	- op: replace
-	  path: /status/platformStatus
-	  value:
-		type: External
-		external:
-		  cloudControllerManager:
-			state: External
-	`
 )
 
 func (p externalProvider) PreCreateManifestsHook(cluster *common.Cluster, envVars *[]string, workDir string) error {
@@ -37,17 +9,5 @@ func (p externalProvider) PreCreateManifestsHook(cluster *common.Cluster, envVar
 }
 
 func (p externalProvider) PostCreateManifestsHook(cluster *common.Cluster, envVars *[]string, workDir string) error {
-	p.Log.Info("Patching infrastructure object")
-
-	platformName := string(p.platformType)
-	clusterInfraPatch := fmt.Sprintf(clusterInfraPatchTemplate, platformName)
-
-	clusterInfraPatchPath := filepath.Join(workDir, "manifests", "cluster-infrastructure-02-config.yml.patch_01_configure_external_platform")
-	err := os.WriteFile(clusterInfraPatchPath, []byte(clusterInfraPatch), 0600)
-	if err != nil {
-		p.Log.Error("Couldn't write infrastructure patch %v", clusterInfraPatchPath)
-		return err
-	}
-
 	return nil
 }
