bc409c827fffdbf9387eab6a7d630a6ecac10423
Paul Maidment <pmaidmen@redhat.com>
2023-10-24 10:45:52 +0300

    NO-ISSUE: Disable local cluster import in ACM 2.9
    This will be released in ACM 2.10 and is being disabled by default for now.

--- cmd/main.go
+++ cmd/main.go
@@ -163,7 +163,7 @@ var Options struct {
 	AllowConvergedFlow                   bool          `envconfig:"ALLOW_CONVERGED_FLOW" default:"true"`
 	PreprovisioningImageControllerConfig controllers.PreprovisioningImageControllerConfig
 	BMACConfig                           controllers.BMACConfig
-	EnableLocalClusterImport             bool   `envconfig:"ENABLE_LOCAL_CLUSTER_IMPORT" default:"true"`
+	EnableLocalClusterImport             bool   `envconfig:"ENABLE_LOCAL_CLUSTER_IMPORT" default:"false"`
 	LocalClusterImportNamespace          string `envconfig:"LOCAL_CLUSTER_IMPORT_NAMESPACE" default:"local-agent-cluster"`
 
 	// Directory containing pre-generated TLS certs/keys for the ephemeral installer
