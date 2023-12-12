1d505063eda52e77f1b27f281fabc6cf103c8c57
CrystalChun <cchun@redhat.com>
2023-12-11 16:54:14 -0800

    MGMT-15886: Fix domain validation
    https://issues.redhat.com/browse/MGMT-15886
    Modify domain validation to adhere to the RFC standard for domains
    https://datatracker.ietf.org/doc/html/rfc1034#section-3.5
    Rules for labels:
    1. start with a letter
    2. end with a letter or digit
    3. and have as interior characters only letters, digits, and hyphen
    
    Domains contains either one label or multiple labels
    separated by a period.
    
    Top level (base) domains must have at least two characters and
    cannot end in a dash.

--- pkg/validations/validations.go
+++ pkg/validations/validations.go
@@ -16,8 +16,8 @@ import (
 )
 
 const (
-	baseDomainRegex          = `^[a-z\d]+[\-]*[a-z\d]+$`
-	dnsNameRegex             = `^([a-z\d]([\-]*[a-z\d]+)*\.)+[a-z\d]+[\-]*[a-z\d]+$`
+	baseDomainRegex          = `^[a-z]([\-]*[a-z\d]+)+$`
+	dnsNameRegex             = `^([a-z]([\-]*[a-z\d]+)*\.)+[a-z\d]+([\-]*[a-z\d]+)+$`
 	wildCardDomainRegex      = `^(validateNoWildcardDNS\.).+\.?$`
 	hostnameRegex            = `^[a-z0-9][a-z0-9\-\.]{0,61}[a-z0-9]$`
 	installerArgsValuesRegex = `^[A-Za-z0-9@!#$%*()_+-=//.,";':{}\[\]]+$`
