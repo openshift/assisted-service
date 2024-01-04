0bcdf312daeaaa88696b2805923ec90033fc32e8
Nick Carboni <ncarboni@redhat.com>
2024-01-04 11:22:45 -0500

    copy status in conversion

--- api/hiveextension/v1beta1/conversion.go
+++ api/hiveextension/v1beta1/conversion.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 
 	"github.com/openshift/assisted-service/api/hiveextension/v1beta2"
+	hivev1 "github.com/openshift/hive/apis/hive/v1"
 	"sigs.k8s.io/controller-runtime/pkg/conversion"
 )
 
@@ -11,6 +12,8 @@ import (
 func (src *AgentClusterInstall) ConvertTo(dstRaw conversion.Hub) error {
 	dst := dstRaw.(*v1beta2.AgentClusterInstall)
 	dst.ObjectMeta = src.ObjectMeta
+
+	// Spec
 
 	// direct copies for non-struct fields
 	dst.Spec.ImageSetRef = src.Spec.ImageSetRef
@@ -128,6 +131,72 @@ func (src *AgentClusterInstall) ConvertTo(dstRaw conversion.Hub) error {
 		copy(dst.Spec.IngressVIPs, src.Spec.IngressVIPs)
 	} else if src.Spec.IngressVIP != "" {
 		dst.Spec.IngressVIPs = []string{src.Spec.IngressVIP}
+	}
+
+	// Status
+
+	// Direct copies
+	dst.Status.ControlPlaneAgentsDiscovered = src.Status.ControlPlaneAgentsDiscovered
+	dst.Status.ControlPlaneAgentsReady = src.Status.ControlPlaneAgentsReady
+	dst.Status.WorkerAgentsDiscovered = src.Status.WorkerAgentsDiscovered
+	dst.Status.WorkerAgentsReady = src.Status.WorkerAgentsReady
+	dst.Status.ConnectivityMajorityGroups = src.Status.ConnectivityMajorityGroups
+	dst.Status.UserManagedNetworking = src.Status.UserManagedNetworking
+	dst.Status.PlatformType = v1beta2.PlatformType(string(src.Status.PlatformType))
+	dst.Status.ValidationsInfo = src.Status.ValidationsInfo
+
+	// json copies
+	if l := len(src.Status.Conditions); l != 0 {
+		tmp, err = json.Marshal(src.Status.Conditions)
+		if err != nil {
+			return err
+		}
+		dst.Status.Conditions = make([]hivev1.ClusterInstallCondition, l)
+		if err = json.Unmarshal(tmp, &dst.Status.Conditions); err != nil {
+			return err
+		}
+	}
+
+	tmp, err = json.Marshal(src.Status.Progress)
+	if err != nil {
+		return err
+	}
+	if err = json.Unmarshal(tmp, &dst.Status.Progress); err != nil {
+		return err
+	}
+
+	if l := len(src.Status.MachineNetwork); l != 0 {
+		tmp, err = json.Marshal(src.Status.MachineNetwork)
+		if err != nil {
+			return err
+		}
+		dst.Status.MachineNetwork = make([]v1beta2.MachineNetworkEntry, l)
+		if err = json.Unmarshal(tmp, &dst.Status.MachineNetwork); err != nil {
+			return err
+		}
+	}
+
+	tmp, err = json.Marshal(src.Status.DebugInfo)
+	if err != nil {
+		return err
+	}
+	if err = json.Unmarshal(tmp, &dst.Status.DebugInfo); err != nil {
+		return err
+	}
+
+	// changed fields
+	if l := len(src.Status.APIVIPs); l != 0 {
+		dst.Status.APIVIPs = make([]string, l)
+		copy(dst.Status.APIVIPs, src.Status.APIVIPs)
+	} else if src.Status.APIVIP != "" {
+		dst.Status.APIVIPs = []string{src.Status.APIVIP}
+	}
+
+	if l := len(src.Status.IngressVIPs); l != 0 {
+		dst.Status.IngressVIPs = make([]string, l)
+		copy(dst.Status.IngressVIPs, src.Status.IngressVIPs)
+	} else if src.Status.IngressVIP != "" {
+		dst.Status.IngressVIPs = []string{src.Status.IngressVIP}
 	}
 
 	return nil
@@ -250,6 +319,69 @@ func (dst *AgentClusterInstall) ConvertFrom(srcRaw conversion.Hub) error {
 	if l := len(src.Spec.IngressVIPs); l != 0 {
 		dst.Spec.IngressVIPs = make([]string, l)
 		copy(dst.Spec.IngressVIPs, src.Spec.IngressVIPs)
+	}
+
+	// Status
+
+	// Direct copies
+	dst.Status.ControlPlaneAgentsDiscovered = src.Status.ControlPlaneAgentsDiscovered
+	dst.Status.ControlPlaneAgentsReady = src.Status.ControlPlaneAgentsReady
+	dst.Status.WorkerAgentsDiscovered = src.Status.WorkerAgentsDiscovered
+	dst.Status.WorkerAgentsReady = src.Status.WorkerAgentsReady
+	dst.Status.ConnectivityMajorityGroups = src.Status.ConnectivityMajorityGroups
+	dst.Status.UserManagedNetworking = src.Status.UserManagedNetworking
+	dst.Status.PlatformType = PlatformType(string(src.Status.PlatformType))
+	dst.Status.ValidationsInfo = src.Status.ValidationsInfo
+
+	// json copies
+	if l := len(src.Status.Conditions); l != 0 {
+		tmp, err = json.Marshal(src.Status.Conditions)
+		if err != nil {
+			return err
+		}
+		dst.Status.Conditions = make([]hivev1.ClusterInstallCondition, l)
+		if err = json.Unmarshal(tmp, &dst.Status.Conditions); err != nil {
+			return err
+		}
+	}
+
+	tmp, err = json.Marshal(src.Status.Progress)
+	if err != nil {
+		return err
+	}
+	if err = json.Unmarshal(tmp, &dst.Status.Progress); err != nil {
+		return err
+	}
+
+	if l := len(src.Status.MachineNetwork); l != 0 {
+		tmp, err = json.Marshal(src.Status.MachineNetwork)
+		if err != nil {
+			return err
+		}
+		dst.Status.MachineNetwork = make([]MachineNetworkEntry, l)
+		if err = json.Unmarshal(tmp, &dst.Status.MachineNetwork); err != nil {
+			return err
+		}
+	}
+
+	tmp, err = json.Marshal(src.Status.DebugInfo)
+	if err != nil {
+		return err
+	}
+	if err = json.Unmarshal(tmp, &dst.Status.DebugInfo); err != nil {
+		return err
+	}
+
+	// changed fields
+
+	if l := len(src.Status.APIVIPs); l != 0 {
+		dst.Status.APIVIPs = make([]string, l)
+		copy(dst.Status.APIVIPs, src.Status.APIVIPs)
+	}
+
+	if l := len(src.Status.IngressVIPs); l != 0 {
+		dst.Status.IngressVIPs = make([]string, l)
+		copy(dst.Status.IngressVIPs, src.Status.IngressVIPs)
 	}
 
 	return nil
