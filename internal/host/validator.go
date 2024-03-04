b9ba27b9569e9fe1aaa742754370d3745cebde7e
Zane Bitter <zbitter@redhat.com>
2024-03-04 14:19:28 +1300

    Update comment on L2 connectivity checks and MachineNetworks
    OpenShift has ~always supported having machines in multiple
    machineNetworks, so update the TODO comment to reflect that accounting
    for this is already something we need to do to fully support
    non-UserManagedNetworking clusters. (UserManagedNetworking clusters use
    only the L3 connectivity check.)
    
    See https://issues.redhat.com/browse/OCPBUGS-30730 for more details.

--- internal/host/validator.go
+++ internal/host/validator.go
@@ -675,8 +675,9 @@ func (v *validator) belongsToL2MajorityGroup(c *validationContext, majorityGroup
 		return ValidationFailure
 	}
 
-	// TODO(mko) This rule should be revised as soon as OCP supports multiple machineNetwork
-	//           entries using the same IP stack.
+	// TODO(mko) This rule must be revised to support multiple machineNetwork
+	//           entries using the same IP stack on clusters without
+	//           user-managed networking. (OCPBUGS-30730)
 	areNetworksEqual := func(ipnet1, ipnet2 *net.IPNet) bool {
 		return ipnet1.IP.Equal(ipnet2.IP) && bytes.Equal(ipnet1.Mask, ipnet2.Mask)
 	}
