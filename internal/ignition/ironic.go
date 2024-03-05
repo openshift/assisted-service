ccb68ea72f69d3f759739bdc3f4efdca1ec57bde
Nick Carboni <ncarboni@redhat.com>
2024-03-05 16:58:39 -0500

    Disable ironic vlan creation when static networking config is present
    Previously the ironic agent would define vlan interfaces when it
    detected that vlans were present using LLDP
    
    This change only allows this type of reconfiguration if the user didn't
    provide static networking config. If a user does provide static
    networking config and requires vlans it's expected that they will
    configure them manually rather than relying on LLDP-based configuration
    from the ironic agent.
    
    Resolves https://issues.redhat.com/browse/MGMT-17106

--- internal/ignition/ironic.go
+++ internal/ignition/ironic.go
@@ -13,12 +13,17 @@ func GenerateIronicConfig(ironicBaseURL string, ironicInspectorURL string, infra
 	config.Ignition.Version = "3.2.0"
 
 	httpProxy, httpsProxy, noProxy := common.GetProxyConfigs(infraEnv.Proxy)
+	ironicInspectorVlanInterfaces := ""
+	// allow ironic to configure vlans if no static network config is provided
+	if infraEnv.StaticNetworkConfig == "" {
+		ironicInspectorVlanInterfaces = "all"
+	}
 	// TODO: this should probably get the pullSecret as well
-	ib, err := iccignition.New([]byte{}, []byte{}, ironicBaseURL, ironicInspectorURL, ironicAgentImage, "", "", "", httpProxy, httpsProxy, noProxy, "")
+	ib, err := iccignition.New([]byte{}, []byte{}, ironicBaseURL, ironicInspectorURL, ironicAgentImage, "", "", "", httpProxy, httpsProxy, noProxy, "", ironicInspectorVlanInterfaces)
 	if err != nil {
 		return []byte{}, err
 	}
-	config.Storage.Files = []ignition_config_types_32.File{ib.IronicAgentConf()}
+	config.Storage.Files = []ignition_config_types_32.File{ib.IronicAgentConf(ironicInspectorVlanInterfaces)}
 	// TODO: sort out the flags (authfile...) and copy network
 	config.Systemd.Units = []ignition_config_types_32.Unit{ib.IronicAgentService(false)}
 	return json.Marshal(config)
