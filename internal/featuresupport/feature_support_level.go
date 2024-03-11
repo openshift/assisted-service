feddc0c1facfb7fb7a977af41fccf90a8db22cc0
Eran Ifrach <eifrach@redhat.com>
2024-03-11 11:38:19 +0200

    fix lint error

--- internal/featuresupport/feature_support_level.go
+++ internal/featuresupport/feature_support_level.go
@@ -6,7 +6,6 @@ import (
 	"github.com/go-openapi/swag"
 	"github.com/openshift/assisted-service/internal/common"
 	"github.com/openshift/assisted-service/models"
-	// "github.com/openshift/assisted-service/restapi/operations/installer"
 	"github.com/thoas/go-funk"
 )
 
