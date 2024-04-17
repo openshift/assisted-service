6e7117aead518370a40d17d4d9f0f0ccaf3b85a3
Eran Ifrach <76165038+eifrach@users.noreply.github.com>
2024-04-17 15:21:06 +0300

    MGMT-17414: Adding new requirements for LVM on 4.16 (#6165)

--- internal/operators/lvm/config.go
+++ internal/operators/lvm/config.go
@@ -5,6 +5,7 @@ const (
 	LvmsMinOpenshiftVersion4_12                        string = "4.12.0"
 	LvmsMinOpenshiftVersion_ForNewResourceRequirements string = "4.13.0"
 	LvmMinMultiNodeSupportVersion                      string = "4.15.0"
+	LvmNewResourcesOpenshiftVersion4_16                string = "4.16.0"
 
 	LvmoSubscriptionName string = "odf-lvm-operator"
 	LvmsSubscriptionName string = "lvms-operator"
@@ -14,4 +15,5 @@ type Config struct {
 	LvmCPUPerHost                 int64 `envconfig:"LVM_CPU_PER_HOST" default:"1"`
 	LvmMemoryPerHostMiB           int64 `envconfig:"LVM_MEMORY_PER_HOST_MIB" default:"400"`
 	LvmMemoryPerHostMiBBefore4_13 int64 `envconfig:"LVM_MEMORY_PER_HOST_MIB" default:"1200"`
+	LvmMemoryPerHostMiBFrom4_16   int64 `envconfig:"LVM_MEMORY_PER_HOST_MIB" default:"100"`
 }
