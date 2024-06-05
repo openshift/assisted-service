b1b58d7f0a1ab48b73719fb1eef607dcbebda8c8
Dan Manor <dmanor@redhat.com>
2024-06-05 14:48:04 +0300

    NO-ISSUE: Fix Failed to download metadata for dnf repositories in centos8 by using archived centos8 repositories (#6397)

---
+++ hack/container_build_scripts/utils.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+function replace_dnf_repositories_ref () {
+    sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+    sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+}
