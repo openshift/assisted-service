4bac6e35385b6727f61a4ae0b6c11cf36e4bf5ea
Dan Manor <dmanor@redhat.com>
2024-06-18 16:44:34 +0300

    [release-ocm-2.11] MGMT-18069: ClusterServiceVersion for release 2.11 branch references "latest" tag (#6443)
    * MGMT-18069: ClusterServiceVersion for release 2.11 branch references 'latest' tag
    
    * MGMT-18187: Reduce the list of approvers for release-ocm-2.11

--- deploy/operator/setup_assisted_operator.sh
+++ deploy/operator/setup_assisted_operator.sh
@@ -153,7 +153,7 @@ $(subscription_config)
   installPlanApproval: Automatic
   name: assisted-service-operator
   source: ${catalog_source_name}
-  channel: ${CHANNEL:-alpha}
+  channel: ${CHANNEL:-ocm-2.11}
   sourceNamespace: openshift-marketplace
 EOCR
 
