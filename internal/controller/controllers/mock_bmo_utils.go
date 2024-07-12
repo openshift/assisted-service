ea90ffafe15148ac711e08fabd57fd0999ee6c0f
Adrien Gentil <agentil@redhat.com>
2024-07-12 12:31:03 +0200

    MGMT-17478: Use ICC config secret for converged flow info when available (#6533)
    * MGMT-17478: Use ICC config secret for converged flow info when available
    
    When available, read the ICC secret set by BMO in order to retrieve the
    agent image, the agent URL(s), and the inspector URL(s).
    
    This this change, we wil try to:
    1/ Get the agent image from the user override
    2/ if 1/ is not found, get the image from the ICC configuration
    3/ if 2/ is not found, get the image from the HUB cluster
    4/ if 3/ is not found, return the default image
    
    * Add context to GetICCConfig method
    
    * remove space
    
    * refactor tiered logic
    
    * Create GetImageArchitecture mehod
    
    * fix lint
    
    * refactor logs and getIronicAgentImageByRelease
    
    * add infraenv in logger earlier
    
    * make getICCConfig private

--- internal/controller/controllers/mock_bmo_utils.go
+++ internal/controller/controllers/mock_bmo_utils.go
@@ -5,6 +5,7 @@
 package controllers
 
 import (
+	context "context"
 	reflect "reflect"
 
 	gomock "github.com/golang/mock/gomock"
@@ -61,4 +62,19 @@ func (m *MockBMOUtils) GetIronicIPs() ([]string, []string, error) {
 func (mr *MockBMOUtilsMockRecorder) GetIronicIPs() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetIronicIPs", reflect.TypeOf((*MockBMOUtils)(nil).GetIronicIPs))
+}
+
+// getICCConfig mocks base method.
+func (m *MockBMOUtils) getICCConfig(arg0 context.Context) (*ICCConfig, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "getICCConfig", arg0)
+	ret0, _ := ret[0].(*ICCConfig)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// getICCConfig indicates an expected call of getICCConfig.
+func (mr *MockBMOUtilsMockRecorder) getICCConfig(arg0 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "getICCConfig", reflect.TypeOf((*MockBMOUtils)(nil).getICCConfig), arg0)
 }
