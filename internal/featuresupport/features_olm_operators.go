d85923dc487c57ef63984ba3600954d8addf2328
Eran Ifrach <eifrach@redhat.com>
2024-08-04 04:34:50 -0400

    [MGMT-18378] allow CNV on ARM tech preview

--- internal/featuresupport/features_olm_operators.go
+++ internal/featuresupport/features_olm_operators.go
@@ -168,6 +168,13 @@ func (feature *CnvFeature) getSupportLevel(filters SupportLevelFilters) models.S
 		return models.SupportLevelUnavailable
 	}
 
+	architectureID := cpuArchitectureFeatureIdMap[swag.StringValue(filters.CPUArchitecture)]
+	if architectureID == models.ArchitectureSupportLevelIDARM64ARCHITECTURE {
+		if isGreaterOrEqual, _ := common.BaseVersionGreaterOrEqual("4.14", filters.OpenshiftVersion); isGreaterOrEqual {
+			return models.SupportLevelDevPreview
+		}
+	}
+
 	return models.SupportLevelSupported
 }
 
@@ -178,12 +185,16 @@ func (feature *CnvFeature) getIncompatibleFeatures(string) *[]models.FeatureSupp
 	}
 }
 
-func (feature *CnvFeature) getIncompatibleArchitectures(_ *string) *[]models.ArchitectureSupportLevelID {
-	return &[]models.ArchitectureSupportLevelID{
+func (feature *CnvFeature) getIncompatibleArchitectures(OCPVersion *string) *[]models.ArchitectureSupportLevelID {
+	incompatibleArchitecture := []models.ArchitectureSupportLevelID{
 		models.ArchitectureSupportLevelIDS390XARCHITECTURE,
 		models.ArchitectureSupportLevelIDPPC64LEARCHITECTURE,
-		models.ArchitectureSupportLevelIDARM64ARCHITECTURE,
 	}
+	if isLessThan, _ := common.BaseVersionLessThan("4.14", *OCPVersion); isLessThan {
+		incompatibleArchitecture = append(incompatibleArchitecture, models.ArchitectureSupportLevelIDARM64ARCHITECTURE)
+	}
+
+	return &incompatibleArchitecture
 }
 
 func (feature *CnvFeature) getFeatureActiveLevel(cluster *common.Cluster, _ *models.InfraEnv, clusterUpdateParams *models.V2ClusterUpdateParams, _ *models.InfraEnvUpdateParams) featureActiveLevel {
