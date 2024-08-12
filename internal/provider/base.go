00a5eb37b615c97220cd012bc889064ae54fd3f1
Adrien Gentil <agentil@redhat.com>
2024-08-12 20:46:02 +0200

    MGMT-18514: Calculate machine networks in external platform (#6661)
    * MGMT-18514: Calculate node-ip in external platform
    
    Since external platform relies on user managed networking, we need the
    feature introduced in
    https://github.com/openshift/assisted-service/pull/6257 to determine the
    machine networks.
    
    This change is useful when using iSCSI boot drive on Oracle Cloud
    Infrastructure, in order to select the machine network that doesn't
    belonmg to the iSCSI subnet.
    
    * refactor

--- internal/provider/base.go
+++ internal/provider/base.go
@@ -1,11 +1,14 @@
 package provider
 
 import (
+	"github.com/go-openapi/swag"
 	"github.com/openshift/assisted-service/internal/common"
+	"github.com/openshift/assisted-service/internal/host/hostutil"
 	"github.com/openshift/assisted-service/internal/installcfg"
 	"github.com/openshift/assisted-service/internal/network"
 	"github.com/openshift/assisted-service/internal/usage"
 	"github.com/openshift/assisted-service/models"
+	"github.com/samber/lo"
 	"github.com/sirupsen/logrus"
 )
 
@@ -51,4 +54,54 @@ func GetMachineNetworkForUserManagedNetworking(log logrus.FieldLogger, cluster *
 	}
 
 	return nil
+}
+
+func replaceMachineNetworkIfNeeded(log logrus.FieldLogger, cluster *common.Cluster, cfg *installcfg.InstallerConfigBaremetal) {
+	bootstrapHost := common.GetBootstrapHost(cluster)
+	if bootstrapHost == nil {
+		log.Warnf("GetBootstrapHost: failed to get bootstrap host for cluster %s", lo.FromPtr(cluster.ID))
+		return
+	}
+	nodeIpAllocations, err := network.GenerateNonePlatformAddressAllocation(cluster, log)
+	if err != nil {
+		log.WithError(err).Warnf("failed to get node ip allocations for cluster %s", lo.FromPtr(cluster.ID))
+		return
+	}
+	allocation, ok := nodeIpAllocations[lo.FromPtr(bootstrapHost.ID)]
+	if !ok {
+		log.Warnf("node ip allocation for bootstrap host %s in cluster %s is missing", lo.FromPtr(bootstrapHost.ID), lo.FromPtr(cluster.ID))
+		return
+	}
+	isIpv4 := network.IsIPV4CIDR(allocation.Cidr)
+	for i := range cfg.Networking.MachineNetwork {
+		if network.IsIPV4CIDR(cfg.Networking.MachineNetwork[i].Cidr) == isIpv4 {
+			if cfg.Networking.MachineNetwork[i].Cidr != allocation.Cidr {
+				log.Infof("Replacing machine network %s with %s", cfg.Networking.MachineNetwork[i].Cidr, allocation.Cidr)
+				cfg.Networking.MachineNetwork[i].Cidr = allocation.Cidr
+			}
+			break
+		}
+	}
+}
+
+func ConfigureUserManagedNetworkingInInstallConfig(log logrus.FieldLogger, cluster *common.Cluster, cfg *installcfg.InstallerConfigBaremetal) {
+	cfg.Networking.MachineNetwork = GetMachineNetworkForUserManagedNetworking(log, cluster)
+	if cluster.NetworkType != nil {
+		cfg.Networking.NetworkType = swag.StringValue(cluster.NetworkType)
+	}
+
+	if common.IsSingleNodeCluster(cluster) {
+
+		if cfg.Networking.NetworkType == "" {
+			cfg.Networking.NetworkType = models.ClusterNetworkTypeOVNKubernetes
+		}
+
+		bootstrap := common.GetBootstrapHost(cluster)
+		if bootstrap != nil {
+			cfg.BootstrapInPlace = &installcfg.BootstrapInPlace{InstallationDisk: hostutil.GetHostInstallationPath(bootstrap)}
+		}
+	} else {
+		replaceMachineNetworkIfNeeded(log, cluster, cfg)
+	}
+
 }
