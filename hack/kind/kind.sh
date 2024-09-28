b8a941a934e967fbe0aa998325572fdcf192d917
danmanor <dmanor@redhat.com>
2024-09-28 13:42:01 +0300

    Revert "NO-ISSUE: Support both minikube and kind for deploying on k8s cluster (#6664)"
    This reverts commit c016547e329883d463a2b247988a1c0d528b7368.

--- hack/kind/kind.sh
+++ hack/kind/kind.sh
@@ -4,17 +4,16 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-KIND_VERSION=${KIND_VERSION:-"0.23.0"}
-HUB_CLUSTER_NAME=${HUB_CLUSTER_NAME:-assisted-hub-cluster}
+KIND_VERSION="0.23.0"
 
 __dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 function check() {
 	if command -v kind > /dev/null 2>&1; then
-		echo "'kind' is installed. '$(kind --version)' will be used"
+		echo "'Kind' is installed. '$(kind --version)' will be used"
 		return 0
 	else
-		echo "'kind' executable is not avialable in PATH"
+		echo "'Kind' executable is not avialable in PATH"
 		return 1
 	fi
 }
@@ -33,7 +32,7 @@ function install() {
 function create() {
 	check
 
-	if ! kind get clusters | grep $HUB_CLUSTER_NAME; then
+	if ! kind get clusters | grep assisted-installer-kind ; then
 		kind create cluster --config $__dir/kind-config.yaml
 	else
 		echo "Cluster already existing. Skipping creation"
@@ -43,17 +42,17 @@ function create() {
 function delete() {
 	check
 
-	kind delete cluster --name=$HUB_CLUSTER_NAME
+	kind delete cluster --name=assisted-installer-kind
 }
 
-function load_image() {
+function load_kind_image() {
 	check
 	
     local image_path=$1
     local cluster_name=$2
 
     if [ -z "$image_path" ] || [ -z "$cluster_name" ]; then
-        echo "Usage: load_image <image_path> <cluster_name>"
+        echo "Usage: load_kind_image <image_path> <cluster_name>"
         return 1
     fi
 
@@ -61,7 +60,7 @@ function load_image() {
 }
 
 if [ $# -eq 0 ]; then
-	echo "Usage: $__dir/kind.sh (install|check|create|load_image)"
+	echo "Usage: $__dir/kind.sh (install|check|create)"
 	exit 1
 else
 	$@
