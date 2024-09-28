ef2f21c69a1b9d32b0569bbb14b739f6ca4aab1e
danmanor <dmanor@redhat.com>
2024-09-28 15:23:27 +0300

    Revert "MGMT-18155: handle state of day2 node in Done stage (#6570)"
    This reverts commit d4e710f863f740f0bae3b7bce9c06655ef1e7edc.

--- internal/host/statemachine.go
+++ internal/host/statemachine.go
@@ -64,7 +64,6 @@ func NewHostStateMachine(sm stateswitch.StateMachine, th TransitionHandler) stat
 		SourceStates: []stateswitch.State{
 			stateswitch.State(models.HostStatusInstallingInProgress),
 			stateswitch.State(models.HostStatusInstalling),
-			stateswitch.State(models.HostStatusInstallingPendingUserAction),
 		},
 		DestinationState: stateswitch.State(models.HostStatusAddedToExistingCluster),
 		PostTransition:   th.PostHostProgress,
