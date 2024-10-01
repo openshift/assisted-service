c4b0b6da8d972d0b540b45389b768531887d0691
Linoy Hadad <lhadad@redhat.com>
2024-10-01 05:19:49 +0300

    Use old static network flow if OCP version is empty (#6848)

--- pkg/staticnetworkconfig/backward_compatibility.go
+++ pkg/staticnetworkconfig/backward_compatibility.go
@@ -1,10 +1,19 @@
 package staticnetworkconfig
 
-import "github.com/openshift/assisted-service/internal/common"
+import (
+	"github.com/openshift/assisted-service/internal/common"
+	log "github.com/sirupsen/logrus"
+)
 
 const MinimalVersionForNmstatectl = "4.14"
 
 func NMStatectlServiceSupported(version, arch string) (bool, error) {
+	// When a cluster is imported, the OpenshiftVersion isn't stored in the database.
+	// Consequently, a bound InfraEnv with static networking uses the Cluster's OpenshiftVersion, which is empty.
+	if version == "" {
+		log.Info("ocp version is empty")
+		return false, nil
+	}
 	versionOK, err := common.VersionGreaterOrEqual(version, MinimalVersionForNmstatectl)
 	if err != nil {
 		return false, err
