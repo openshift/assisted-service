9525a42944e3cefd3cc686191dca37112e132a08
Gilad Ravid <60739156+giladravid16@users.noreply.github.com>
2024-10-14 16:35:42 +0300

    MGMT-19129: Migrate assisted-service to Konflux (#6911)

--- build_deploy.sh
+++ build_deploy.sh
@@ -8,6 +8,8 @@ DOCKER_CONF="${PWD}/.docker"
 mkdir -p "${DOCKER_CONF}"
 docker --config="${DOCKER_CONF}" login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io
 
+docker tag "${ASSISTED_SERVICE_IMAGE}:${TAG}" "${ASSISTED_SERVICE_IMAGE}:${SHORT_TAG}"
 docker tag "${ASSISTED_SERVICE_IMAGE}:${TAG}" "${ASSISTED_SERVICE_IMAGE}:latest"
 docker --config="${DOCKER_CONF}" push "${ASSISTED_SERVICE_IMAGE}:${TAG}"
+docker --config="${DOCKER_CONF}" push "${ASSISTED_SERVICE_IMAGE}:${SHORT_TAG}"
 docker --config="${DOCKER_CONF}" push "${ASSISTED_SERVICE_IMAGE}:latest"
