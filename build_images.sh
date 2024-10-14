637bac4c3837b4ac458391c9fc006a35c3930ba3
Gilad Ravid <60739156+giladravid16@users.noreply.github.com>
2024-10-14 16:35:42 +0300

    MGMT-19129: Migrate assisted-service to Konflux (#6911)

--- build_images.sh
+++ build_images.sh
@@ -5,7 +5,8 @@ set -o pipefail
 set -o errexit
 set -o xtrace
 
-TAG=$(git rev-parse --short=7 HEAD)
+TAG=$(git rev-parse HEAD)
+SHORT_TAG=$(git rev-parse --short=7 HEAD)
 ASSISTED_SERVICE_IMAGE=quay.io/app-sre/assisted-service
 export SERVICE="${ASSISTED_SERVICE_IMAGE}:${TAG}"
 export CONTAINER_BUILD_EXTRA_PARAMS="--no-cache"
