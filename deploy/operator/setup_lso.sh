1982c30b8526fc17a8c2fa8cacb0c03403648b9c
danmanor <dmanor@redhat.com>
2024-10-29 09:47:17 +0200

    MGMT-19190: Bump LSO version override to 4.18 to enable assisted operator deployment scripts for OCP 4.18

--- deploy/operator/setup_lso.sh
+++ deploy/operator/setup_lso.sh
@@ -24,22 +24,22 @@ function install_lso() {
 
 
     OC_VERSION_MAJOR_MINOR=$(oc version -o json | jq --raw-output '.openshiftVersion' | cut -d'.' -f1-2)
-    if [[ ${OC_VERSION_MAJOR_MINOR} == "4.17" && "${DISCONNECTED}" != true ]]; then
-        # LSO has not been published to the 4.15 redhat-operators catalog, so
-        # it cannot be installed on OpenShift 4.15. Until this is resolved,
-        # we explicitly install the 4.13 catalog as redhat-operators-v4-14
-        # and then subscribe to the LSO version from the 4.14 rather than the 4.15 catalog.
-        # TODO: Bump the versions once LSO is published to the 4.14 catalog.
-        catalog_source_name="redhat-operators-v4-16"
+    if [[ ${OC_VERSION_MAJOR_MINOR} == "4.18" && "${DISCONNECTED}" != true ]]; then
+        # LSO has not been published to the 4.18 redhat-operators catalog, so
+        # it cannot be installed on OpenShift 4.18. Until this is resolved,
+        # we explicitly install the 4.17 catalog as redhat-operators-v4-17
+        # and then subscribe to the LSO version from the 4.17 rather than the 4.18 catalog.
+        # TODO: Bump the versions once LSO is published to the 4.18 catalog.
+        catalog_source_name="redhat-operators-v4-17"
         oc create -f - <<EOCR
         kind: CatalogSource
         apiVersion: operators.coreos.com/v1alpha1
         metadata:
-          name: redhat-operators-v4-16
+          name: redhat-operators-v4-17
           namespace: openshift-marketplace
         spec:
-          displayName: Red Hat Operators v4.16
-          image: registry.redhat.io/redhat/redhat-operator-index:v4.16
+          displayName: Red Hat Operators v4.17
+          image: registry.redhat.io/redhat/redhat-operator-index:v4.17
           priority: -100
           publisher: Red Hat
           sourceType: grpc
