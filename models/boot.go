d8cdc344c31f20e61168d83c268725d6b1b19b97
Juan Hernández <juan.hernandez@redhat.com>
2024-11-22 23:55:35 +0100

    MGMT-19331: Add _secure_boot_state_ to inventory (#7016)
    This patch adds a new `secure_boot_state` field to hosts inventory. This
    will be collected by the agent, and used to validate that secure boot is
    disabled when installing the _OpenShift AI_ operator with NVIDIA GPUs,
    as the NVIDIA driver requires that.
    
    Related: https://issues.redhat.com/browse/MGMT-19331
    Related: https://issues.redhat.com/browse/MGMT-19056
    
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- models/boot.go
+++ models/boot.go
@@ -8,6 +8,7 @@ package models
 import (
 	"context"
 
+	"github.com/go-openapi/errors"
 	"github.com/go-openapi/strfmt"
 	"github.com/go-openapi/swag"
 )
@@ -25,15 +26,67 @@ type Boot struct {
 
 	// pxe interface
 	PxeInterface string `json:"pxe_interface,omitempty"`
+
+	// secure boot state
+	SecureBootState SecureBootState `json:"secure_boot_state,omitempty"`
 }
 
 // Validate validates this boot
 func (m *Boot) Validate(formats strfmt.Registry) error {
+	var res []error
+
+	if err := m.validateSecureBootState(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if len(res) > 0 {
+		return errors.CompositeValidationError(res...)
+	}
 	return nil
 }
 
-// ContextValidate validates this boot based on context it is used
+func (m *Boot) validateSecureBootState(formats strfmt.Registry) error {
+	if swag.IsZero(m.SecureBootState) { // not required
+		return nil
+	}
+
+	if err := m.SecureBootState.Validate(formats); err != nil {
+		if ve, ok := err.(*errors.Validation); ok {
+			return ve.ValidateName("secure_boot_state")
+		} else if ce, ok := err.(*errors.CompositeError); ok {
+			return ce.ValidateName("secure_boot_state")
+		}
+		return err
+	}
+
+	return nil
+}
+
+// ContextValidate validate this boot based on the context it is used
 func (m *Boot) ContextValidate(ctx context.Context, formats strfmt.Registry) error {
+	var res []error
+
+	if err := m.contextValidateSecureBootState(ctx, formats); err != nil {
+		res = append(res, err)
+	}
+
+	if len(res) > 0 {
+		return errors.CompositeValidationError(res...)
+	}
+	return nil
+}
+
+func (m *Boot) contextValidateSecureBootState(ctx context.Context, formats strfmt.Registry) error {
+
+	if err := m.SecureBootState.ContextValidate(ctx, formats); err != nil {
+		if ve, ok := err.(*errors.Validation); ok {
+			return ve.ValidateName("secure_boot_state")
+		} else if ce, ok := err.(*errors.CompositeError); ok {
+			return ce.ValidateName("secure_boot_state")
+		}
+		return err
+	}
+
 	return nil
 }
 
