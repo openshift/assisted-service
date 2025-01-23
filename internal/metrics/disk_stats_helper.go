f8e3e31c6e56762f0eaa67c6b4a9565c7a566cdf
Paul Maidment <pmaidmen@redhat.com>
2025-01-23 16:03:47 +0200

    NO-ISSUE: Ensure that hard links are accounted for when measuring disk usage. (#7217)
    When we measure the disk usage for metrics, we need to ensure that hard links are accounted for as these may lead to an inaccurate count.
    This PR modifies the gathering of filesizes slightly to perform a stat on each file and take into account the unique inode number.
    
    Hard links and actual files share the same inode number as this is a reference to the underlying data.

--- internal/metrics/disk_stats_helper.go
+++ internal/metrics/disk_stats_helper.go
@@ -1,8 +1,10 @@
 package metrics
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"syscall"
 
 	"github.com/pkg/errors"
 	"golang.org/x/sys/unix"
@@ -22,12 +24,22 @@ func NewOSDiskStatsHelper() *OSDiskStatsHelper {
 
 func (c *OSDiskStatsHelper) getUsedBytesInDirectory(directory string) (uint64, error) {
 	var totalBytes uint64
+	// Maintain a map of inodes we have seen so that we don't double count storage
+	seenInodes := make(map[uint64]bool)
 	err := filepath.Walk(directory, func(path string, fileInfo os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-		if !fileInfo.IsDir() {
+		// We need to ensure that the size check is based on inodes and not just the sizes gleaned from files.
+		// we should ensure that we have not seen a particular inode for a given file before.
+		// this is because there are hard links in use and we need to account for this.
+		stat, ok := fileInfo.Sys().(*syscall.Stat_t)
+		if !ok {
+			return fmt.Errorf("unable to determine stat information for path %s", path)
+		}
+		if !fileInfo.IsDir() && !seenInodes[stat.Ino] {
 			totalBytes += uint64(fileInfo.Size())
+			seenInodes[stat.Ino] = true
 		}
 		return nil
 	})
