75f5ba6ff83a3c01aaef3fe4a7400997756c4587
aantal <aantal@redhat.com>
2025-02-12 11:19:13 +0100

    fixing some syntax errors in dir 'pkg'

--- pkg/webhooks/hiveextension/v1beta1/agentclusterinstall_mutating_hook.go
+++ pkg/webhooks/hiveextension/v1beta1/agentclusterinstall_mutating_hook.go
@@ -173,7 +173,7 @@ func patchUserManagedNetworking(newObject *hiveext.AgentClusterInstall, logger *
 	// 2. Platform is specified.
 	if !isNonePlatformOrSNO(newObject) && newObject.Spec.PlatformType != "" {
 		platform := common.PlatformTypeToPlatform(newObject.Spec)
-		_, platformUserManagedNetworking, err = provider.GetClusterPlatformByHighAvailabilityMode(platform, nil, highAvailabilityMode)
+		_, platformUserManagedNetworking, err = provider.GetClusterPlatformByHighAvailabilityMode(platform, nil, &highAvailabilityMode)
 		if err != nil {
 			logger.Warnf("Cannot set UserManagedNetworking automatically due to: %s", err.Error())
 			return nil
