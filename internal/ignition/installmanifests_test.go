4ae882bc9f07862009fac2ee94534885922103dc
Matthieu Bernardin <mbernard@redhat.com>
2025-03-27 12:20:23 +0100

    NO-ISSUE: Upgrade linter

--- internal/ignition/installmanifests_test.go
+++ internal/ignition/installmanifests_test.go
@@ -177,7 +177,7 @@ var _ = Describe("Bootstrap Ignition Update", func() {
 			Expect(err).ToNot(HaveOccurred())
 			for i := range config.Storage.Files {
 				if isBMHFile(&config.Storage.Files[i]) {
-					bmhFile, err2 := fileToBMH(&config.Storage.Files[i]) //nolint,shadow
+					bmhFile, err2 := fileToBMH(&config.Storage.Files[i])
 					Expect(err2).ToNot(HaveOccurred())
 					Expect(bmhIsMaster(bmhFile, masterHostnames, workerHostnames)).To(Equal(masterExpected))
 					return
