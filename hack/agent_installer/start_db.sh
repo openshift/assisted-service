f8add95dce8948690b2d24ba4468ea7d7e4961f3
Prashanth Sundararaman <psundara@redhat.com>
2025-03-27 20:11:18 -0700

    OKD: Use /tmp as socket directory when calling pg_ctl and postgres (#7458)
    OKD installations are failing when using agent and assisted service as
    described here: https://github.com/okd-project/okd/discussions/2068#discussioncomment-11733877
    with:
    
    ```
    FATAL:  could not create lock file "/var/run/postgresql/.s.PGSQL.5432.lock": No such file or directory"
    ```
    
    Explicitly tell postgres to use /tmp as the unix socket directory to fix the issue.

--- hack/agent_installer/start_db.sh
+++ hack/agent_installer/start_db.sh
@@ -4,9 +4,9 @@ set -o nounset
 set -euo pipefail
 set -o errexit
 
-/usr/bin/pg_ctl -D /tmp/postgres/data/ -l /tmp/postgres/logfile start -w
+/usr/bin/pg_ctl -D /tmp/postgres/data/ -l /tmp/postgres/logfile start -w -o "-k /tmp"
 createuser -s admin -h localhost
 createdb installer -h localhost
 
-/usr/bin/pg_ctl -D /tmp/postgres/data/ -l /tmp/postgres/logfile stop -w
-exec postgres -D /tmp/postgres/data/
+/usr/bin/pg_ctl -D /tmp/postgres/data/ -l /tmp/postgres/logfile stop -w -o "-k /tmp"
+exec postgres -D /tmp/postgres/data/ -k /tmp
