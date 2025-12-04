2a148d4c64705e43c9bebe8c1768aa15eeab8a55
CrystalChun <cchun@redhat.com>
2025-12-04 13:48:35 -0600

    MGMT-22189: Order dual-stack networks when fetching from DB
    Previously, dual-stack network ordering was not guaranteed
    when fetching a cluster from the DB since the fields are
    text fields.
    
    In order to guarantee the order, every fetch for the cluster
    from the db will add a postgres clause for the network
    subtables.
    
    The postgres clause gets ip family of the network ip address
    or cidrs, and will then order them based on the primary_ip_family
    column for that cluster.

--- internal/bminventory/inventory.go
+++ internal/bminventory/inventory.go
@@ -392,6 +392,7 @@ func (b *bareMetalInventory) updatePullSecret(pullSecret string, log logrus.Fiel
 	return pullSecret, nil
 }
 
+// TODO: handle defaults for dual-stack clusters with primary IPv6
 func (b *bareMetalInventory) setDefaultRegisterClusterParams(ctx context.Context, params installer.V2RegisterClusterParams) (installer.V2RegisterClusterParams, error) {
 	log := logutil.FromContext(ctx, b.log)
 
@@ -1412,11 +1413,7 @@ func (b *bareMetalInventory) InstallClusterInternal(ctx context.Context, params 
 	if cluster, err = common.GetClusterFromDBWithHosts(b.db, params.ClusterID); err != nil {
 		return nil, common.NewApiError(http.StatusNotFound, err)
 	}
-
-	b.orderClusterNetworks(cluster)
-
 	var autoAssigned bool
-
 	// auto select hosts roles if not selected yet.
 	err = b.db.Transaction(func(tx *gorm.DB) error {
 		var updated bool
@@ -1469,8 +1466,6 @@ func (b *bareMetalInventory) InstallClusterInternal(ctx context.Context, params 
 		if cluster, err = common.GetClusterFromDBWithHosts(b.db, params.ClusterID); err != nil {
 			return nil, common.NewApiError(http.StatusNotFound, err)
 		}
-
-		b.orderClusterNetworks(cluster)
 	}
 	// Verify cluster is ready to install
 	if ok, reason := b.clusterApi.IsReadyForInstallation(cluster); !ok {
@@ -1496,8 +1491,6 @@ func (b *bareMetalInventory) InstallClusterInternal(ctx context.Context, params 
 	if cluster, err = common.GetClusterFromDB(b.db, params.ClusterID, common.UseEagerLoading); err != nil {
 		return nil, err
 	}
-
-	b.orderClusterNetworks(cluster)
 
 	if err = b.clusterApi.GenerateAdditionalManifests(ctx, cluster); err != nil {
 		b.log.WithError(err).Errorf("Failed to generate additional cluster manifest")
@@ -1834,7 +1827,6 @@ func (b *bareMetalInventory) UpdateClusterInstallConfigInternal(ctx context.Cont
 			log.WithError(err).Errorf("failed to find cluster %s", params.ClusterID)
 			return err
 		}
-		b.orderClusterNetworks(cluster)
 
 		clusterInfraenvs, err = b.getClusterInfraenvs(cluster)
 		if err != nil {
@@ -2346,8 +2338,6 @@ func (b *bareMetalInventory) v2UpdateClusterInternal(ctx context.Context, params
 		log.WithError(err).Errorf("failed to get cluster %s after update", params.ClusterID)
 		return nil, err
 	}
-
-	b.orderClusterNetworks(cluster)
 
 	if cluster != nil {
 		notifiableCluster := stream.GetNotifiableCluster(cluster)
@@ -3501,8 +3491,6 @@ func (b *bareMetalInventory) listClustersInternal(ctx context.Context, params in
 			h.FreeAddresses = ""
 		}
 
-		b.orderClusterNetworks(c)
-
 		clusters = append(clusters, &c.Cluster)
 	}
 	return clusters, nil
@@ -3529,8 +3517,6 @@ func (b *bareMetalInventory) GetClusterInternal(ctx context.Context, params inst
 	if err != nil {
 		return nil, err
 	}
-
-	b.orderClusterNetworks(cluster)
 
 	cluster.HostNetworks = b.calculateHostNetworks(log, cluster)
 	for _, host := range cluster.Hosts {
@@ -4430,8 +4416,6 @@ func (b *bareMetalInventory) CancelInstallationInternal(ctx context.Context, par
 		return nil, err
 	}
 
-	b.orderClusterNetworks(cluster)
-
 	return cluster, nil
 }
 
@@ -4662,8 +4646,6 @@ func (b *bareMetalInventory) getCluster(ctx context.Context, clusterID string, f
 		log.Error(err)
 		return nil, err
 	}
-
-	b.orderClusterNetworks(cluster)
 
 	return cluster, nil
 }
@@ -7174,7 +7156,6 @@ func (b *bareMetalInventory) generateAgentInstallerKubeconfigWithLock(ctx contex
 		if err != nil {
 			return err
 		}
-		b.orderClusterNetworks(cluster)
 
 		// Double-check: File might have been created by another request while we were waiting for the lock
 		objectName := fmt.Sprintf("%s/%s", cluster.ID, fileName)
@@ -7206,17 +7187,6 @@ func (b *bareMetalInventory) generateAgentInstallerKubeconfigWithLock(ctx contex
 	})
 
 	return err
-}
-
-// orderClusterNetworks orders cluster networks according to PrimaryIPStack
-func (b *bareMetalInventory) orderClusterNetworks(cluster *common.Cluster) {
-	if cluster.PrimaryIPStack != nil {
-		cluster.ClusterNetworks = network.OrderNetworksByPrimaryStack(cluster.ClusterNetworks, *cluster.PrimaryIPStack).([]*models.ClusterNetwork)
-		cluster.ServiceNetworks = network.OrderNetworksByPrimaryStack(cluster.ServiceNetworks, *cluster.PrimaryIPStack).([]*models.ServiceNetwork)
-		cluster.MachineNetworks = network.OrderNetworksByPrimaryStack(cluster.MachineNetworks, *cluster.PrimaryIPStack).([]*models.MachineNetwork)
-		cluster.APIVips = network.OrderNetworksByPrimaryStack(cluster.APIVips, *cluster.PrimaryIPStack).([]*models.APIVip)
-		cluster.IngressVips = network.OrderNetworksByPrimaryStack(cluster.IngressVips, *cluster.PrimaryIPStack).([]*models.IngressVip)
-	}
 }
 
 // extractMirroredRegistriesFromConfig takes a MirrorRegistryConfiguration and returns a list of source
