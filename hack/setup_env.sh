3fafc7d254da10936ce569df5f89321ae53b3480
omer-vishlitzky <omer.vishlitzky@gmail.com>
2025-12-28 13:12:13 +0200

    Upgrade go version to 1.25

--- hack/setup_env.sh
+++ hack/setup_env.sh
@@ -63,7 +63,7 @@ function test_tools() {
   go install github.com/onsi/ginkgo/ginkgo@v1.16.4
   go install github.com/golang/mock/mockgen@v1.6.0
   go install github.com/vektra/mockery/v2@v2.12.3
-  go install gotest.tools/gotestsum@v1.6.3
+  go install gotest.tools/gotestsum@v1.13.0
   go install github.com/axw/gocov/gocov@v1.1.0
   go install github.com/AlekSi/gocov-xml@v1.1.0
   envtest
