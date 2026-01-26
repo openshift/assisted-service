5b7752f9f2289eb6075d2c8a43291cb42a3c3887
Yoav Schwammenthal <yoavsc0302@gmail.com>
2026-01-26 17:40:12 +0200

    MGMT-22287: Remove OCP 4.12 support from assisted-service
    OCP 4.12 has reached end of life.
    
    Remove OpenShift 4.12 from default OS images, release images,
    and release sources. Regenerate all deployment manifests.
    
    - Removed 4.12 entries from data/default_os_images.json
    - Removed 4.12 entries from data/default_release_images.json
    - Removed 4.12 entry from data/default_release_sources.json
    - Regenerated configuration files via 'skipper make generate'

--- subsystem/subsystem_suite_test.go
+++ subsystem/subsystem_suite_test.go
@@ -36,7 +36,7 @@ var kubeClient k8sclient.Client
 var openshiftVersion string = "4.11"
 var snoVersion string = "4.11"
 var multiarchOpenshiftVersion string = "4.11.0-multi"
-var dualstackVipsOpenShiftVersion string = "4.12.0"
+var dualstackVipsOpenShiftVersion string = "4.13.0"
 var VipAutoAllocOpenshiftVersion string = "4.14.0"
 var SDNNetworkTypeOpenshiftVersion string = "4.14.0"
 var pullSecret = "{\"auths\":{\"cloud.openshift.com\":{\"auth\":\"dXNlcjpwYXNzd29yZAo=\",\"email\":\"r@r.com\"}}}" // #nosec
